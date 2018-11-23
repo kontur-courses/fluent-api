891c13bf0eecf74287f9facac15edaf43b146db1
Avel7884 <Avel7884@users.noreply.github.com>
2018-11-23 20:45:42 +0500

    Classwork

---
+++ ObjectPrinting/ObjectExtensions.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ObjectPrinting
+{
+    public static class ObjectExtensions
+    {
+        public static string Serialize<TOwner>(this TOwner owner,Func<PrintingConfig<TOwner>,PrintingConfig<TOwner>> config = null)
+        {
+            return config != null
+                ? config(ObjectPrinter.For<TOwner>()).PrintToString(owner)
+                : ObjectPrinter.For<TOwner>().PrintToString(owner);
+        }
+    }
+}
--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ObjectPrinting
@@ -36,6 +38,87 @@ namespace ObjectPrinting
                               nestingLevel + 1));
             }
             return sb.ToString();
+        }
+
+        public PrintingConfig<TOwner> Exclude<TPropType>()
+        {
+            return this;
+        }
+
+
+//        public PrintingConfig<TOwner> SetCulture(CultureInfo currentCulture)
+//        {
+//            return this;
+//        }
+
+        public PrintingConfig<TOwner,TPropType> Serializing<TPropType>()
+        {
+            return new PrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public PropertyPrintingConfig<TOwner,TPropType> Serializing<TPropType>(Expression<Func<TOwner,TPropType>> selector)
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this,selector);
+        }
+
+        public PrintingConfig<TOwner> Exclude<TPropType>(Expression<Func<TOwner,TPropType>> selector)
+        {
+            return this;
+        }
+    }
+    
+    public class PrintingConfig<TOwner,TPropType> : IPrintingConfig<TOwner>
+    {
+        private readonly PrintingConfig<TOwner> printingConfig;
+        public PrintingConfig(PrintingConfig<TOwner> printingConfig)
+        {
+            this.printingConfig = printingConfig;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializer)
+        {
+            return printingConfig;
+        }
+
+        PrintingConfig<TOwner> IPrintingConfig<TOwner>.PrintingConfig => printingConfig;
+    }
+    
+    
+    public class PropertyPrintingConfig<TOwner,TPropType> : IPrintingConfig<TOwner>
+    {
+        private readonly PrintingConfig<TOwner> printingConfig;
+        private readonly Expression<Func<TOwner, TPropType>> selector;
+
+        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig, Expression<Func<TOwner,TPropType>>  selector)
+        {
+            this.printingConfig = printingConfig;
+            this.selector = selector;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializer)
+        {
+            return printingConfig;
+        }
+        PrintingConfig<TOwner> IPrintingConfig<TOwner>.PrintingConfig => printingConfig;
+    }
+
+    public interface IPrintingConfig<TOwner>
+    {
+        PrintingConfig<TOwner> PrintingConfig { get; }
+    }
+
+    public static class PrintingConfigExtenstion
+    {
+        public static PrintingConfig<TOwner> Using<TOwner>(this PrintingConfig<TOwner, int> printingConfig,
+            CultureInfo currentCulture)
+        {
+            return ((IPrintingConfig<TOwner>)printingConfig).PrintingConfig;
+        }
+        
+        public static PrintingConfig<TOwner> TrimToLength<TOwner>(this PropertyPrintingConfig<TOwner, string> printingConfig,
+            int length)
+        {
+            return ((IPrintingConfig<TOwner>)printingConfig).PrintingConfig;
         }
     }
 }
