9ec9ca1dd39b15b8ea234bc98f7a220fdea9803b
Andrew <avural-k@mail.ru>
2018-11-24 20:49:27 +0500

    interface

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,15 +1,83 @@
 using System;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
 namespace ObjectPrinting
 {
+
+    public class TypePrintingConfig<TOwner, TPropType> : ITypePrintingConfig<TOwner>
+    {
+        private readonly PrintingConfig<TOwner> printConfig;
+
+        public TypePrintingConfig(PrintingConfig<TOwner> printConfig)
+        {
+            this.printConfig = printConfig;
+        }
+
+        public TypePrintingConfig(PrintingConfig<TOwner> printConfig, Func<TOwner, TPropType> func)
+        {
+            this.printConfig = printConfig;
+        }
+
+        PrintingConfig<TOwner> ITypePrintingConfig<TOwner>.PrintingConfig => printConfig;
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serFunc)
+        {
+            return printConfig;
+        }
+    }
+
+    interface ITypePrintingConfig<TOwner>
+    {
+        PrintingConfig<TOwner> PrintingConfig { get; }
+    }
+     
+    public static class TypePrintingConfigExtention
+    {
+        public static PrintingConfig<TOwner> Using<TOwner>(this TypePrintingConfig<TOwner, double> me, CultureInfo culture)
+        {
+            return (me as ITypePrintingConfig<TOwner>).PrintingConfig;
+        }
+
+        public static PrintingConfig<TOwner> Trimming<TOwner>(this TypePrintingConfig<TOwner, string> me, int len)
+        {
+            return (me as ITypePrintingConfig<TOwner>).PrintingConfig;
+        }
+    }
+
     public class PrintingConfig<TOwner>
     {
+        public PrintingConfig<TOwner> Exclude<TPropType>()
+        {
+            return this;
+        }
+
+        public PrintingConfig<TOwner> Exclude<TPropType>(Func<TOwner, TPropType> func)
+        {
+            return this;
+        }
+
+        public TypePrintingConfig<TOwner, TPropType> NewSerialise<TPropType>()
+        {
+            return new TypePrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public TypePrintingConfig<TOwner, TPropType> NewSerialise<TPropType>(Func<TOwner, TPropType> func)
+        {
+            return new TypePrintingConfig<TOwner, TPropType>(this, func);
+        }
+
+        //public PropertyPrintingConfig<TOwner, string> NewSerialise(Func<TOwner, string> func, int len)
+        //{
+        //    return new PropertyPrintingConfig<TOwner, string>(this, (s) => (s as string).Substring(0, len));
+        //}
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
         }
+
 
         private string PrintToString(object obj, int nestingLevel)
         {
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Globalization;
 
 namespace ObjectPrinting.Tests
 {
@@ -10,14 +11,20 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Exclude<int>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .NewSerialise<char>().Using((ch) => ((int)ch).ToString())
                 //3. Для числовых типов указать культуру
+                .NewSerialise<double>().Using(CultureInfo.CurrentCulture)
                 //4. Настроить сериализацию конкретного свойства
+                .NewSerialise(p => p.Age).Using((ch) => (ch).ToString())
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .NewSerialise(p => p.Name).Trimming(3)
                 //6. Исключить из сериализации конкретного свойства
-            
+                .Exclude(p => p.Id);
+
             string s1 = printer.PrintToString(person);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
