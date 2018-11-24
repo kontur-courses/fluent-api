aa8d1f5d3009d3811f169892cf220ff58d0c5d10
Turin_nowadays <k.dobrovolsky@gmail.com>
2018-11-24 12:33:03 +0500

    Работа на лекции

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,27 @@
 using System;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        public TypePrintingConfig<TOwner, TPropType> Serialize<TPropType>()
+        {
+            return new TypePrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public TypePrintingConfig<TOwner, TPropType> Serialize<TPropType>(Expression<Func<TOwner, TPropType>> propertyFunc)
+        {
+            return new TypePrintingConfig<TOwner, TPropType>(this);
+        }
+        public PrintingConfig<TOwner> Exclude<TPropType>()
+        {
+            return this;
+        }
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -36,6 +52,46 @@ namespace ObjectPrinting
                               nestingLevel + 1));
             }
             return sb.ToString();
+        }
+    }
+
+    public class TypePrintingConfig<TOwner, TPropType> : ITypePrintingConfig<TOwner>
+    {
+        private readonly PrintingConfig<TOwner> printingConfig;
+
+        PrintingConfig<TOwner> ITypePrintingConfig<TOwner>.PrintingConfig => printingConfig;
+
+        internal TypePrintingConfig(PrintingConfig<TOwner> printingConfig)
+        {
+            this.printingConfig = printingConfig;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializationMethod)
+        {
+            return printingConfig;
+        }
+        //public PrintingConfig<TOwner> WithCulture(CultureInfo culture)
+        //{
+        //    return printingConfig;
+        //}
+
+    }
+
+    interface ITypePrintingConfig<TOwner>
+    {
+        PrintingConfig<TOwner> PrintingConfig { get; }
+    }
+
+    public static class TypePrintingConfigExtensions
+    {
+        public static PrintingConfig<TOwner> Using<TOwner> (this TypePrintingConfig<TOwner, int> config, CultureInfo culture)
+        {
+            return ((ITypePrintingConfig<TOwner>) config).PrintingConfig;
+        }
+
+        public static PrintingConfig<TOwner> TrimToLength<TOwner>(this TypePrintingConfig<TOwner, string> config, int length)
+        {
+            return ((ITypePrintingConfig<TOwner>)config).PrintingConfig;
         }
     }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using System.Runtime.Remoting.Messaging;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,20 +11,37 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person {Name = "Alex", Age = 19};
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Exclude<int>()
+                .Serialize<int>().Using(l => l.ToString())
                 //2. Указать альтернативный способ сериализации для определенного типа
                 //3. Для числовых типов указать культуру
+                //---.Serialize<string>().WithCulture(CultureInfo.CurrentCulture);
+                .Serialize<Int32>().Using(CultureInfo.CurrentCulture)
+                .Serialize<string>().Using()
                 //4. Настроить сериализацию конкретного свойства
+                .Serialize(p => p.Age).Using(a => a.ToString())
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
+                .Serialize(p => p.Name).TrimToLength(8);
+
+            //6. Исключить из сериализации конкретного свойства
+
             string s1 = printer.PrintToString(person);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
+
+
         }
     }
+
+    class Serializer
+    {
+    }
 }
+
+georgij.al@yandex.ru 
+
