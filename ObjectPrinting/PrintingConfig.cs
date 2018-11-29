ce994985369e377eea0006181bc63803da694ce1
Ivan Plaksin <zero.netto.o@gmail.com>
2018-11-29 23:05:16 +0500

    Added main structure.

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -7,31 +7,31 @@ namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
-        public string PrintToString(TOwner obj)
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
         {
-            return PrintToString(obj, 0);
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
         }
 
-        public PrintingConfig<TOwner> Exclude<TPropType>()
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        {
+
+            return this;
+        }
+
+        internal PrintingConfig<TOwner> Excluding<TPropType>()
         {
             return this;
         }
 
-        public TypePrintingConfig<TOwner, TPropType> Serializing<TPropType>()
+        public string PrintToString(TOwner obj)
         {
-            return new TypePrintingConfig<TOwner, TPropType>(this);
+            return PrintToString(obj, 0);
         }
-
-        public TypePrintingConfig<TOwner, TPropType> Serializing<TPropType>(Expression<Func<TOwner, TPropType>> propertyFunc)
-        {
-            return new TypePrintingConfig<TOwner, TPropType>(this);
-        }
-
-        public TypePrintingConfig<TOwner, string> Serializing<String>(Expression<Func<TOwner, string>> propertyFunc)
-        {
-            return new TypePrintingConfig<TOwner, string>(this);
-        }
-
 
         private string PrintToString(object obj, int nestingLevel)
         {
@@ -47,13 +47,13 @@ namespace ObjectPrinting
             if (finalTypes.Contains(obj.GetType()))
                 return obj + Environment.NewLine;
 
-            var identation = new string('\t', nestingLevel + 1);
+            var indentation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
+                sb.Append(indentation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
             }
---
+++ ObjectPrinting/PropertyPrintingConfigExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPrinting
+{
+    public static class PropertyPrintingConfigExtensions
+    {
+        public static string PrintToString<T>(this T obj, Func<PrintingConfig<T>, PrintingConfig<T>> config)
+        {
+            return config(ObjectPrinter.For<T>()).PrintToString(obj);
+        }
+
+        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this PropertyPrintingConfig<TOwner, string> propConfig, int maxLen)
+        {
+            return ((IPropertyPrintingConfig<TOwner, string>)propConfig).ParentConfig;
+        }
+    }
+}
