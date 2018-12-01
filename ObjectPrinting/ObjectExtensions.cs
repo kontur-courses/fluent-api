aac0f1818a44a8d5d3ab6f954ec406c446635730
Ivan Plaksin <zero.netto.o@gmail.com>
2018-12-02 00:53:27 +0500

    Remove unnecessary libraries + rename IEnumerable test

--- ObjectPrinting/ObjectExtensions.cs
+++ ObjectPrinting/ObjectExtensions.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ObjectPrinting
+﻿namespace ObjectPrinting
 {
     public static class ObjectExtensions
     {
