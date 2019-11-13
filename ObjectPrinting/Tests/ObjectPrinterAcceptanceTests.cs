0ff620c4079c6e7a81286b455054213bae28c49f
Fedor Barkov <hitsedesen@yandex.ru>
2019-11-13 20:17:36 +0500

    Create PrintingConfig' basic interface

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using System.Linq.Expressions;
+using System.Reflection;
+using FluentAssertions.Common;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,20 +13,29 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person {Name = "Alex", Age = 19};
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+            var printer = ObjectPrinter.For<Person>()
+                .Excluding<Guid>()
+                .Printing<string>().Using(s => s.Trim())
+                .Printing<int>().Using(CultureInfo.InvariantCulture)
+                .Printing(p => p.Name).Using(s => s.Trim())
+                .Printing(p => p.Name).Substring(0, 15)  
+                .Printing<string>().Substring(3, 10)
+                .Excluding(p => p.Age);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
+            //✔1. Исключить из сериализации свойства определенного типа
+            //✔2. Указать альтернативный способ сериализации для определенного типа
+            //✔3. Для числовых типов указать культуру
+            //✔4. Настроить сериализацию конкретного свойства
+            //✔5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+            //✔6. Исключить из сериализации конкретного свойства
+
+            var s1 = printer.PrintToString(person);
+
+            var s2 = person.Printing().Excluding<Guid>().PrintToString(35);
+            //✔7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            //✔8. ...с конфигурированием
         }
     }
 }
