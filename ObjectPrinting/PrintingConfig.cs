703597d06b00a5770fe9333c34f3276c1e7d5fac
TVIGes <khdv66@gmail.com>
2019-11-16 18:08:15 +0500

    Add tasks solution

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,17 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private readonly ISet<Type> excluded = new HashSet<Type>();
+        
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -13,9 +19,11 @@ namespace ObjectPrinting
 
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
+            
+            if (excluded.Contains(obj.GetType()))
+                return string.Empty;
 
             var finalTypes = new[]
             {
@@ -35,7 +43,80 @@ namespace ObjectPrinting
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
             }
+
             return sb.ToString();
+        }
+
+        public PrintingConfig<TOwner> Excluding<T>()
+        {
+            excluded.Add(typeof(T));
+            return this;
+        }
+
+        public PropertySerializingConfig<TOwner, T> Serializing<T>()
+        {
+            return new PropertySerializingConfig<TOwner, T>(this);
+        }
+
+        public PropertySerializingConfig<TOwner, T> Serializing<T>(Expression<Func<TOwner, T>> func)
+        {
+            var property = ((MemberExpression) func.Body).Member as PropertyInfo;
+            return new PropertySerializingConfig<TOwner, T>(this);
+        }
+
+        public PrintingConfig<TOwner> Excluding<T>(Expression<Func<TOwner, T>> func)
+        {
+            return new PrintingConfig<TOwner>();
+        }
+    }
+
+    internal interface IPropertySerializingConfig<TOwner>
+    {
+        PrintingConfig<TOwner> ParentConfig { get; }
+    }
+
+    public class PropertySerializingConfig<TOwner, TPropType> : IPropertySerializingConfig<TOwner>
+    {
+        private readonly PrintingConfig<TOwner> parentConfig;
+
+        PrintingConfig<TOwner> IPropertySerializingConfig<TOwner>.ParentConfig => parentConfig;
+
+        public PropertySerializingConfig(PrintingConfig<TOwner> parentConfig)
+        {
+            this.parentConfig = parentConfig;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializationMethod)
+        {
+            return parentConfig;
+        }
+    }
+
+    public static class PropertySerializingConfigExtensions
+    {
+        public static PrintingConfig<TOwner> Using<TOwner>(this PropertySerializingConfig<TOwner, int> config,
+            CultureInfo cultureInfo)
+        {
+            return (config as IPropertySerializingConfig<TOwner>).ParentConfig;
+        }
+        
+        public static PrintingConfig<TOwner> TrimmingToLength<TOwner>(this PropertySerializingConfig<TOwner, string> config,
+            int count)
+        {
+            return (config as IPropertySerializingConfig<TOwner>).ParentConfig;
+        }
+    }
+
+    public static class ObjectExtensions
+    {
+        public static string PrintToString<T>(this T obj)
+        {
+            return ObjectPrinter.For<T>().PrintToString(obj);
+        }
+        
+        public static string PrintToString<T>(this T obj, Func<PrintingConfig<T>, PrintingConfig<T>> func)
+        {
+            return ObjectPrinter.For<T>().PrintToString(obj);
         }
     }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,20 +10,29 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person {Name = "Alex", Age = 19};
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Excluding<Guid>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .Serializing<DateTime>().Using(d => d.ToString())
                 //3. Для числовых типов указать культуру
+                .Serializing<int>().Using(CultureInfo.CurrentCulture)
                 //4. Настроить сериализацию конкретного свойства
+                .Serializing(p => p.Id).Using(p => p.ToString())
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Serializing(p => p.Name).TrimmingToLength(20)
                 //6. Исключить из сериализации конкретного свойства
+                .Excluding(p => p.Age);
             
-            string s1 = printer.PrintToString(person);
+            var s1 = printer.PrintToString(person);
+            Console.WriteLine(s1);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+            var s2 = person.PrintToString();
             //8. ...с конфигурированием
+            var s3 = person.PrintToString(p => p.Excluding<string>());
         }
     }
 }
