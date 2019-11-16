149c4495c05c38bba2b665c63de96e7c17ab1e99
And <fakmaki99@mail.ru>
2019-11-16 13:45:33 +0500

    ClassWork

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -11,17 +13,29 @@ namespace ObjectPrinting.Tests
             var person = new Person { Name = "Alex", Age = 19 };
 
             var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            //1. Исключить из сериализации свойства определенного типа
+            printer.Excluding<Guid>();
+            //2. Указать альтернативный способ сериализации для определенного типа
+            printer.For<string>().WithSerialization(p => p.ToString() + "changed");
+            //3. Для числовых типов указать культуру
+            printer.For<int>().WithCulture(CultureInfo.CurrentCulture);
+            //4. Настроить сериализацию конкретного свойства
+            printer.For(p => p.Age).WithSerialization(p => p.ToString());
+            //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+            printer.For(p => p.Name).Trim(4);
+            // printer.For(p => p.Age).Trim(4);
+            //6. Исключить из сериализации конкретного свойства
+            printer.Excluding(p => p.Name);
+            printer.For(p => p.Age).WithSerialization(p => p.ToString());
+            //string s1 = printer.PrintToString(person);
+
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию  
+            printer.PrintToString(person);
+            person.PrintToString();
             //8. ...с конфигурированием
+            person.PrintToString(settings => settings.For<string>().WithSerialization(p => p.ToString() + "changed"));
+
         }
     }
 }
