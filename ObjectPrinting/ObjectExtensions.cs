4d8e3ee4360f7d1c48de7fbcd035be4b21522916
Fedor Barkov <hitsedesen@yandex.ru>
2019-11-22 17:11:28 +0300

    Extract object extensions, property printing config and it's extensions into new files

---
+++ ObjectPrinting/ObjectExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectPrinting
+{
+    public static class ObjectExtensions
+    {
+        public static PrintingConfig<TOwner> Printing<TOwner>(this TOwner obj)
+        {
+            return new PrintingConfig<TOwner>(obj);
+        }
+    }
+}
---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ObjectPrinting
+{
+    public interface IPropertyPrintingConfig<TOwner>
+    {
+        PrintingConfig<TOwner> ParentConfig { get; }
+    }
+    
+    public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner>
+    {
+        private PrintingConfig<TOwner> parentConfig;
+
+        public PropertyPrintingConfig(PrintingConfig<TOwner> parentConfig)
+        {
+            this.parentConfig = parentConfig;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> func)
+        {
+            return parentConfig;
+        }
+        
+        PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner>.ParentConfig => parentConfig;
+    }
+}
