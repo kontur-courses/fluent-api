727b34697aaadd3dc6f5120fe823624e76540f5b
Данил Панков <mistertimechip@mail.ru>
2019-11-23 12:57:30 +0300

    Finished acceptance tests

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Runtime.Serialization;
+using NUnit.Framework;
+using ObjectPrinting.Extensions;
 
 namespace ObjectPrinting.Tests
 {
@@ -10,18 +13,26 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Excluding<Guid>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .Serialize<string>().Using(s => s.Trim())
                 //3. Для числовых типов указать культуру
+                .Serialize<int>().Using(System.Globalization.CultureInfo.CurrentCulture)
                 //4. Настроить сериализацию конкретного свойства
+                .Serialize(p => p.Name).Using(str => "Name: " + str)
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Serialize(p => p.Name).Take(5)
                 //6. Исключить из сериализации конкретного свойства
+                .Excluding(p => p.Age);
             
             string s1 = printer.PrintToString(person);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+            person.Serialized();
             //8. ...с конфигурированием
+            person.Serialized().Excluding(p => p.Age);
         }
     }
 }
