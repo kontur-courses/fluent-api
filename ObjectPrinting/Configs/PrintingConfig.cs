fd2fe4fcbc1f44ad8cd47b40d3b9f1f6d94da6ca
Данил Панков <mistertimechip@mail.ru>
2019-11-23 14:13:43 +0300

    Wrote realisation using tdd

--- ObjectPrinting/Configs/PrintingConfig.cs
+++ ObjectPrinting/Configs/PrintingConfig.cs
@@ -2,14 +2,39 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using NUnit.Framework.Constraints;
+using ObjectPrinting.Configs.ConfigInterfaces;
 
 namespace ObjectPrinting.Configs
 {
-    public class PrintingConfig<TOwner>
+    public class PrintingConfig<TOwner> : IPrintingConfig<TOwner>
     {
-        private HashSet<Type> excludedTypes = new HashSet<Type>();
-        private Dictionary<Type, Func<Type, string>> alternativeSerializations = new Dictionary<Type, Func<Type, string>>();
+        private readonly HashSet<Type> excludedTypes;
+        private readonly HashSet<PropertyInfo> excludedProperties;
+        private readonly Dictionary<Type, Func<Type, string>> alternativeSerializations;
+
+        HashSet<Type> IPrintingConfig<TOwner>.ExcludedTypes => excludedTypes;
+        Dictionary<Type, Func<Type, string>> IPrintingConfig<TOwner>.AlternativeSerializations => alternativeSerializations;
+
+        public PrintingConfig()
+        {
+            excludedTypes = new HashSet<Type>();
+            excludedProperties = new HashSet<PropertyInfo>();
+            alternativeSerializations = new Dictionary<Type, Func<Type, string>>();
+        }
+        
+        public PrintingConfig(
+            HashSet<Type> excludedTypes,
+            HashSet<PropertyInfo> excludedProperties,
+            Dictionary<Type, Func<Type, string>> alternativeSerializations)
+        {
+            this.excludedTypes = new HashSet<Type>(excludedTypes);
+            this.excludedProperties = new HashSet<PropertyInfo>(excludedProperties);
+            this.alternativeSerializations = new Dictionary<Type, Func<Type, string>>(alternativeSerializations);
+        }
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -37,6 +62,7 @@ namespace ObjectPrinting.Configs
             foreach (var propertyInfo in type.GetProperties())
             {
                 if(excludedTypes.Contains(propertyInfo.PropertyType)) continue;
+                if(excludedProperties.Contains(propertyInfo)) continue;
                 sb.Append(identation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
@@ -46,13 +72,16 @@ namespace ObjectPrinting.Configs
 
         public PrintingConfig<TOwner> Excluding<T>()
         {
-            excludedTypes.Add(typeof(T));
-            return this;
+            var newExcludingTypes = new HashSet<Type>(excludedTypes) {typeof(T)};
+            return new PrintingConfig<TOwner>(newExcludingTypes, excludedProperties, alternativeSerializations);
         }
         
         public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> propertyDelegate)
         {
-            return this;
+            var propertyInfo = ((MemberExpression) propertyDelegate.Body).Member as PropertyInfo;
+            if(propertyInfo == null) throw new ArgumentException();
+            var newExcludingProperties = new HashSet<PropertyInfo>(excludedProperties) {propertyInfo};
+            return new PrintingConfig<TOwner>(excludedTypes, newExcludingProperties, alternativeSerializations);
         }
 
         public PropertySerializationConfig<TOwner, TPropType> Serialize<TPropType>()
