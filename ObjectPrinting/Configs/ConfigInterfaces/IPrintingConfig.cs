a728c2d4a7043ce70f8089fdb81a88b3beb9f43a
Данил Панков <mistertimechip@mail.ru>
2019-11-23 14:14:12 +0300

    Interface for internal purposes

---
+++ ObjectPrinting/Configs/ConfigInterfaces/IPrintingConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectPrinting.Configs.ConfigInterfaces
+{
+    internal interface IPrintingConfig<TOwner>
+    {
+        HashSet<Type> ExcludedTypes { get; }
+        Dictionary<Type, Func<Type, string>> AlternativeSerializations { get;}
+    }
+}
