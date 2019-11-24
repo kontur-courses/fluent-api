158fac5273c99e2d16cb1ea4b5ae1293fe3d24e4
Zakepanka <arzybek@users.noreply.github.com>
2019-11-24 19:38:13 +0500

    Version from class

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
+using ObjectPrinting;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private Dictionary<Type, bool> finalTypes = new Dictionary<Type, bool>();
+
+        public PrintingConfig()
+        {
+            finalTypes = new Dictionary<Type, bool>()
+            {
+                {typeof(int) , true}, {typeof(double) , true},
+                {typeof(float) , true}, {typeof(string) , true},
+                {typeof(DateTime) , true}, {typeof(TimeSpan) , true}
+            };
+        }
+        
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -16,13 +33,10 @@ namespace ObjectPrinting
             //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
-
-            var finalTypes = new[]
-            {
-                typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
-            };
-            if (finalTypes.Contains(obj.GetType()))
+            if (excludedTypes.Contains(obj.GetType()))
+                return string.Empty;
+            
+            if (finalTypes.ContainsKey(obj.GetType()))
                 return obj + Environment.NewLine;
 
             var identation = new string('\t', nestingLevel + 1);
@@ -37,5 +51,69 @@ namespace ObjectPrinting
             }
             return sb.ToString();
         }
+        
+        private  HashSet<Type> excludedTypes = new HashSet<Type>();
+        
+        public PrintingConfig<TOwner> Excluding<T>()
+        {
+            //finalTypes.Remove(typeof(T));
+            excludedTypes.Add(typeof(T));
+            return this;
+        }
+
+        public PropertySerilizingConfig<TOwner, T> WithSerilizing<T>()
+        {
+            // ...
+            return new PropertySerilizingConfig<TOwner, T>(this); 
+        }
+        
+        public PropertySerilizingConfig<TOwner, T> WithSerilizing<T>(Expression<Func<TOwner, T>> func)
+        {
+            // ...
+            return new PropertySerilizingConfig<TOwner, T>(this);
+        }
+        
+        public PrintingConfig<TOwner> Excluding<T>(Expression<Func<TOwner, T>> func)
+        {
+            // ...
+            return this;
+        }
     }
+
+    public class PropertySerilizingConfig<TOwner, T> : IPropertySerializingConfig<TOwner>
+    {
+        private PrintingConfig<TOwner> parentConfig;
+        
+        public PropertySerilizingConfig(PrintingConfig<TOwner> parentConfig)
+        {
+            this.parentConfig = parentConfig;
+        }
+        
+        public PrintingConfig<TOwner> Using<T>(Func<T, string> func)
+        {
+            // ...
+            return parentConfig; 
+        }
+
+        PrintingConfig<TOwner> IPropertySerializingConfig<TOwner>.ParentConfig => parentConfig;
+    }
+}
+
+public static class PropertySerializingConfigExtension 
+{
+    public static PrintingConfig<TOwner> Using<TOwner>(this PropertySerilizingConfig<TOwner, int> config, CultureInfo cultureInfo)
+    {
+        // ...
+        return (config as IPropertySerializingConfig<TOwner>).ParentConfig; 
+    }
+    
+    public static PrintingConfig<TOwner> Trim<TOwner>(this PropertySerilizingConfig<TOwner, string> config, int index)
+    {
+        // ...
+        return (config as IPropertySerializingConfig<TOwner>).ParentConfig; 
+    }
+}
+
+public interface IPropertySerializingConfig<TOwner> {
+    PrintingConfig<TOwner> ParentConfig { get;  }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -10,8 +12,20 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
+            var printer = ObjectPrinter.For<Person>()
+                .Excluding<string>();
+                
+                
+//                .WithSerilizing<string>().Using<string>(x => String.Format("{0} : {1}", x, x.Length))
+//                .WithSerilizing<int>().Using(CultureInfo.CurrentCulture)
+//                .WithSerilizing(x => x.Name).Using<string>(x => String.Format("{0} : {1}", x, x.Length))
+//                .WithSerilizing(x => x.Name).Trim(5)
+//                .Excluding(x => x.Name);
+                
+                //.Serializing<DateTime>().Using(d => d.toString());
+                // Serializing(x => x.Name).Using();
+                ;
+            //1. Исключить из сериализации свойства определенного типа
                 //2. Указать альтернативный способ сериализации для определенного типа
                 //3. Для числовых типов указать культуру
                 //4. Настроить сериализацию конкретного свойства
@@ -19,7 +33,8 @@ namespace ObjectPrinting.Tests
                 //6. Исключить из сериализации конкретного свойства
             
             string s1 = printer.PrintToString(person);
-
+            
+            Console.WriteLine();
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
         }
