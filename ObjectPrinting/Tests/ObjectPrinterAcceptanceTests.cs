c6ea6d03fd8388eea6564a222cea65c17e91e2c4
vaseQ-NotAllow <hayvasahay@gmail.com>
2019-11-24 20:10:24 +0500

    Copy from solved

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,6 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
-namespace ObjectPrinting.Tests
+namespace ObjectPrinting.Solved.Tests
 {
     [TestFixture]
     public class ObjectPrinterAcceptanceTests
@@ -10,18 +12,29 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Excluding<Guid>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .Printing<int>().Using(i => i.ToString("X"))
                 //3. Для числовых типов указать культуру
+                .Printing<double>().Using(CultureInfo.InvariantCulture)
                 //4. Настроить сериализацию конкретного свойства
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Printing(p => p.Name).TrimmedToLength(10)
                 //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+                .Excluding(p => p.Age);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            string s1 = printer.PrintToString(person);
+            
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+            string s2 = person.PrintToString();
+            
             //8. ...с конфигурированием
+            string s3 = person.PrintToString(s => s.Excluding(p => p.Age));
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
         }
     }
 }
