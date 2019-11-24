a607ffb1e07011a6707c58ca54d565e59d3e35bc
Данил Панков <mistertimechip@mail.ru>
2019-11-24 13:28:04 +0300

    Added tests for serializations

--- ObjectPrinting/Extensions/PropertySerializationConfigExtensions.cs
+++ ObjectPrinting/Extensions/PropertySerializationConfigExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 using ObjectPrinting.Configs;
 using ObjectPrinting.Configs.ConfigInterfaces;
 
@@ -7,17 +10,29 @@ namespace ObjectPrinting.Extensions
     public static class PropertySerializationConfigExtensions
     {
         public static PrintingConfig<TOwner> Using<TOwner>(
-            this PropertySerializationConfig<TOwner, int> pc, 
+            this PropertySerializationConfig<TOwner, double> propertySerializationConfig, 
             CultureInfo culture)
         {
-            return (pc as IPropertySerializationConfig<TOwner>).ParentConfig;
+            var castedPropertySerialization = propertySerializationConfig as IPropertySerializationConfig<TOwner>;
+            var propertyInfo = castedPropertySerialization.PropertyInfo;
+            var parentConfig = castedPropertySerialization.ParentConfig;
+            var casted = (IPrintingConfig<TOwner>) parentConfig;
+            Func<object, string> serialization = o => Convert.ToString((double) o, culture);
+            if (propertyInfo != null) return casted.AddPropertySerialization(propertyInfo, serialization);
+            return casted.AddTypeSerialization(typeof(double), serialization);
         }
 
         public static PrintingConfig<TOwner> Take<TOwner>(
-            this PropertySerializationConfig<TOwner, string> pc,
+            this PropertySerializationConfig<TOwner, string> propertySerializationConfig,
             int count)
         {
-            return (pc as IPropertySerializationConfig<TOwner>).ParentConfig;
+            var castedPropertySerialization = propertySerializationConfig as IPropertySerializationConfig<TOwner>;
+            var propertyInfo = castedPropertySerialization.PropertyInfo;
+            var parentConfig = castedPropertySerialization.ParentConfig;
+            var casted = (IPrintingConfig<TOwner>) parentConfig;
+            Func<object, string> serialization = o => o.ToString().Substring(0, count);
+            if (propertyInfo != null) return casted.AddPropertySerialization(propertyInfo, serialization);
+            return casted.AddTypeSerialization(typeof(double), serialization);
         }
     }
 }
---
+++ ObjectPrinting/Tests/PropertySerializationConfigTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
+using ObjectPrinting.Extensions;
+
+namespace ObjectPrinting.Tests
+{
+    [TestFixture]
+    public class PropertySerializationConfigTests
+    {
+        private Person person = new Person();
+
+        [SetUp]
+        public void SetUp()
+        {
+            person.Id = Guid.NewGuid();
+            person.Name = "Gordon";
+            person.Height = 178;
+            person.Age = 27;
+        }
+
+        [Test]
+        public void Using_Should_UseAlternativeSerialization()
+        {
+            var printer = ObjectPrinter
+                .For<Person>()
+                .Serialize<string>()
+                .Using(s => "This is " + s);
+            var result = printer.PrintToString(person);
+
+            result.Should().Contain("This is Gordon");
+        }
+        
+        [Test]
+        public void Using_Should_UseCultureInfoWithDouble()
+        {
+            var printer = ObjectPrinter
+                .For<Person>()
+                .Serialize<double>()
+                .Using(CultureInfo.CreateSpecificCulture("en-US"));
+            person.Height = 202667.4;
+            var result = printer.PrintToString(person);
+
+            result.Should().Contain("202667.4");
+        }
+        
+        [Test]
+        public void Using_Should_SerializeCertainProperties()
+        {
+            var printer = ObjectPrinter
+                .For<Person>()
+                .Serialize(p => p.Name)
+                .Using(name => "Hello, my name is " + name);
+            var result = printer.PrintToString(person);
+
+            result.Should().Contain("Hello, my name is Gordon");
+        }
+        
+        [Test]
+        public void Using_Should_SerializeCertainStringPropertiesUsingStringSpecificMethods()
+        {
+            var printer = ObjectPrinter
+                .For<Person>()
+                .Serialize(p => p.Name)
+                .Take(3);
+            var result = printer.PrintToString(person);
+
+            result.Should().Contain("Gor");
+            result.Should().NotContain("Gordon");
+        }
+    }
+}
