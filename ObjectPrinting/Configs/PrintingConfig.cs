db02012ffbe600c683e8e8a74a88fbfcc87ec9d6
Данил Панков <mistertimechip@mail.ru>
2019-11-24 13:28:41 +0300

    Wrote realisations using tdd

--- ObjectPrinting/Configs/PrintingConfig.cs
+++ ObjectPrinting/Configs/PrintingConfig.cs
@@ -12,27 +12,64 @@ namespace ObjectPrinting.Configs
     {
         private readonly HashSet<Type> excludedTypes;
         private readonly HashSet<PropertyInfo> excludedProperties;
-        private readonly Dictionary<Type, Func<object, string>> alternativeSerializations;
+        private readonly Dictionary<Type, Queue<Func<object, string>>> typesSerializations;
+        private readonly Dictionary<PropertyInfo, Queue<Func<object, string>>> propertiesSerializations;
+        PrintingConfig<TOwner> IPrintingConfig<TOwner>.AddExcludedType(Type type)
+        {
+            var newExcludingTypes = new HashSet<Type>(excludedTypes) {type};
+            return new PrintingConfig<TOwner>(newExcludingTypes, excludedProperties, typesSerializations, propertiesSerializations);
+        }
 
-        HashSet<Type> IPrintingConfig<TOwner>.ExcludedTypes => excludedTypes;
-        HashSet<PropertyInfo> IPrintingConfig<TOwner>.ExcludedProperties => excludedProperties;
-        Dictionary<Type, Func<object, string>> IPrintingConfig<TOwner>.AlternativeSerializations => alternativeSerializations;
+        PrintingConfig<TOwner> IPrintingConfig<TOwner>.AddTypeSerialization(Type type, Func<object, string> serialization)
+        {
+            var newSerializations = CopySerializations(typesSerializations);
+            if(!newSerializations.ContainsKey(type))
+                newSerializations[type] = new Queue<Func<object, string>>();
+            newSerializations[type].Enqueue(obj => serialization(obj));
+            return new PrintingConfig<TOwner>(
+                excludedTypes,
+                excludedProperties,
+                newSerializations,
+                propertiesSerializations);
+        }
+
+        PrintingConfig<TOwner> IPrintingConfig<TOwner>.AddPropertySerialization(PropertyInfo propertyInfo, Func<object, string> serialization)
+        {
+            var newSerializations = CopySerializations(propertiesSerializations);
+            if(!newSerializations.ContainsKey(propertyInfo))
+                newSerializations[propertyInfo] = new Queue<Func<object, string>>();
+            newSerializations[propertyInfo].Enqueue(obj => serialization(obj));
+            return new PrintingConfig<TOwner>(
+                excludedTypes,
+                excludedProperties,
+                typesSerializations,
+                newSerializations);
+        }
+
+        PrintingConfig<TOwner> IPrintingConfig<TOwner>.AddExcludedProperty(PropertyInfo propertyInfo)
+        {
+            var newExcludingProperties = new HashSet<PropertyInfo>(excludedProperties) {propertyInfo};
+            return new PrintingConfig<TOwner>(excludedTypes, newExcludingProperties, typesSerializations, propertiesSerializations);
+        }
 
         public PrintingConfig()
         {
             excludedTypes = new HashSet<Type>();
             excludedProperties = new HashSet<PropertyInfo>();
-            alternativeSerializations = new Dictionary<Type, Func<object, string>>();
+            typesSerializations = new Dictionary<Type, Queue<Func<object, string>>>();
+            propertiesSerializations = new Dictionary<PropertyInfo, Queue<Func<object, string>>>();
         }
         
         public PrintingConfig(
             HashSet<Type> excludedTypes,
             HashSet<PropertyInfo> excludedProperties,
-            Dictionary<Type, Func<object, string>> alternativeSerializations)
+            Dictionary<Type, Queue<Func<object, string>>> typesSerializations,
+            Dictionary<PropertyInfo, Queue<Func<object, string>>> propertiesSerializations)
         {
             this.excludedTypes = new HashSet<Type>(excludedTypes);
             this.excludedProperties = new HashSet<PropertyInfo>(excludedProperties);
-            this.alternativeSerializations = new Dictionary<Type, Func<object, string>>(alternativeSerializations);
+            this.typesSerializations = CopySerializations(typesSerializations);
+            this.propertiesSerializations = propertiesSerializations;
         }
 
         public string PrintToString(TOwner obj)
@@ -52,8 +89,10 @@ namespace ObjectPrinting.Configs
             };
             if (excludedTypes.Contains(obj.GetType()))
                 return string.Empty;
-            if (alternativeSerializations.ContainsKey(obj.GetType()))
-                return alternativeSerializations[obj.GetType()](obj) + Environment.NewLine;
+            if (typesSerializations.ContainsKey(obj.GetType()) &&
+                typesSerializations[obj.GetType()].Count != 0)
+                return ApplySerializationChain(obj, typesSerializations[obj.GetType()]);
+                
             if (finalTypes.Contains(obj.GetType()))
                 return obj + Environment.NewLine;
 
@@ -65,6 +104,13 @@ namespace ObjectPrinting.Configs
             {
                 if(excludedTypes.Contains(propertyInfo.PropertyType)) continue;
                 if(excludedProperties.Contains(propertyInfo)) continue;
+                if (propertiesSerializations.ContainsKey(propertyInfo) &&
+                    propertiesSerializations[propertyInfo].Count != 0)
+                {
+                    var serialized = ApplySerializationChain(propertyInfo.GetValue(obj), propertiesSerializations[propertyInfo]);
+                    sb.Append(identation + propertyInfo.Name + " = " + serialized);
+                    continue;
+                }
                 sb.Append(identation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
@@ -74,16 +120,16 @@ namespace ObjectPrinting.Configs
 
         public PrintingConfig<TOwner> Excluding<T>()
         {
-            var newExcludingTypes = new HashSet<Type>(excludedTypes) {typeof(T)};
-            return new PrintingConfig<TOwner>(newExcludingTypes, excludedProperties, alternativeSerializations);
+            var casted = (IPrintingConfig<TOwner>) this;
+            return casted.AddExcludedType(typeof(T));
         }
         
         public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> propertyDelegate)
         {
             var propertyInfo = ((MemberExpression) propertyDelegate.Body).Member as PropertyInfo;
             if(propertyInfo == null) throw new ArgumentException();
-            var newExcludingProperties = new HashSet<PropertyInfo>(excludedProperties) {propertyInfo};
-            return new PrintingConfig<TOwner>(excludedTypes, newExcludingProperties, alternativeSerializations);
+            var casted = (IPrintingConfig<TOwner>) this;
+            return casted.AddExcludedProperty(propertyInfo);
         }
 
         public PropertySerializationConfig<TOwner, TPropType> Serialize<TPropType>()
@@ -91,9 +137,31 @@ namespace ObjectPrinting.Configs
             return new PropertySerializationConfig<TOwner, TPropType>(this);
         }
         
-        public PropertySerializationConfig<TOwner, TPropType> Serialize<TPropType>(Func<TOwner, TPropType> propertyDelegate)
+        public PropertySerializationConfig<TOwner, TPropType> Serialize<TPropType>(Expression<Func<TOwner, TPropType>> propertyDelegate)
         {
-            return new PropertySerializationConfig<TOwner, TPropType>(this);
+            var propertyInfo = ((MemberExpression) propertyDelegate.Body).Member as PropertyInfo;
+            if(propertyInfo == null) throw new ArgumentException();
+            return new PropertySerializationConfig<TOwner, TPropType>(this, propertyInfo);
+        }
+
+        private Dictionary<T, Queue<Func<object, string>>> CopySerializations<T>(
+            Dictionary<T, Queue<Func<object, string>>> serializations)
+        {
+            var newDict = new Dictionary<T, Queue<Func<object, string>>>();
+            foreach (var pair in serializations)
+                newDict[pair.Key] = new Queue<Func<object, string>>(serializations[pair.Key]);
+            return newDict;
+        }
+
+        private string ApplySerializationChain(object obj, Queue<Func<object, string>> serializationChain)
+        {
+            var result = serializationChain.Dequeue()(obj);
+            while (serializationChain.Count != 0)
+            {
+                var serialization = serializationChain.Dequeue();
+                result = serialization(result);
+            }
+            return result + Environment.NewLine;
         }
     }
 }
--- ObjectPrinting/Configs/PropertySerializationConfig.cs
+++ ObjectPrinting/Configs/PropertySerializationConfig.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-using System.Linq.Expressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using ObjectPrinting.Configs.ConfigInterfaces;
 
 namespace ObjectPrinting.Configs
@@ -7,17 +9,29 @@ namespace ObjectPrinting.Configs
     public class PropertySerializationConfig<TOwner, TPropType>
         : IPropertySerializationConfig<TOwner>
     {
-        private PrintingConfig<TOwner> parentConfig;
+        private readonly PrintingConfig<TOwner> parentConfig;
+        private readonly PropertyInfo propertyInfo;
         
         public PropertySerializationConfig(PrintingConfig<TOwner> parentConfig)
         {
             this.parentConfig = parentConfig;
-        }
-        public PrintingConfig<TOwner> Using(Expression<Func<TPropType, string>> serializationFunc)
-        {
-            return parentConfig;
+            propertyInfo = null;
         }
         
+        public PropertySerializationConfig(PrintingConfig<TOwner> parentConfig, PropertyInfo propertyInfo)
+        {
+            this.parentConfig = parentConfig;
+            this.propertyInfo = propertyInfo;
+        }
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializationFunc)
+        {
+            var casted = (IPrintingConfig<TOwner>) parentConfig;
+            if (propertyInfo != null) return casted.AddPropertySerialization(propertyInfo, o => serializationFunc((TPropType)o));
+            return casted.AddTypeSerialization(typeof(TPropType), o => serializationFunc((TPropType)o));
+        }
+
         PrintingConfig<TOwner> IPropertySerializationConfig<TOwner>.ParentConfig => parentConfig;
+        PropertyInfo IPropertySerializationConfig<TOwner>.PropertyInfo => propertyInfo;
+
     }
 }
