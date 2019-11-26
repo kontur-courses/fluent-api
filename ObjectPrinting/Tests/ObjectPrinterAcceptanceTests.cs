92f3c4b30db0a1e5a767ce90fd9361388593e893
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-27 01:36:51 +0500

    Добавлены тесты

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,20 +10,32 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person {Name = "Alex", Age = 19, Height = 1213.123};
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+            var printer = ObjectPrinter.For<Person>()
+                // 1. Исключить из сериализации свойства определенного типа 
+                .Excluding<Guid>()
+                // 2. Указать альтернативный способ сериализации для определенного типа
+                .Printing<int>().Using(i => $"{i} years old")
+                // . Для числовых типов указать культуру
+                .Printing<double>().Using(CultureInfo.GetCultureInfo("de-de"))
+                // 4. Настроить сериализацию конкретного свойства
+                .Printing(p => p.Height).Using(d => $"{d} см.")
+                // 5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Printing(p => p.Name).TrimmedToLength(20)
+                // 6. Исключить из сериализации конкретного свойства
+                .Excluding(p => p.Age);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
+            var s1 = printer.PrintToString(person);
+
+            // 7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+            var s2 = person.PrintToString();
+
+            // 8. ...с конфигурированием
+            var s3 = person.PrintToString(s => s.Excluding(p => p.Age));
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
         }
     }
 }
---
+++ ObjectPrinting/Tests/ObjectPrinterTests.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
+
+
+namespace ObjectPrinting.Tests
+{
+    [TestFixture]
+    class ObjectPrinterTests
+    {
+        private readonly Person person = new Person {Name = "Alex", Age = 19, Height = 1.2};
+
+        [Test]
+        public void Excluding_ShouldExcludeType()
+        {
+            ObjectPrinter.For<Person>()
+                .Excluding<Guid>()
+                .PrintToString(person)
+                .Should()
+                .NotContain("Guid =");
+        }
+
+        [Test]
+        public void Excluding_ShouldExcludeProperty()
+        {
+            ObjectPrinter.For<Person>()
+                .Excluding(p => p.Age)
+                .PrintToString(person)
+                .Should()
+                .NotContain("Age");
+        }
+
+        [Test]
+        public void Using_ShouldSetFormattingMethodForType()
+        {
+            ObjectPrinter.For<Person>()
+                .Printing<int>()
+                .Using(i => $"foo {i} bar")
+                .PrintToString(person)
+                .Should()
+                .Contain("foo 19 bar");
+        }
+
+        [Test]
+        public void Using_ShouldSetFormattingMethodForProperty()
+        {
+            ObjectPrinter.For<Person>()
+                .Printing(p => p.Age)
+                .Using(i => $"foo {i} bar")
+                .PrintToString(person)
+                .Should()
+                .Contain("foo 19 bar");
+        }
+
+        [Test]
+        public void TrimmedToLength_ShouldTrimProperty_WhenMaxLengthLessStringLength()
+        {
+            var result = ObjectPrinter.For<Person>()
+                .Printing(p => p.Name)
+                .TrimmedToLength(2)
+                .PrintToString(person);
+
+            result.Should()
+                .NotContain("Alex");
+
+            result.Should().Contain("Al");
+        }
+
+        [Test]
+        public void TrimmedToLength_ShouldNotTrimProperty_WhenMaxLengthGreaterOrEqualStringLength()
+        {
+            var result = ObjectPrinter.For<Person>()
+                .Printing(p => p.Name)
+                .TrimmedToLength(5)
+                .PrintToString(person);
+
+            result.Should()
+                .Contain("Alex");
+        }
+
+        [Test]
+        public void Using_ShouldUseGivenCulture_WhenSelectedNumber()
+        {
+            ObjectPrinter.For<Person>()
+                .Printing<double>()
+                .Using(new CultureInfo("en-us"))
+                .PrintToString(person)
+                .Should()
+                .Contain("1.2");
+        }
+
+        [Test]
+        public void PrintToString_ShouldFormatCorrectly_WhenFinalType()
+        {
+            42.PrintToString()
+                .Should()
+                .BeEquivalentTo("42");
+        }
+
+        [Test]
+        public void PrintToString_ShouldFormatCorrectly_WhenComplexEntity()
+        {
+            person.PrintToString()
+                .Should()
+                .BeEquivalentTo(
+                    $"Person{Environment.NewLine}\t" +
+                    $"Id = Guid{Environment.NewLine}\t" +
+                    $"Name = Alex{Environment.NewLine}\t" +
+                    $"Height = 1,2{Environment.NewLine}\t" +
+                    $"Age = 19{Environment.NewLine}");
+        }
+    }
+}
