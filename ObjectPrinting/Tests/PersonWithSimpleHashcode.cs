181520366b88451ff61b10917fa49f8c29ba3799
Данил Панков <mistertimechip@mail.ru>
2019-11-26 22:36:46 +0300

    Added class for hashcode testing

---
+++ ObjectPrinting/Tests/PersonWithSimpleHashcode.cs
@@ -0,0 +1,19 @@
+﻿namespace ObjectPrinting.Tests
+{
+    public class PersonWithSimpleHashCode
+    {
+        public string Name { get; set; }
+        public PersonWithSimpleHashCode Friend { get; set; }
+
+        public override int GetHashCode() => Name?.GetHashCode() ?? 0;
+
+        protected bool Equals(PersonWithSimpleHashCode other) => Name == other.Name;
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == this.GetType() && Equals((PersonWithSimpleHashCode) obj);
+        }
+    }
+}
