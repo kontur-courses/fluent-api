1c1000064d9a60d71d1a5f14276e213ec8d541cc
Zakepanka <arzybek@users.noreply.github.com>
2019-11-27 03:08:45 +0500

    Added Circle, IEnumerable

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,8 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using ObjectPrinting;
@@ -17,7 +19,7 @@ namespace ObjectPrinting
         private Dictionary<Type, CultureInfo> cultureInfos = new Dictionary<Type, CultureInfo>();
         private Dictionary<string, Delegate> propertyFuncs = new Dictionary<string, Delegate>();
         private HashSet<string> excludedProperties = new HashSet<string>();
-        
+        private Stack<object> nestingStack = new Stack<object>();
         
         public void SetFuncFor(Type type, Delegate pr)
         {
@@ -44,44 +46,75 @@ namespace ObjectPrinting
                 typeof(DateTime), typeof(TimeSpan)
             };
             if (finalTypes.Contains(obj.GetType()))
-                return obj.ToString() + Environment.NewLine;
+                return obj + Environment.NewLine;
             
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
+            if(nestingLevel==0)
+                nestingStack.Push(obj);
             foreach (var propertyInfo in type.GetProperties())
             {
-                string str = "";
-                if (typeFuncs.ContainsKey(propertyInfo.PropertyType))
-                {
-                    str = typeFuncs[propertyInfo.PropertyType].DynamicInvoke(propertyInfo.GetValue(obj)) + Environment.NewLine;
-                }
-                if (propertyFuncs.ContainsKey(propertyInfo.Name))
-                {
-                    var ci = propertyFuncs[propertyInfo.Name];
-                    str = ci.DynamicInvoke(propertyInfo.GetValue(obj)).ToString() + Environment.NewLine;
-                }
-                if (cultureInfos.ContainsKey(propertyInfo.PropertyType))
-                {
-                    var ci = cultureInfos[propertyInfo.PropertyType];
-                    str = ((IFormattable)propertyInfo.GetValue(obj)).ToString( null, ci) + Environment.NewLine;
-                }
-
                 if (excludedTypes.Contains(propertyInfo.PropertyType) || excludedProperties.Contains(propertyInfo.Name))
-                {
-                    str = string.Empty;
-                    sb.Append(str);
                     continue;
+                string str;
+
+                if (nestingStack.Contains(propertyInfo.GetValue(obj)))
+                {
+                    sb.Append(identation + propertyInfo.Name + " = " +
+                              "circle reference");
+                    continue; 
                 }
 
-                if (str == "")
+                if (obj is IEnumerable)
+                {
+                    var sbList = new StringBuilder();
+                    foreach (var item in (IEnumerable)obj)
+                    {
+                        sbList.Append(PrintToString(item, nestingLevel + 1));
+                    }
+                    sb.Append(sbList);
+                    break;
+                }
+
+                if (!TryGetString(propertyInfo, out str, obj))
+                {
                     str = PrintToString(propertyInfo.GetValue(obj),
                         nestingLevel + 1);
+                }
                 sb.Append(identation + propertyInfo.Name + " = " +
                           str);
+                nestingStack.Push(propertyInfo.GetValue(obj));
             }
+            
+            if(nestingStack.Count != 0)
+                nestingStack.Pop();
             return sb.ToString();
+        }
+
+        private bool TryGetString(PropertyInfo propertyInfo, out string str, object obj)
+        {
+            str = "";
+            if (propertyFuncs.ContainsKey(propertyInfo.Name))
+            {
+                var pf = propertyFuncs[propertyInfo.Name];
+                str = pf.DynamicInvoke(propertyInfo.GetValue(obj)) + Environment.NewLine;
+                return true;
+            }
+            if (typeFuncs.ContainsKey(propertyInfo.PropertyType))
+            {
+                str = typeFuncs[propertyInfo.PropertyType].DynamicInvoke(propertyInfo.GetValue(obj)) + Environment.NewLine;
+                return true;
+            }
+            if (cultureInfos.ContainsKey(propertyInfo.PropertyType))
+            {
+                var ci = cultureInfos[propertyInfo.PropertyType];
+                str = ((IFormattable)propertyInfo.GetValue(obj)).ToString( null, ci) + Environment.NewLine;
+                return true;
+            }
+
+            return false;
         }
 
         public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
@@ -100,7 +133,7 @@ namespace ObjectPrinting
         public void SetFuncForProperty<TPropType>(string name, Func<TPropType, string> compile)
         {
             propertyFuncs[name] = compile;
-            this.lastProperty = null;
+            lastProperty = null;
         }
 
         public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
+    class TestClass
+    {
+        public int number { get; set; }
+        public TestClass Parent { get; set; }
+    }
+
     [TestFixture]
     public class ObjectPrinterAcceptanceTests
     {
@@ -44,11 +51,14 @@ namespace ObjectPrinting.Tests
         [Test]
         public void ObjPrinter_UsingCultureInfoForType_ShouldNotThrowExceptions()
         {
-            var printer = ObjectPrinter.For<Person>()
-                .Printing<double>().Using(CultureInfo.GetCultureInfo("de-DE"))
-                .Printing<int>().Using(CultureInfo.GetCultureInfo("de-DE"));
-            string s1 = printer.PrintToString(person);
+            var printer1 = ObjectPrinter.For<Person>()
+                .Printing<double>().Using(CultureInfo.GetCultureInfo("en-US"));
+            var printer2 = ObjectPrinter.For<Person>()
+                    .Printing<double>().Using(CultureInfo.GetCultureInfo("de-DE"));
+            string s1 = printer1.PrintToString(person);
+            string s2 = printer2.PrintToString(person);
             Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
         
         [Test]
@@ -95,31 +105,51 @@ namespace ObjectPrinting.Tests
         }
         
         [Test]
+        public void ObjPrinter_IEnumerable_ShouldNotThrowExceptions()
+        {
+            var printer = ObjectPrinter.For<List<Person>>();
+            var lp = new List<Person> {person, person};
+            string s1 = printer.PrintToString(lp);
+            
+            Console.WriteLine(s1);
+        }
+        
+        [Test]
+        public void ObjPrinter_CircleReference_ShouldNotThrowExceptions()
+        {
+            var printer = ObjectPrinter.For<TestClass>();
+            var first = new TestClass(){ number = 1, Parent = null };
+            var second = new TestClass(){ number = 2, Parent = first };
+            first.Parent = second;
+            
+            string s1 = printer.PrintToString(first);
+            
+            Console.WriteLine(s1);
+        }
+        
+        [Test]
         public void AcceptanceTests()
         {
-            var printer = ObjectPrinter.For<Person>();
-//                .Printing<double>().Using(x => String.Format("{0} : {1}", x, x))
-//                .Printing<double>().Using(CultureInfo.GetCultureInfo("de-DE"))
+            var printer = ObjectPrinter.For<Person>()
+                .Printing<double>().Using(x => String.Format("{0} : {1}", x, x))
+                .Printing<double>().Using(CultureInfo.GetCultureInfo("de-DE"));
 //                .Printing<int>().Using(CultureInfo.GetCultureInfo("de-DE"))
 //                 .Printing(x => x.Name).Using(x => String.Format("{0} : {1}", x, x.Length+"ababa"));
 //                 .Printing(x => x.Name).TrimmedToLength(3)
 //                .Excluding(x => x.Name);
-                
-                //.Serializing<DateTime>().Using(d => d.toString());
-                // Serializing(x => x.Name).Using();
-            
+
             //1. Исключить из сериализации свойства определенного типа
                 //2. Указать альтернативный способ сериализации для определенного типа
                 //3. Для числовых типов указать культуру
                 //4. Настроить сериализацию конкретного свойства
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
                 //6. Исключить из сериализации конкретного свойства
-            
+                //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+                //8. ...с конфигурированием
+                
             string s1 = printer.PrintToString(person);
             
             Console.WriteLine(s1);
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
         }
     }
 }
