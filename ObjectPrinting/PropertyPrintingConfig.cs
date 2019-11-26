bf4185b3be32aa6446567662c74c7bd1498b0296
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-27 01:35:49 +0500

    Добавлен PropertyPrintingConfig и интерфейс для него реализующий логику Using

---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
+    {
+        private readonly PrintingConfig<TOwner> printingConfig;
+        private readonly PropertyInfo propertyInfo;
+
+        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig)
+        {
+            this.printingConfig = printingConfig;
+        }
+
+        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig, PropertyInfo propertyInfo)
+        {
+            this.printingConfig = printingConfig;
+            this.propertyInfo = propertyInfo;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
+        {
+            if (propertyInfo == null)
+                ((IPrintingConfig) printingConfig).TypePrintingMethod[typeof(TPropType)] = print;
+            else
+                ((IPrintingConfig) printingConfig).PropertyPrintingMethod[propertyInfo] = print;
+            return printingConfig;
+        }
+
+        PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => printingConfig;
+        PropertyInfo IPropertyPrintingConfig<TOwner, TPropType>.PropertyInfo => propertyInfo;
+    }
+}
