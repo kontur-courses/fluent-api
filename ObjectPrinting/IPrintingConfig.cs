9777e7124a5e82d9fae5defe52980b06a9f4ef14
lgnv <43182296+lgnv@users.noreply.github.com>
2019-11-27 01:36:06 +0500

    Добавлен интерфейс для PrintingConfig

---
+++ ObjectPrinting/IPrintingConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    interface IPrintingConfig
+    {
+        Dictionary<Type, Delegate> TypePrintingMethod { get; }
+        Dictionary<PropertyInfo, Delegate> PropertyPrintingMethod { get; }
+        Dictionary<Type, CultureInfo> CultureForNumber { get; }
+    }
+}
