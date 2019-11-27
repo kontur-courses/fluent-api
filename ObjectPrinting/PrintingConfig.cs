ff7c26c3419eec64a9414b2c5cdfa6c120c063c2
shemloft <shem.dar99@gmail.com>
2019-11-27 16:25:06 +0500

    Рефакторинг основного метода, реализация Exclude

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,12 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using FluentAssertions.Common;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private readonly HashSet<Type> excludedTypes = new HashSet<Type>();
+        private readonly HashSet<PropertyInfo> excludedProperties = new HashSet<PropertyInfo>();
+
         public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
         {
             return new PropertyPrintingConfig<TOwner, TPropType>(this);
@@ -19,11 +25,13 @@ namespace ObjectPrinting
 
         public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
         {
+            excludedProperties.Add(memberSelector.GetPropertyInfo());
             return this;
         }
 
         internal PrintingConfig<TOwner> Excluding<TPropType>()
         {
+            excludedTypes.Add(typeof(TPropType));
             return this;
         }
 
@@ -34,24 +42,36 @@ namespace ObjectPrinting
 
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
+            return TryToPrintType(obj) ?? PrintProperties(obj, nestingLevel);
+        }
 
+        private string TryToPrintType(object obj)
+        {
+            var type = obj.GetType();
+            if (excludedTypes.Contains(type))
+                return "";
             var finalTypes = new[]
             {
                 typeof(int), typeof(double), typeof(float), typeof(string),
                 typeof(DateTime), typeof(TimeSpan)
             };
-            if (finalTypes.Contains(obj.GetType()))
+            if (finalTypes.Contains(type))
                 return obj + Environment.NewLine;
+            return null;
+        }
 
+        private string PrintProperties(object obj, int nestingLevel)
+        {
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
+                if (excludedProperties.Contains(propertyInfo))
+                    continue;
                 sb.Append(identation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
---
+++ ObjectPrinting/Tests/ObjectPrinterTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ObjectPrinting.Tests
+{
+    public class ObjectPrinterTests
+    {
+        private Person person;
+
+        [SetUp]
+        public void SetUp()
+        {
+            person = new Person { Age = 20, Height = 185.5, Name = "Alex" };
+        }
+
+        [Test]
+        public void Printer_ShouldPrintEverything_WithoutConfiguration()
+        {
+            var printer = ObjectPrinter.For<Person>();
+            var result = printer.PrintToString(person);
+            result.Should().Contain("185,5").And.Contain("Alex").And.Contain("20");
+        }
+
+        [Test]
+        public void Excluding_ShouldExcludeType_OnInt()
+        {
+            var printer = ObjectPrinter.For<Person>().Excluding<int>();
+            var result = printer.PrintToString(person);
+            result.Should().Contain("185,5").And.Contain("Alex").And.NotContain("20");
+        }
+
+        [Test]
+        public void Excluding_ShouldExcludeProperty_OnName()
+        {
+            var printer = ObjectPrinter.For<Person>().Excluding(p => p.Name);
+            var result = printer.PrintToString(person);
+            result.Should().Contain("185,5").And.Contain("20").And.NotContain("Alex");
+        }
+    }
+}
