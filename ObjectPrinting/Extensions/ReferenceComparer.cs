b89eb58b9fb4a69bc3a3267c1a961d9ee809bec4
Данил Панков <mistertimechip@mail.ru>
2019-11-27 14:10:57 +0300

    Added comparer for reference comparing

---
+++ ObjectPrinting/Extensions/ReferenceComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ObjectPrinting.Extensions
+{
+    public class ReferenceComparer : IEqualityComparer<object>
+    {
+        public bool Equals(object x, object y)
+        {
+            return x == y;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
