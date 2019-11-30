5ece46398634268a570f67adeee6decc89b32f29
Shcherbina99 <ej99@example.com>
2019-11-30 11:50:27 +0500

    feat: add numbers support Using(CultureInfo), change bulding serFunc

--- ObjectPrinting/PropertySerializingConfig.cs
+++ ObjectPrinting/PropertySerializingConfig.cs
@@ -20,16 +20,28 @@ namespace ObjectPrinting
             this.propertyInfo = propertyInfo;
         }
 
-        public PrintingConfig<TOwner> Using(Func<object, string> serializationMethod)
+        public PrintingConfig<TOwner> Using(Func<TPropType, string> serializationMethod)
         {
             if (propertyInfo == null)
             {
-                (parentConfig as IPrintingConfig).typeSerialisation[typeof(TPropType)] = serializationMethod;
+                (parentConfig as IPrintingConfig).typeSerialisation[typeof(TPropType)] = BuildFuncSerial(serializationMethod);
                 return parentConfig;
             }
 
-            (parentConfig as IPrintingConfig).propertySerialisation[propertyInfo] = serializationMethod;
+            (parentConfig as IPrintingConfig).propertySerialisation[propertyInfo] = BuildFuncSerial(serializationMethod);
             return parentConfig;
+        }
+
+        private static Func<object, string> BuildFuncSerial(Func<TPropType, string> funcSerial)
+        {
+            return obj =>
+            {
+                if (obj == null)
+                    return funcSerial((TPropType)obj);
+                if (obj is TPropType propType)
+                    return funcSerial(propType);
+                throw new ArgumentException($"Cannot be casted to {nameof(TPropType)}");
+            };
         }
     }
 }
--- ObjectPrinting/PropertySerializingConfigExtension.cs
+++ ObjectPrinting/PropertySerializingConfigExtension.cs
@@ -9,26 +9,54 @@ namespace ObjectPrinting
         public static PrintingConfig<TOwner> Using<TOwner>(
             this PropertySerializingConfig<TOwner, double> propertySerializingConfig, CultureInfo cultureInfo)
         {
-            var printingConfig = (propertySerializingConfig as IPropertySerializingConfig<TOwner>).ParentConfig;
-            ((IPrintingConfig) printingConfig).typeSerialisation.Add(typeof(double), new Func<object, string>(x =>
-            {
-                var num = (double)x;
-                return num.ToString(cultureInfo);
-            }));
-            return printingConfig;
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner>(
+            this PropertySerializingConfig<TOwner, int> propertySerializingConfig, CultureInfo cultureInfo)
+        {
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner>(
+            this PropertySerializingConfig<TOwner, short> propertySerializingConfig, CultureInfo cultureInfo)
+        {
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner>(
+            this PropertySerializingConfig<TOwner, uint> propertySerializingConfig, CultureInfo cultureInfo)
+        {
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner>(
+            this PropertySerializingConfig<TOwner, ulong> propertySerializingConfig, CultureInfo cultureInfo)
+        {
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner>(
+            this PropertySerializingConfig<TOwner, ushort> propertySerializingConfig, CultureInfo cultureInfo)
+        {
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner>(
+            this PropertySerializingConfig<TOwner, float> propertySerializingConfig, CultureInfo cultureInfo)
+        {
+            return propertySerializingConfig.Using(d => d.ToString(cultureInfo) );
         }
 
         public static PrintingConfig<TOwner> TakeOnly<TOwner>(this PropertySerializingConfig<TOwner, string> config,
             int numberChar)
         {
-            var printingConfig = (config as IPropertySerializingConfig<TOwner>).ParentConfig;
-            (printingConfig as IPrintingConfig).typeSerialisation.Add(typeof(string), 
-                new Func<object, string>(v =>
-                {
-                    var str = (string) v;
-                    return string.IsNullOrEmpty(str) ? "" : str.Substring(0, numberChar);
-                }));
-            return printingConfig;
+            return config.Using(str =>
+            {
+                if (str == null)
+                    return "<null>";
+                return str.Length < numberChar ? str : str.Substring(0, numberChar);
+            });
         }
     }
 }
