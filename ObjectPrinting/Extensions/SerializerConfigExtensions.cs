1b558c51a1c0d8b22e1e84a18ba9f02d6dad6363
JDreyka <belous860@gmail.com>
2019-11-30 12:53:45 +0500

    Отделил SerializerConfigExtesions от SerializerConfig

---
+++ ObjectPrinting/Extensions/SerializerConfigExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace ObjectPrinting
+{
+    public static class SerializerConfigExtensions
+    {
+        public static PrintingConfig<TOwner> WithCulture<TOwner>(this SerializerConfig<TOwner, int> config, CultureInfo currentCulture)
+        {
+            (config as ISerializerConfig<TOwner>).SerializeFunc = x => ((int)x).ToString("N", currentCulture.NumberFormat);
+            return (config as ISerializerConfig<TOwner>).ParentConfig;
+        }
+        
+        public static PrintingConfig<TOwner> WithCulture<TOwner>(this SerializerConfig<TOwner, double> config, CultureInfo currentCulture)
+        {
+            (config as ISerializerConfig<TOwner>).SerializeFunc = x => ((double)x).ToString("N", currentCulture.NumberFormat);
+            return (config as ISerializerConfig<TOwner>).ParentConfig;
+        }
+        
+        public static PrintingConfig<TOwner> WithCulture<TOwner>(this SerializerConfig<TOwner, long> config, CultureInfo currentCulture)
+        {
+            (config as ISerializerConfig<TOwner>).SerializeFunc = x => ((long)x).ToString("N", currentCulture.NumberFormat);
+            return (config as ISerializerConfig<TOwner>).ParentConfig;
+        }
+        
+        public static PrintingConfig<TOwner> Cut<TOwner>(this SerializerConfig<TOwner, string> config, int countSymbols)
+        {
+            (config as ISerializerConfig<TOwner>).SerializeFunc = x => (x.ToString()).Substring(0, countSymbols);
+            return (config as ISerializerConfig<TOwner>).ParentConfig;
+        }
+    }
+}
