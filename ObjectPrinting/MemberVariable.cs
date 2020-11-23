6abee885d06a5833d58316e1d42489e0167a1b88
Denis Bautin <denis020301@mail.ru>
2020-11-24 03:23:26 +0500

    Add class MemberVariable and tests for it

---
+++ ObjectPrinting/MemberVariable.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class MemberVariable : IEquatable<MemberVariable>
+    {
+        private readonly MemberInfo memberVarInfo;
+
+        public string Name => memberVarInfo.Name;
+
+        public Type MemberVarType => memberVarInfo switch
+        {
+            PropertyInfo property => property.PropertyType,
+            FieldInfo field => field.FieldType,
+        };
+
+        public MemberVariable(PropertyInfo varInfo)
+        {
+            if (varInfo == null)
+                throw new ArgumentException("Parameter is null");
+            memberVarInfo = varInfo;
+        }
+
+        public MemberVariable(FieldInfo varInfo)
+        {
+            if (varInfo == null)
+                throw new ArgumentException("Parameter is null");
+            memberVarInfo = varInfo;
+        }
+
+        public object GetValue(object obj)
+        {
+            return memberVarInfo switch
+            {
+                PropertyInfo property => property.GetValue(obj),
+                FieldInfo field => field.GetValue(obj),
+            };
+        }
+
+        public bool Equals(MemberVariable other)
+        {
+            return memberVarInfo.Equals(other?.memberVarInfo);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is MemberVariable memberVar && Equals(memberVar);
+        }
+
+        public override int GetHashCode()
+        {
+            return memberVarInfo.GetHashCode();
+        }
+
+        public static MemberVariable[] GetMemberVariables(Type type)
+        {
+            var fieldMembers = type.GetFields()
+                .Select(info => new MemberVariable(info));
+            var propertyMembers = type.GetProperties()
+                .Select(info => new MemberVariable(info));
+
+            return fieldMembers.Concat(propertyMembers).ToArray();
+        }
+
+        public static MemberVariable FromExpression<TDeclaring, TMember>(
+            Expression<Func<TDeclaring, TMember>> memberSelector)
+        {
+            var member = ((MemberExpression) memberSelector.Body).Member;
+
+            return member switch
+            {
+                PropertyInfo propertyInfo => new MemberVariable(propertyInfo),
+                FieldInfo fieldInfo => new MemberVariable(fieldInfo),
+                _ => throw new ArgumentException("Member isn't property or field")
+            };
+        }
+    }
+}
---
+++ ObjectPrintingTests/MemberVariableTests.cs
@@ -0,0 +1,116 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ObjectPrinting;
+
+namespace ObjectPrintingTests
+{
+    [TestFixture]
+    public class MemberVariableTests
+    {
+        private TestClass testClass;
+
+        private static MemberVariable[] _memberVarsFromTestClass =
+        {
+            new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.FirstProp))),
+            new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.SecondProp))),
+            new MemberVariable(typeof(TestClass).GetField(nameof(TestClass.Field)))
+        };
+
+        [SetUp]
+        public void SetUp()
+        {
+            testClass = new TestClass();
+        }
+
+        [Test]
+        public void GetMemberVariables_ReturnOnlyPublicFieldsAndProperties()
+        {
+            var memberVars = MemberVariable.GetMemberVariables(typeof(TestClass));
+
+            memberVars.Should().BeEquivalentTo(_memberVarsFromTestClass);
+        }
+
+        [Test]
+        public void FromExpression_IfSelectField_ReturnMemberVariableForThisField()
+        {
+            var expected = new MemberVariable(typeof(TestClass).GetField(nameof(TestClass.Field)));
+            
+            MemberVariable.FromExpression((TestClass testClass) => testClass.Field).Should()
+                .BeEquivalentTo(expected);
+        }
+        
+        [Test]
+        public void FromExpression_IfSelectProperty_ReturnMemberVariableForThisProperty()
+        {
+            var expected = new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.FirstProp)));
+            
+            MemberVariable.FromExpression((TestClass testClass) => testClass.FirstProp).Should()
+                .BeEquivalentTo(expected);
+        }
+
+        [Test]
+        public void GetValue_CanReturnValueOfProperty()
+        {
+            var memberVar = new MemberVariable(
+                typeof(TestClass).GetProperty(nameof(TestClass.SecondProp)));
+
+            memberVar.GetValue(testClass).Should().Be(testClass.SecondProp);
+        }
+
+        [Test]
+        public void GetValue_CanReturnValueOfField()
+        {
+            var memberVar = new MemberVariable(
+                typeof(TestClass).GetField(nameof(TestClass.Field)));
+
+            memberVar.GetValue(testClass).Should().Be(testClass.Field);
+        }
+
+        [Test]
+        public void MemberVarType_CanReturnTypeOfProperty()
+        {
+            var memberVar = new MemberVariable(
+                typeof(TestClass).GetProperty(nameof(TestClass.SecondProp)));
+
+            memberVar.MemberVarType.Should().Be(
+                testClass.SecondProp.GetType());
+        }
+
+        [Test]
+        public void MemberVarType_CanReturnTypeOfField()
+        {
+            var memberVar = new MemberVariable(
+                typeof(TestClass).GetField(nameof(TestClass.Field)));
+
+            memberVar.MemberVarType.Should().Be(
+                testClass.Field.GetType());
+        }
+
+        [Test]
+        public void Equals_IfMemberInfoFieldsAreEqual_ReturnTrue()
+        {
+            var member = new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.FirstProp)));
+            var sameMember = new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.FirstProp)));
+
+            member.Equals(sameMember).Should().BeTrue();
+        }
+        
+        [Test]
+        public void Equals_IfMemberInfoFieldsAreNotEqual_ReturnFalse()
+        {
+            var member = new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.FirstProp)));
+            var otherMember = new MemberVariable(typeof(TestClass).GetProperty(nameof(TestClass.SecondProp)));
+
+            member.Equals(otherMember).Should().BeFalse();
+        }
+    }
+
+    public class TestClass
+    {
+        public int FirstProp { get; set; } = 1;
+        public string SecondProp { get; set; } = "two";
+
+        public double Field = 2.5;
+        private double closeField;
+    }
+}
