94af9f259a57c8b5918f17b13b43ce64137b5d3a
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-23 23:56:56 +0500

    Added tests

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,27 +1,122 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
     [TestFixture]
     public class ObjectPrinterAcceptanceTests
     {
-        [Test]
-        public void Demo()
+        [SetUp]
+        public void SetUp()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            person = new Person();
+            printer = ObjectPrinter.For<Person>();
+        }
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+        private Person person;
+        private PrintingConfig<Person> printer;
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
+        [Test]
+        public void PrintToString_PrintingAll_IfHasNoConfig()
+        {
+            person.Name = "Alex";
+            person.Age = 19;
+            person.Height = 180;
+            person.Surname = "Brown";
+            printer.PrintToString(person).Should().Be("Person\r\n\tId = Guid\r\n\tName = Alex\r\n\tHeight = 180\r\n\t" +
+                                                      "Age = 19\r\n\tFriend = null\r\n\tSurname = Brown\r\n");
+        }
+
+        [Test]
+        public void PrintToString_NotPrintingType_IfTypeExcluded()
+        {
+            printer = printer.Excluding<int>();
+            printer.PrintToString(person).Should().NotContain("Age");
+        }
+
+        [Test]
+        public void PrintToString_NotPrintingProperty_IfPropertyExcluded()
+        {
+            printer = printer.Excluding(x => x.Height);
+            printer.PrintToString(person).Should().NotContain(nameof(person.Height));
+        }
+
+        [Test]
+        public void PrintToString_NotPrintingField_IfFieldExcluded()
+        {
+            printer = printer.Excluding(x => x.Surname);
+            printer.PrintToString(person).Should().NotContain(nameof(person.Surname));
+        }
+
+        [Test]
+        public void PrintToString_PrintingWithCulture_IfCultureWasSet()
+        {
+            person.Height = 0.5;
+            printer = printer.SetCultureInfo<double>(CultureInfo.InvariantCulture);
+            printer.PrintToString(person).Should().Contain($"{nameof(person.Height)} = 0.5");
+        }
+
+        [Test]
+        public void PrintToString_PrintingPropertyByAlternateSerializing()
+        {
+            person.Name = "Alex";
+            printer = printer.SetAlternateSerialize(x => x.Name, x => x + "!!");
+            printer.PrintToString(person).Should().Contain($"{nameof(person.Name)} = Alex!!");
+        }
+
+        [Test]
+        public void PrintToString_PrintingFieldByAlternateSerializing()
+        {
+            person.Surname = "Brown";
+            printer = printer.SetAlternateSerialize(x => x.Surname, x => x + "!!");
+            printer.PrintToString(person).Should().Contain($"{nameof(person.Surname)} = Brown!!");
+        }
+
+        [Test]
+        public void PrintToString_PrintingTypeByAlternateSerializing()
+        {
+            person.Age = 18;
+            printer = printer.SetAlternateSerialize<int>(x => x + "!!!");
+            printer.PrintToString(person).Should().Contain($"{nameof(person.Age)} = 18!!!");
+        }
+
+        [Test]
+        public void PrintToString_PrintingTrimmedProperty()
+        {
+            person.Name = "Alexander";
+            printer = printer.SetTrimming(x => x.Name, 4);
+            printer.PrintToString(person).Should().Contain($"{nameof(person.Name)} = Alex");
+        }
+
+        [Test]
+        public void PrintToString_PrintingTrimmedField()
+        {
+            person.Surname = "Brown";
+            printer = printer.SetTrimming(x => x.Surname, 1);
+            printer.PrintToString(person).Should().Contain($"{nameof(person.Surname)} = B");
+        }
+
+        [Test]
+        public void PrintToString_ThrowsSerializationException_IfCircularReference()
+        {
+            person.Friend = person;
+            Action act = () => printer.PrintToString(person);
+            act.Should().Throw<SerializationException>();
+        }
+
+        [Test]
+        public void PrintToString_ThrowsSerializationException_IfCircularReferenceBetween3objects()
+        {
+            var person2 = new Person();
+            var person3 = new Person();
+            person.Friend = person2;
+            person2.Friend = person3;
+            person3.Friend = person;
+            Action act = () => printer.PrintToString(person);
+            act.Should().Throw<SerializationException>();
         }
     }
 }
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -8,5 +8,19 @@ namespace ObjectPrinting.Tests
         public string Name { get; set; }
         public double Height { get; set; }
         public int Age { get; set; }
+        public string Surname;
+        public Person Friend { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Person)) return false;
+            var otherPerson = (Person) obj;
+            return Id.Equals(otherPerson.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
     }
 }
