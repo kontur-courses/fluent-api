92d3e993cddcc0850fdd36f16d23da920d337d83
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-23 23:58:01 +0500

    Added class 'MemberInfoExtensions'

---
+++ ObjectPrinting/MemberInfoExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public static class MemberInfoExtensions
+    {
+        public static bool IsPropertyOrField(this MemberInfo memberInfo)
+        {
+            return memberInfo.MemberType == MemberTypes.Field
+                   || memberInfo.MemberType == MemberTypes.Property;
+        }
+
+        public static object GetValue(this MemberInfo memberInfo, object obj)
+        {
+            return memberInfo.MemberType switch
+            {
+                MemberTypes.Field => ((FieldInfo) memberInfo).GetValue(obj),
+                MemberTypes.Property => ((PropertyInfo) memberInfo).GetValue(obj),
+                _ => throw new ArgumentException()
+            };
+        }
+
+        public static Type GetValueType(this MemberInfo memberInfo)
+        {
+            return memberInfo.MemberType switch
+            {
+                MemberTypes.Field => ((FieldInfo) memberInfo).FieldType,
+                MemberTypes.Property => ((PropertyInfo) memberInfo).PropertyType,
+                _ => throw new ArgumentException()
+            };
+        }
+    }
+}
