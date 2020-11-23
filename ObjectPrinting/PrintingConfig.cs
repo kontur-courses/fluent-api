3e551f46df0d8314255d6a42c3bb249c9ee0e237
Ivan Grabovsky <raccoon.twitch@gmail.com>
2020-11-23 18:11:30 +0500

    initial commit

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ObjectPrinting
@@ -9,6 +11,28 @@ namespace ObjectPrinting
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
+        }
+
+        public SelectedProperty<TOwner, PropertyConfigurator<TProperty>, TProperty> Choose<TProperty>()
+        {
+            return new SelectedProperty<TOwner, PropertyConfigurator<TProperty>, TProperty>();
+        }
+
+        public SelectedProperty<TOwner, PropertyConfigurator<TProperty>, TProperty> Choose<TProperty>(
+            Expression<Func<TOwner, TProperty>> selector)
+        {
+            return new SelectedProperty<TOwner, PropertyConfigurator<TProperty>, TProperty>();
+        }
+
+        public SelectedProperty<TOwner, StringPropertyConfigurator, string> Choose(
+            Expression<Func<TOwner, string>> selector)
+        {
+            return new SelectedProperty<TOwner, StringPropertyConfigurator, string>();
+        }
+        public SelectedProperty<TOwner, NumberPropertyConfigurator, int> Choose(
+            Expression<Func<TOwner, int>> selector)
+        {
+            return new SelectedProperty<TOwner, NumberPropertyConfigurator, int>();
         }
 
         private string PrintToString(object obj, int nestingLevel)
@@ -25,17 +49,82 @@ namespace ObjectPrinting
             if (finalTypes.Contains(obj.GetType()))
                 return obj + Environment.NewLine;
 
-            var identation = new string('\t', nestingLevel + 1);
+            var indentation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
+                sb.Append(indentation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
             }
+
             return sb.ToString();
         }
+    }
+
+    public class SelectedProperty<TOwner, TConfigurator, TProperty>
+        where TConfigurator : PropertyConfigurator<TProperty>
+    {
+        public PrintingConfig<TOwner> Parent { get; }
+
+        public PrintingConfig<TOwner> Using(Action<TConfigurator> configurator)
+        {
+            return Parent;
+        }
+
+        public PrintingConfig<TOwner> Exclude()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PrintingConfig<TOwner> UseSerializer(Func<object, string> func)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PrintingConfig<TOwner> SetCulture(CultureInfo currentCulture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PrintingConfig<TOwner> Trim(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class PropertyConfigurator<TProperty>
+    {
+        public void Exclude()
+        {
+        }
+
+        public PropertyConfigurator<TProperty> UseSerializer(Func<TProperty, string> serializer)
+        {
+            return this;
+        }
+    }
+
+    public class StringPropertyConfigurator : PropertyConfigurator<string>
+    {
+        public StringPropertyConfigurator Substring()
+        {
+            return this;
+        }
+    }
+
+    public class NumberPropertyConfigurator : PropertyConfigurator<int>
+    {
+        public NumberPropertyConfigurator SetCulture(CultureInfo cultureInfo)
+        {
+            return this;
+        }
+    }
+
+    public class PersonExtensions
+    {
+        
     }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,27 +1,39 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
+using System.IO;
+using NUnit.Framework;
+using ObjectPrinting;
+using ObjectPrinting.Configuration;
+using ObjectPrinting.Tests;
 
-namespace ObjectPrinting.Tests
+namespace Tests
 {
     [TestFixture]
-    public class ObjectPrinterAcceptanceTests
+    public class AcceptanceTests
     {
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person {Name = "Alex", Age = 19};
 
-            var printer = ObjectPrinter.For<Person>();
+            ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Choose<int>().Exclude()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .Choose<double>().UseSerializer(d => $"My beautiful shiny double: {d}")
                 //3. Для числовых типов указать культуру
+                .Choose(o => o.Age).SetCulture(CultureInfo.CurrentCulture)
                 //4. Настроить сериализацию конкретного свойства
+                .Choose(o => o.Id).UseSerializer(i => i.ToString())
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Choose(o => o.Name).Trim(10)
                 //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+                .Choose(o => o.Height).Exclude()
+                .PrintToString(person);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            /*person.Serialize();
             //8. ...с конфигурированием
+            person.Serialize(d => d.Choose<int>().Exclude());*/
         }
     }
 }
