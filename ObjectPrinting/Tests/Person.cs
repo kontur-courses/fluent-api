249c40a218f1d58e10ef2d9263065bacc31e2da9
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-24 02:43:14 +0500

    Removed methods 'Equals' and 'GetHashCode'

--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -10,17 +10,5 @@ namespace ObjectPrinting.Tests
         public int Age { get; set; }
         public string Surname;
         public Person Friend { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is Person)) return false;
-            var otherPerson = (Person) obj;
-            return Id.Equals(otherPerson.Id);
-        }
-
-        public override int GetHashCode()
-        {
-            return (Id.GetHashCode() * 997) ^ Name.GetHashCode();
-        }
     }
 }
