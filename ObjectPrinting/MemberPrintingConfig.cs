813e32b18d99390bbca34f4ff93557f375f21deb
Denis Bautin <denis020301@mail.ru>
2020-11-24 03:25:18 +0500

    Rename PropertyPrintingConfig to MemberPrintingConfig Rename IPropertyPrintingConfig to IMemberPrintingConfig Realize MemberPrintingConfig

---
+++ ObjectPrinting/MemberPrintingConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ObjectPrinting
+{
+    public class MemberPrintingConfig<TOwner, TMemberType> : IMemberPrintingConfig<TOwner, TMemberType>
+    {
+        private readonly PrintingConfig<TOwner> printingConfig;
+        private Func<object, string> printMember = obj => obj.ToString();
+
+        public MemberPrintingConfig(PrintingConfig<TOwner> printingConfig)
+        {
+            this.printingConfig = printingConfig;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<TMemberType, string> print)
+        {
+            printMember = obj => print((TMemberType) obj);
+            return printingConfig;
+        }
+
+        PrintingConfig<TOwner> IMemberPrintingConfig<TOwner, TMemberType>.ParentConfig => printingConfig;
+        Func<object, string> IMemberPrintingConfig<TOwner, TMemberType>.PrintMember => printMember;
+    }
+
+    public interface IMemberPrintingConfig<TOwner, TMemberType>
+    {
+        PrintingConfig<TOwner> ParentConfig { get; }
+        Func<object, string> PrintMember { get; }
+    }
+}
