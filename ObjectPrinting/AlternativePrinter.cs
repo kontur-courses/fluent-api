a73c3ea3ad3cddcddf11250b548d0f21d10be542
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-11-24 02:43:14 +0500

    Added classes-storages for excluding and alternative representations

---
+++ ObjectPrinting/AlternativePrinter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class AlternativePrinter
+    {
+        private Dictionary<Type, Func<object, string>> PrintsForTypes { get; }
+        private Dictionary<PropertyInfo, Func<object, string>> PrintsForProps { get; }
+        private Dictionary<FieldInfo, Func<object, string>> PrintsForFields { get; }
+
+        public AlternativePrinter()
+        {
+            PrintsForTypes = new Dictionary<Type, Func<object, string>>();
+            PrintsForFields = new Dictionary<FieldInfo, Func<object, string>>();
+            PrintsForProps = new Dictionary<PropertyInfo, Func<object, string>>();
+        }
+
+        private AlternativePrinter(
+            Dictionary<Type, Func<object, string>> printsForTypes,
+            Dictionary<PropertyInfo, Func<object, string>> printsForProps,
+            Dictionary<FieldInfo, Func<object, string>> printsForFields
+        )
+        {
+            PrintsForTypes = printsForTypes;
+            PrintsForProps = printsForProps;
+            PrintsForFields = printsForFields;
+        }
+
+        public bool HasAlternativePrintFor(object value) =>
+            value != default && PrintsForTypes.ContainsKey(value.GetType());
+
+        public bool HasAlternativePrintFor(PropertyInfo property) =>
+            property != default && PrintsForProps.ContainsKey(property);
+
+        public bool HasAlternativePrintFor(FieldInfo field) =>
+            field != default && PrintsForFields.ContainsKey(field);
+
+        public AlternativePrinter AddOrUpdate(Type type, Func<object, string> print) =>
+            new AlternativePrinter
+            (
+                AddOrUpdateDictionary(PrintsForTypes, type, print),
+                PrintsForProps,
+                PrintsForFields
+            );
+
+        public AlternativePrinter AddOrUpdate(PropertyInfo property, Func<object, string> print) =>
+            new AlternativePrinter
+            (
+                PrintsForTypes,
+                AddOrUpdateDictionary(PrintsForProps, property, print),
+                PrintsForFields
+            );
+
+        public AlternativePrinter AddOrUpdate(FieldInfo field, Func<object, string> print) =>
+            new AlternativePrinter
+            (
+                PrintsForTypes,
+                PrintsForProps,
+                AddOrUpdateDictionary(PrintsForFields, field, print)
+            );
+
+        private static Dictionary<TKey, Func<object, string>> AddOrUpdateDictionary<TKey>(
+            IDictionary<TKey, Func<object, string>> dict, TKey key,
+            Func<object, string> print)
+        {
+            var newDict = dict.ToDictionary(pair => pair.Key, pair => pair.Value);
+            newDict[key] = dict.TryGetValue(key, out var prevState)
+                ? obj => print(prevState(obj))
+                : print;
+            return newDict;
+        }
+
+        public string SerializeValue(object obj) => PrintsForTypes[obj.GetType()](obj);
+        public string SerializeProperty(PropertyInfo property, object obj) => PrintsForProps[property](obj);
+        public string SerializeField(FieldInfo field, object obj) => PrintsForFields[field](obj);
+    }
+}
---
+++ ObjectPrinting/PrintExcluder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class PrintExcluder
+    {
+        private ImmutableHashSet<Type> TypesForExclude { get; }
+        private ImmutableHashSet<PropertyInfo> PropertiesForExclude { get; }
+        private ImmutableHashSet<FieldInfo> FieldsForExclude { get; }
+
+        public PrintExcluder()
+        {
+            TypesForExclude = ImmutableHashSet<Type>.Empty;
+            PropertiesForExclude = ImmutableHashSet<PropertyInfo>.Empty;
+            FieldsForExclude = ImmutableHashSet<FieldInfo>.Empty;
+        }
+
+        private PrintExcluder(
+            ImmutableHashSet<FieldInfo> fieldsForExclude,
+            ImmutableHashSet<PropertyInfo> propertiesForExclude,
+            ImmutableHashSet<Type> typesForExclude)
+        {
+            FieldsForExclude = fieldsForExclude;
+            PropertiesForExclude = propertiesForExclude;
+            TypesForExclude = typesForExclude;
+        }
+
+        public bool DidExclude(PropertyInfo property) => PropertiesForExclude.Contains(property) ||
+                                                         TypesForExclude.Contains(property.PropertyType);
+
+        public bool DidExclude(FieldInfo field) => FieldsForExclude.Contains(field) ||
+                                                   TypesForExclude.Contains(field.FieldType);
+
+        public PrintExcluder Exclude(Type type) =>
+            new PrintExcluder(FieldsForExclude, PropertiesForExclude, TypesForExclude.Add(type));
+
+        public PrintExcluder Exclude(FieldInfo field) =>
+            new PrintExcluder(FieldsForExclude.Add(field), PropertiesForExclude, TypesForExclude);
+
+        public PrintExcluder Exclude(PropertyInfo property) =>
+            new PrintExcluder(FieldsForExclude, PropertiesForExclude.Add(property), TypesForExclude);
+    }
+}
