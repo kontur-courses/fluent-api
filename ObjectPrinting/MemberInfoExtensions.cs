5632b1118f4ca22cc22b951414cf965d5b81d8b1
BandLComplete <boroviki2000@mail.ru>
2020-11-23 23:49:11 +0500

    Расширения для работы с MemberInfo.

---
+++ ObjectPrinting/MemberInfoExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public static class MemberInfoExtensions
+    {
+        public static object GetValue(this MemberInfo memberInfo, object forObject)
+        {
+            switch (memberInfo.MemberType)
+            {
+                case MemberTypes.Field:
+                    return ((FieldInfo)memberInfo).GetValue(forObject);
+                case MemberTypes.Property:
+                    return ((PropertyInfo)memberInfo).GetValue(forObject);
+                default:
+                    throw new ArgumentException("Not field or property");
+            }
+        } 
+        
+        public static Type GetMemberType(this MemberInfo memberInfo)
+        {
+            switch (memberInfo.MemberType)
+            {
+                case MemberTypes.Field:
+                    return ((FieldInfo)memberInfo).FieldType;
+                case MemberTypes.Property:
+                    return ((PropertyInfo)memberInfo).PropertyType;
+                default:
+                    throw new ArgumentException("Not field or property");
+            }
+        }
+    }
+}
