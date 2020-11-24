72d165f567265eabb48c82c53fa41b0236d7e1c7
TerribleVoice <terriblevoice@ya.ru>
2020-11-25 02:27:01 +0500

    добавил поле money

--- ObjectPrinterTests/Person.cs
+++ ObjectPrinterTests/Person.cs
@@ -8,5 +8,7 @@ namespace ObjectPrinterTests
         public string Name { get; set; }
         public double Height { get; set; }
         public int Age { get; set; }
+
+        public int money;
     }
 }
