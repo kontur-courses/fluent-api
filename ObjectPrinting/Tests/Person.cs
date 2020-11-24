5a450e660fdf681db9282bce28c7f234f845ff87
MaxCross <dan25001@gmail.com>
2020-11-24 20:35:15 +0500

    Добавил в Person SomeArray и SomeDict для примера

--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ObjectPrinting.Tests
 {
@@ -10,6 +11,15 @@ namespace ObjectPrinting.Tests
         public string Name { get; set; }
         public double Height { get; set; }
         public DateTime BirthDate { get; set; }
+        
+        public int[] SomeArray { get; set; } = {1, 2, 3, 5, 7};
+
+        public Dictionary<string, int> SomeDict { get; set; } = new Dictionary<string, int>()
+        {
+            ["a"] = 1,
+            ["b"] = 2,
+            ["c"] = 7
+        };
 
         public int Age
         {
