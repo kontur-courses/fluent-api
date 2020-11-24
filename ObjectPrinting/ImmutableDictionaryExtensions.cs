27e6d272675dcf761d494efeb00cc79603e0cf6b
Artemy Gordon <artemy.gordon@gmail.com>
2020-11-25 00:40:22 +0500

    Added extension method 'AddOrSet' for ImmutableDictionary

---
+++ ObjectPrinting/ImmutableDictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+
+namespace ObjectPrinting
+{
+    public static class ImmutableDictionaryExtensions
+    {
+        public static ImmutableDictionary<TKey, TValue> AddOrSet<TKey, TValue>(
+            this ImmutableDictionary<TKey, TValue> dictionary,
+            TKey key,
+            TValue value)
+        {
+            return dictionary.ContainsKey(key)
+                ? dictionary.SetItem(key, value)
+                : dictionary.Add(key, value);
+        }
+    }
+}
