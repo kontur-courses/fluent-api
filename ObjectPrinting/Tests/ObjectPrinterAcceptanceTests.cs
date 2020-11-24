c8758ad9056389a523b2e43d4bccd1fa1479ac2f
MaxCross <dan25001@gmail.com>
2020-11-24 07:28:28 +0500

    Демо в ObjectPrinterAcceptanceTests

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,17 +10,44 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person
+            {
+                Father = new Person
+                {
+                    Mother = new Person
+                    {
+                        Mother = new Person
+                        {
+                            Name = "Adam",
+                            Father = new Person
+                            {
+                                Age = 2000,
+                                Name = "???"
+                            }
+                        },
+                    }
+                },
+                Name = "Alex", Age = 19
+            };
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Excluding<Guid>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .Printing<string>().As(s => $"<<{s}>>")
+                .Printing<DateTime>().As(d => $"year: {d.Year}, the rest is mistery...")
                 //3. Для числовых типов указать культуру
                 //4. Настроить сериализацию конкретного свойства
+                .Printing(p => p.Father.Mother.Mother.Father.Name).As(name => $"who is that?{name}")
+                .Printing(p => p.Father.Mother).Using(config => config
+                    .Printing<DateTime>().As(d => $"...but there is less mystery: {d}"))
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
                 //6. Исключить из сериализации конкретного свойства
+                .Excluding(p => p.Mother)
+                .Excluding(p => p.Father.Father);
             
-            string s1 = printer.PrintToString(person);
+            var s1 = printer.PrintToString(person);
+            Console.WriteLine(s1);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -4,9 +4,17 @@ namespace ObjectPrinting.Tests
 {
     public class Person
     {
+        public Person Mother { get; set; }
+        public Person Father { get; set; }
         public Guid Id { get; set; }
         public string Name { get; set; }
         public double Height { get; set; }
-        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public int Age
+        {
+            get => (DateTime.Now - BirthDate).Days / 365;
+            set => BirthDate = DateTime.Now - TimeSpan.FromDays(value * 365);
+        }
     }
 }
