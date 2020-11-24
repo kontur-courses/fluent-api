cdf7ebc91aa392000b34a688b19f4970c4356f98
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-11-24 23:21:31 +0500

    Rewritten test on ApprovalTests

--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -6,12 +6,12 @@ namespace ObjectPrinting.Tests
     public class Person
     {
         public Guid Id;
+        public List<Person> Family;
         public string Name { get; set; }
         public string Surname { get; set; }
         public double Height { get; set; }
         public int Age { get; set; }
         public DateTime BirthDay { get; set; }
         public Person Spouse { get; set; }
-        public List<Person> Family;
     }
 }
--- ObjectPrinting/Tests/PrintingConfig_Should.cs
+++ ObjectPrinting/Tests/PrintingConfig_Should.cs
@@ -1,143 +1,91 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 using ObjectPrinting.Extensions;
+using ApprovalTests;
+using ApprovalTests.Reporters;
 
 namespace ObjectPrinting.Tests
 {
     [TestFixture]
-    public class PrintingConfig
+    [UseReporter(typeof(DiffReporter), typeof(FileLauncherReporter))]
+    [ApprovalTests.Namers.UseApprovalSubdirectory("Approval")]
+    public class PrintingConfig_Should
     {
         [Test]
-        public void PrintToString_Null_ReturnsNull() => ObjectPrinter.For<string>().PrintToString(null)
-            .Should()
-            .Be("null" + Environment.NewLine);
+        public void PrintToString_Null_ReturnsNull() =>
+            Approvals.Verify(ObjectPrinter.For<string>().PrintToString(null));
 
         [Test]
-        public void PrintToString_PersonWithoutExcludingAnything_ContainsAllPublicProperties()
-        {
-            var personProperties = typeof(Person).GetProperties().Select(p => p.Name).ToList();
-            new Person().PrintToString().Should().ContainAll(personProperties);
-        }
+        public void PrintToString_PersonWithoutExcludingAnything_ContainsAllPublicProperties() =>
+            Approvals.Verify(new Person().PrintToString());
 
         [Test]
         public void PrintToString_PersonExcludingGuidValues_NotContainsGuid() =>
-            PersonDoesntContain(nameof(Guid), c => c.Excluding<Guid>());
+            VerifyWithCustomConfig(config => config.Excluding<Guid>());
 
         [Test]
         public void PrintToString_PersonExcludingProperty_NotContainsProperty() =>
-            PersonDoesntContain(nameof(Person.Spouse), c => c.Excluding(p => p.Spouse));
+            VerifyWithCustomConfig(config => config.Excluding(p => p.Spouse));
 
         [Test]
         public void PrintToString_PersonExcludingField_NotContainsProperty() =>
-            PersonDoesntContain(nameof(Person.Id), c => c.Excluding(p => p.Id));
+            VerifyWithCustomConfig(config => config.Excluding(p => p.Id));
 
         [Test]
         public void PrintToString_PersonHasAlternativePrintForDouble_ContainsAlternativeStringInsteadDouble() =>
-            PersonContains("this is double",
-                pc => pc.Printing<double>().Using(d => "this is double"));
+            VerifyWithCustomConfig(config => config.Printing<double>().Using(d => "this is double"));
 
         [Test]
         public void PrintToString_PersonHasAlternativePrintForHeightProperty_ContainsAlternativeStringForHeight() =>
-            PersonContains("this is height",
-                pc => pc.Printing(person => person.Height).Using(d => "this is height"));
+            VerifyWithCustomConfig(config => config.Printing(person => person.Height).Using(d => "this is height"));
 
         [Test]
         public void PrintToString_PersonHasAlternativePrintForIdField_ContainsAlternativeStringForHeight() =>
-            PersonContains("this is personal ID",
-                pc => pc.Printing(person => person.Id).Using(d => "this is personal ID"));
+            VerifyWithCustomConfig(config => config.Printing(person => person.Id).Using(d => "this is personal ID"));
 
         [Test]
         public void PrintToString_PersonHasInvariantCultureForDateTime_ContainsInvariantDateTime() =>
-            PersonContains("01/01/0001 00:00:00",
-                pc => pc.Printing<DateTime>().Using(CultureInfo.InvariantCulture));
+            VerifyWithCustomConfig(config => config.Printing<DateTime>().Using(CultureInfo.InvariantCulture));
 
         [Test]
-        public void PrintToString_PersonHasCurrentCultureForDouble_ContainsCorrespondingDouble()
-        {
-            var person = new Person {Height = 1.78};
-            person
-                .PrintToString(p => p.Printing<double>().Using(CultureInfo.CurrentCulture))
-                .Should()
-                .Contain("Height = 1,78");
-        }
+        public void PrintToString_PersonHasCurrentCultureForDouble_ContainsCorrespondingDouble() =>
+            Approvals.Verify(new Person {Height = 1.78}.PrintToString(
+                config => config.Printing<double>().Using(CultureInfo.CurrentCulture))
+            );
 
         [Test]
-        public void PrintToString_TrimmedPersonStrings_ContainsOnlyTrimmedStrings()
-        {
-            var person = new Person {Name = "Humpty", Surname = "Dumpty"};
-            person
-                .PrintToString(c => c.Printing<string>().TrimmedToLength(3))
-                .Should()
-                .NotContainAll("Humpty", "Dumpty")
-                .And
-                .ContainAll("Hum", "Dum");
-        }
-
-        [Test]
-        public void PrintToString_DifferentConfigs_ReturnsDifferentResults()
-        {
-            var config = ObjectPrinter.For<Person>();
-            var first = config.Excluding(p => p.Id);
-            var second = config.Excluding<int>().Printing<string>().Using(s => "bar");
-            first = first.Printing<string>().Using(s => "foo");
-
-
-            var firstResult = first.PrintToString(new Person());
-            var secondResult = second.PrintToString(new Person());
-
-            firstResult.Should().Contain("Age");
-            secondResult.Should().Contain("Id");
-
-            firstResult.Should().NotContain("bar");
-            secondResult.Should().NotContain("foo");
-        }
+        public void PrintToString_TrimmedPersonStrings_ContainsOnlyTrimmedStrings() =>
+            Approvals.Verify(new Person {Name = "Humpty", Surname = "Dumpty"}.PrintToString(
+                config => config.Printing<string>().TrimmedToLength(3)
+            ));
 
         [Test]
         public void PrintToString_IEnumerable_ReturnsCorrectly() =>
-            Enumerable.Range(0, 10)
-                .PrintToString()
-                .Should()
-                .ContainAll(Enumerable.Range(0, 10).Select(i => i.ToString()));
+            Approvals.Verify(Enumerable.Range(0, 10).PrintToString());
 
         [Test]
-        public void PrintToString_Array_ReturnsCorrectly()
-        {
-            var array = Enumerable.Range(0, 10).Select(i => i.ToString()).ToArray();
-            array.PrintToString().Should().ContainAll(array);
-        }
+        public void PrintToString_Array_ReturnsCorrectly() =>
+            Approvals.Verify(Enumerable.Range(0, 10).ToArray().PrintToString());
 
         [Test]
-        public void PrintToString_List_ReturnsCorrectly()
-        {
-            var list = Enumerable.Range(0, 10).Select(i => i.ToString()).ToList();
-            list.PrintToString().Should().ContainAll(list);
-        }
+        public void PrintToString_List_ReturnsCorrectly() =>
+            Approvals.Verify(Enumerable.Range(0, 10).ToList().PrintToString());
 
         [Test]
         public void PrintToString_Dictionary_ReturnsCorrectly()
         {
-            var dictionary = Enumerable.Range(0, 10)
-                .Select(_ => new Person
-                {
-                    Id = Guid.NewGuid(), BirthDay = DateTime.Now, Height = new Random().Next(0, 100)
-                })
-                .ToDictionary(p => p.Id, p => p);
-            dictionary.PrintToString().Should().ContainAll(dictionary.ToList().PrintToString().Substring(7));
+            var count = 0;
+            var dictionary = Enumerable.Range(0, 3)
+                .Select(_ => new Person())
+                .ToDictionary(person => count++, person => person);
+            Approvals.Verify(dictionary.PrintToString());
         }
 
-        private void PersonDoesntContain(string expected, Func<PrintingConfig<Person>, PrintingConfig<Person>> conf) =>
-            new Person()
-                .PrintToString(conf)
-                .Should()
-                .NotContain(expected);
-
-        private void PersonContains(string expected, Func<PrintingConfig<Person>, PrintingConfig<Person>> conf) =>
-            new Person()
-                .PrintToString(conf)
-                .Should()
-                .Contain(expected);
+        private static void VerifyWithCustomConfig(Func<PrintingConfig<Person>, PrintingConfig<Person>> conf) =>
+            Approvals.Verify(new Person().PrintToString(conf));
     }
 }
