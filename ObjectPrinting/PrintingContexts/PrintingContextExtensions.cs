e6969a0af49cfd938b879c0b5837b22afd92965e
MaxCross <dan25001@gmail.com>
2020-11-24 17:08:28 +0500

    Возможность указать культуру и формат для форматируемых типов

---
+++ ObjectPrinting/PrintingContexts/PrintingContextExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ObjectPrinting
+{
+    public static class PrintingContextExtensions
+    {
+        public static PrintingConfig<TOwner> Using<TOwner, TMember>(
+            this PrintingConfig<TOwner>.PrintingContext<TMember> context,
+            IFormatProvider formatProvider)
+            where TMember : IFormattable
+        {
+            return context.As(f => f.ToString(null, formatProvider));
+        }
+        
+        public static PrintingConfig<TOwner> Using<TOwner, TMember>(
+            this PrintingConfig<TOwner>.PrintingContext<TMember> context,
+            string format,
+            IFormatProvider formatProvider = null)
+            where TMember : IFormattable
+        {
+            return context.As(f => f.ToString(format, formatProvider));
+        }
+    }
+}
