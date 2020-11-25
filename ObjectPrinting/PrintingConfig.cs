8d41211a1ad44ac61df583b3e45085c86c11d12c
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2020-11-25 11:49:52 +0500

    Added Excluding realization

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,29 +1,44 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private HashSet<Type> excludedTypes;
+        private HashSet<PropertyInfo> exludedProperties;
+
+        public PrintingConfig()
+        {
+            excludedTypes = new HashSet<Type>();
+            exludedProperties = new HashSet<PropertyInfo>();
+        }
+
         public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
         {
             return new PropertyPrintingConfig<TOwner, TPropType>(this);
         }
 
-        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(
+            Expression<Func<TOwner, TPropType>> memberSelector)
         {
             return new PropertyPrintingConfig<TOwner, TPropType>(this);
         }
 
         public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
         {
+            var propInfo = ((MemberExpression) memberSelector.Body).Member as PropertyInfo;
+            exludedProperties.Add(propInfo);
             return this;
         }
 
         internal PrintingConfig<TOwner> Excluding<TPropType>()
         {
+            excludedTypes.Add(typeof(TPropType));
             return this;
         }
 
@@ -34,7 +49,6 @@ namespace ObjectPrinting
 
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
 
@@ -52,10 +66,15 @@ namespace ObjectPrinting
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
+                if (exludedProperties.Contains(propertyInfo))
+                    continue;
+                if (excludedTypes.Contains(propertyInfo.PropertyType))
+                    continue;
                 sb.Append(identation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
             }
+
             return sb.ToString();
         }
     }
