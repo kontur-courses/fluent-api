8b63ec1f7e3aafc832a08d818a894bf49cc497e8
FoolLikeYou <Lambda2712@gmail.com>
2020-11-26 13:32:33 +0500

    added Configuration.cs

---
+++ ObjectPrinting/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ObjectPrinting
+{
+    public class Configuration
+    {
+        public Dictionary<Type, Dictionary<Type, CultureInfo>> SpecialSerializeCulture;
+        
+        public Dictionary<Type, HashSet<Type>> ExcudedTypes;
+        public Dictionary<Type, HashSet<string>> ExcudedProperties;
+
+        public Dictionary<Type, Dictionary<Type, Delegate>> SpecialSerializeTypes;
+        public Dictionary<Type, Dictionary<string, Delegate>> SpecialSerializeProperties;
+
+        public Dictionary<Type, Dictionary<string, Delegate>>TrimmingProperties;
+
+        public Configuration()
+        {
+            SpecialSerializeCulture = new Dictionary<Type, Dictionary<Type, CultureInfo>>();
+            
+            ExcudedTypes = new Dictionary<Type, HashSet<Type>>();
+            ExcudedProperties = new Dictionary<Type, HashSet<string>>();
+            
+            SpecialSerializeTypes =new Dictionary<Type, Dictionary<Type, Delegate>>();
+
+            
+            SpecialSerializeProperties = new Dictionary<Type, Dictionary<string, Delegate>>();
+            
+            TrimmingProperties = new Dictionary<Type, Dictionary<string, Delegate>>();
+        }
+    }
+}
