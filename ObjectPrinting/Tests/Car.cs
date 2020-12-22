225072f77f9d0e39ee03181a3d269f940b57a47b
Scindapsus345 <zavnikita345@gmail.com>
2020-12-22 18:59:54 +0500

    Add classes for tests

---
+++ ObjectPrinting/Tests/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ObjectPrinting.Tests
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public string LicensePlateNumber { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public Car(string brand, string color, string licensePlateNumber, DateTime releaseDate)
+        {
+            Brand = brand;
+            Color = color;
+            LicensePlateNumber = licensePlateNumber;
+            ReleaseDate = releaseDate;
+        }
+    }
+}
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,5 +9,19 @@ namespace ObjectPrinting.Tests
         public string Name { get; set; }
         public double Height { get; set; }
         public int Age { get; set; }
+        public bool IsMale { get; set; }
+        public Car Car { get; set; }
+        //public List<Person> Children { get; set; }
+        //public Person Parent { get; set; }
+
+        public Person(Guid id, string name, double height, int age, bool isMale, Car car)
+        {
+            Id = id;
+            Name = name;
+            Height = height;
+            Age = age;
+            IsMale = isMale;
+            Car = car;
+        }
     }
 }
