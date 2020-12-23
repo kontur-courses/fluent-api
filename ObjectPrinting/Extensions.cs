2bd5a5ac22ab372c4e4496112e12823f93b8dafc
Scindapsus345 <zavnikita345@gmail.com>
2020-12-23 23:03:46 +0500

    Add extensions

---
+++ ObjectPrinting/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Globalization;
+
+namespace ObjectPrinting
+{
+    public static class ImmutableDictionaryExtensions
+    {
+        public static ImmutableDictionary<TKey, TValue> AddOrUpdate<TKey, TValue>(
+            this ImmutableDictionary<TKey, TValue> dict, TKey key, TValue value)
+        {
+            if (dict.ContainsKey(key))
+                return dict.SetItem(key, value);
+            return dict.Add(key, value);
+        }
+    }
+
+    public static class PropertyPrintingConfigExtensions
+    {
+        public static PrintingConfig<TOwner> SetCulture<TOwner, TProperty>(
+            this PropertyPrintingConfig<TOwner, TProperty> propConfig, CultureInfo culture)
+            where TProperty : IFormattable
+        {
+            return propConfig.SetSerializer(x => ((IFormattable)x).ToString("N", culture));
+        }
+
+        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(
+            this PropertyPrintingConfig<TOwner, string> propConfig, int maxLength)
+        {
+            return propConfig.SetSerializer(str => str.Substring(0, Math.Min(str.Length, maxLength)));
+        }
+    }
+    public static class ObjectExtensions
+    {
+        public static string PrintToString<T>(this T obj)
+        {
+            return ObjectPrinter<T>.Should().Build().PrintToString(obj);
+        }
+    }
+}
