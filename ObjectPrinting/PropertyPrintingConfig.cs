2b84d07b13e9c0fd7056b2b7e1a15cc31cb97f36
Scindapsus345 <zavnikita345@gmail.com>
2020-12-23 23:04:15 +0500

    Extract PropertyPrintingConfig

---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class PropertyPrintingConfig<TOwner, TProperty>
+    {
+        public PrintingConfig<TOwner> ParentPrintingConfig { get; set; }
+
+        public PropertyInfo Property { get; set; }
+
+        public PropertyPrintingConfig(PropertyInfo property, PrintingConfig<TOwner> parentPrintingConfig)
+        {
+            Property = property;
+            ParentPrintingConfig = parentPrintingConfig;
+        }
+
+        public PrintingConfig<TOwner> SetSerializer(Func<TProperty, string> serializer)
+        {
+            var newConfig = new PrintingConfig<TOwner>(ParentPrintingConfig);
+            newConfig.AltSerializerForProperty = ParentPrintingConfig
+                .AltSerializerForProperty.AddOrUpdate(Property, serializer);
+            return newConfig;
+        }
+    }
+}
