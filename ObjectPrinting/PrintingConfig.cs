327417279772537d24369738d7de0b49bd5b55a8
Nazar Garipov <nazar.garipov475@gmail.com>
2021-11-18 19:47:02 +0500

    Classwork

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,28 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private List<string> excludedProperties = new List<string>();
+        private List<Type> excludedTypes = new List<Type>();
+
+        private Dictionary<Type, Func<object, string>>
+            typeRepresentation = new Dictionary<Type, Func<object, string>>();
+
+        private Dictionary<string, Func<object, string>> propertyRepresentation =
+            new Dictionary<string, Func<object, string>>();
+
+        private CultureInfo cultureInfo = CultureInfo.CurrentCulture;
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -22,8 +39,8 @@ namespace ObjectPrinting
                 typeof(int), typeof(double), typeof(float), typeof(string),
                 typeof(DateTime), typeof(TimeSpan)
             };
-            if (finalTypes.Contains(obj.GetType()))
-                return obj + Environment.NewLine;
+            /*if (finalTypes.Contains(obj.GetType()))
+                return Convert.ToString(obj, cultureInfo) + Environment.NewLine;*/
 
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
@@ -31,11 +48,66 @@ namespace ObjectPrinting
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
+                if (excludedProperties.Contains(propertyInfo.Name)) continue;
+                if (excludedTypes.Contains(propertyInfo.PropertyType)) continue;
+
+
+                sb.Append(GetPropertyRepresentaion(obj, propertyInfo, identation, nestingLevel));
             }
+
             return sb.ToString();
+        }
+
+        private string GetPropertyRepresentaion(object obj, PropertyInfo propertyInfo, string identation,
+            int nestingLevel)
+        {
+            if (typeRepresentation.ContainsKey(propertyInfo.PropertyType))
+            {
+                return identation + typeRepresentation[propertyInfo.PropertyType](propertyInfo.GetValue(obj));
+            }
+
+            if (propertyRepresentation.ContainsKey(propertyInfo.Name))
+            {
+                return identation + propertyRepresentation[propertyInfo.Name](propertyInfo.GetValue(obj));
+            }
+
+            return identation + propertyInfo.Name + " = " +
+                   PrintToString(propertyInfo.GetValue(obj),
+                       nestingLevel + 1);
+        }
+
+        /*public PrintingConfig<TOwner> Excluding(Expression<Func<TOw>>)
+        {
+            excludedProperties.Add(propertyName);
+            return this;
+        }*/
+
+        public PrintingConfig<TOwner> Excluding(Type propertyName)
+        {
+            excludedTypes.Add(propertyName);
+            return this;
+        }
+
+        public PrintingConfig<TOwner> SetTypeSerialization<T>(Func<T, string> func)
+        {
+            string Converted(object x) => func((T)x);
+            typeRepresentation.Add(typeof(T), Converted);
+            return this;
+        }
+
+        public PrintingConfig<TOwner> SetCultureInfo(CultureInfo cultureInfo)
+        {
+            this.cultureInfo = cultureInfo;
+            return this;
+        }
+
+        public PrintingConfig<TOwner> SetPropertySerialization<TResult>(Func<TOwner, TResult> getProperty,
+            Func<TResult, string> serializationFunc)
+        {
+            var propName = typeof(TResult).Name;
+            var objectSerialization = serializationFunc as Func<object, string>;
+            propertyRepresentation[propName] = objectSerialization;
+            return this;
         }
     }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
@@ -8,18 +10,23 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            var person = new Person { Name = "Alex", Age = 19, Height = 189.4, Id = Guid.NewGuid() };
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
+            var printer = ObjectPrinter.For<Person>()
+                .SetTypeSerialization<int>(i => $"int: {i}")
+                .SetCultureInfo(CultureInfo.InvariantCulture)
+                .SetPropertySerialization(person => person.Height, (height) => $"Height is {height}");
+
+            //1. Исключить из сериализации свойства определенного типа
+            //2. Указать альтернативный способ сериализации для определенного типа
+            //3. Для числовых типов указать культуру
+
+            //4. Настроить сериализацию конкретного свойства
+            //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+            //6. Исключить из сериализации конкретного свойства
+
             string s1 = printer.PrintToString(person);
-
+            Console.WriteLine(s1);
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
         }
