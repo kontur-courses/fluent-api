f76c244c60fce1f97068e2fbb08b5742f78868e0
Georgy Grantsev <mrxenonmr@gmail.com>
2021-11-27 14:26:28 +0500

    Перегрузка с форматом и эксепшен при невозможной длине строки

--- ObjectPrinting/Extensions/IInnerConfigExtensions.cs
+++ ObjectPrinting/Extensions/IInnerConfigExtensions.cs
@@ -10,8 +10,16 @@ namespace ObjectPrinting.Extensions
             return config.Using(obj => obj.ToString(null, cultureInfo));
         }
 
+        public static PrintingConfig<TOwner> Using<TOwner, TPropType>(this IInnerPrintingConfig<TOwner, TPropType> config, string format, CultureInfo cultureInfo = null) where TPropType : IFormattable
+        {
+            return config.Using(obj => obj.ToString(format, cultureInfo));
+        }
+
         public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this IInnerPrintingConfig<TOwner, string> config, int maxLen)
         {
+            if (maxLen < 0)
+                throw new ArgumentException("Length cannot be negative");
+
             return config.Using(str => str[..Math.Min(str.Length, maxLen)]);
         }
     }
