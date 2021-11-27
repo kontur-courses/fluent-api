2fb4c82ac0d47f9257015e2c649f4e994a971e9c
Georgy Grantsev <mrxenonmr@gmail.com>
2021-11-27 17:21:08 +0500

    Добавил класс для тестирования циклической ссылки

---
+++ ObjectPrintingTests/PersonWithParent.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectPrintingTests
+{
+    internal class PersonWithParent
+    {
+        public string Name { get; set; }
+        public PersonWithParent Parent { get; set; }
+    }
+}
