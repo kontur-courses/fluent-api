65d9482d1ae41efab8064bdb8f1a136b0b22fabe
Dmitry_Murzin <dmitrymurzin91@gmail.com>
2021-11-27 16:24:33 +0500

    Copy starting interface and project infrastructure

---
+++ Homework/ObjectPrinting/ObjectPrinter.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectPrinting
+{
+    class ObjectPrinter
+    {
+        public static PrintingConfig<T> For<T>()
+        {
+            return new PrintingConfig<T>();
+        }
+    }
+}
