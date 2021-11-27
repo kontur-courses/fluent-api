20bd166f88ae5af3dd9f18bce7c415af0de65603
makspanshin <makspanshin97@gmail.com>
2021-11-27 21:39:12 +0500

    Add test Excluding

---
+++ ObjectPrintingUnitTest/ObjectPrinterTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using ObjectPrinting;
+using ObjectPrinting.Solved.Tests;
+using FluentAssertions;
+namespace ObjectPrintingUnitTest
+{
+    public class ObjectPrinterTests
+    {
+        private Person person;
+        [SetUp]
+        public void SetUp()
+        {
+            person = new Person { Id = Guid.NewGuid(), Name = "Maxim", Age = 21, Height = 180.0};
+        }
+
+        [Test]
+        public void PrintToString_ShouldExcludedTypeGuid()
+        {
+            var printer = ObjectPrinter.For<Person>().Excluding<Guid>();
+
+            printer.PrintToString(person).Should().Be("Person\r\n\tName = Maxim\r\n\tHeight = 180\r\n\tAge = 21\r\n");
+        }
+
+        [Test]
+        public void PrintToString_ShouldExcludedMember()
+        {
+            var printer = ObjectPrinter.For<Person>().Excluding(p => p.Id);
+
+            printer.PrintToString(person).Should().Be("Person\r\n\tName = Maxim\r\n\tHeight = 180\r\n\tAge = 21\r\n");
+        }
+    }
+}
