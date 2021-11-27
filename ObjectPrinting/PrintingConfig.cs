540fbaeccda4817cf52d863cdb016bbfb8390cdb
makspanshin <makspanshin97@gmail.com>
2021-11-27 21:41:23 +0500

    Add Excluding type and members

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private HashSet<Type> excludedTypes = new HashSet<Type>();
+        private HashSet<MemberInfo> excludedMembers = new HashSet<MemberInfo>();
+
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
+        }
+
+        public PrintingConfig<TOwner> Excluding<TPropType>()
+        {
+            excludedTypes.Add(typeof(TPropType));
+            return this;
+        }
+
+        public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        {
+            var selectedProp = (PropertyInfo)((MemberExpression)memberSelector.Body).Member;
+            excludedMembers.Add(selectedProp);
+            return this;
+        }
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -31,6 +61,12 @@ namespace ObjectPrinting
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
+                if (excludedTypes.Contains(propertyInfo.PropertyType))
+                    continue;
+
+                if (excludedMembers.Contains(propertyInfo))
+                    continue;
+
                 sb.Append(identation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
