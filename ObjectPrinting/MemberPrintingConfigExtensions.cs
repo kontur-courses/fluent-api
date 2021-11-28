64eb75e601ee9f88d26d238b045aab39ea88ca34
Titov Stepan <titovstepan@mail.ru>
2021-11-28 13:41:55 +0500

    add extensions for objects and string config

---
+++ ObjectPrinting/MemberPrintingConfigExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace ObjectPrinting
+{
+    public static class MemberPrintingConfigExtensions
+    {
+        public static PrintingConfig<TOwner> Using<TOwner, T>(
+            this IMemberPrintingConfig<TOwner, T> config,
+            CultureInfo cultureInfo) where T : IFormattable
+        {
+            return config.Using(prop => prop.ToString(null, cultureInfo));
+        }
+
+        public static PrintingConfig<TOwner> TrimToLength<TOwner>(
+            this IMemberPrintingConfig<TOwner, string> config, 
+            int trimLength)
+        {
+            return config.Using(s => s[..Math.Min(trimLength, s.Length)]);
+        }
+    }
+}
