b1259e1f11fb1476d4e0a7e95ee12c9e8fe35280
a_lesh_a <aleshabrave@gmail.com>
2021-11-28 21:36:22 +0500

    add 1 and 2 features

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -6,14 +7,38 @@ namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private readonly HashSet<Type> excludingTypes;
+        private readonly Dictionary<Type, Delegate> typePrints;
+            
+        public PrintingConfig()
+        {
+            excludingTypes = new HashSet<Type>();
+            typePrints = new Dictionary<Type, Delegate>();
+        }
+        
+        public PrintingConfig<TOwner> Excluding<TPropType>()
+        {
+            excludingTypes.Add(typeof(TPropType));
+            return this;
+        }
+        
+        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
+        {
+            return new PropertyPrintingConfig<TOwner, TPropType>(this);
+        }
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
         }
 
+        public void UpdatePrintType(Type type, Delegate print)
+        {
+            typePrints[type] = print;
+        }
+
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
 
@@ -22,19 +47,30 @@ namespace ObjectPrinting
                 typeof(int), typeof(double), typeof(float), typeof(string),
                 typeof(DateTime), typeof(TimeSpan)
             };
-            if (finalTypes.Contains(obj.GetType()))
+            
+            var type = obj.GetType();
+            if (finalTypes.Contains(type))
+            {
+                if (typePrints.ContainsKey(type))
+                    return typePrints[type].DynamicInvoke(obj) + Environment.NewLine;
                 return obj + Environment.NewLine;
+            }
 
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
-            var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
+                if (excludingTypes.Contains(propertyInfo.PropertyType))
+                    continue;
                 sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
+                          PrintToString(
+                              propertyInfo.GetValue(obj),
+                              nestingLevel + 1
+                          )
+                );
             }
+
             return sb.ToString();
         }
     }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,10 +1,29 @@
-﻿using NUnit.Framework;
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
     [TestFixture]
     public class ObjectPrinterAcceptanceTests
     {
+        private PrintingConfig<Person> printer;
+        private Person person;
+
+        [SetUp]
+        public void SetUp()
+        {
+            printer = ObjectPrinter.For<Person>();
+            person = new Person
+            {
+                Name = "Albert",
+                Age = 54,
+                Height = 146,
+                Id = new Guid()
+            };
+        }
+
+
         [Test]
         public void Demo()
         {
@@ -22,6 +41,26 @@ namespace ObjectPrinting.Tests
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
+        }
+
+        [Test]
+        public void Excluding_ShouldExcludePropertyOfSpecificType()
+        {
+            printer
+                .Excluding<Guid>()
+                .PrintToString(person)
+                .Should()
+                .Be("Person\r\n\tName = Albert\r\n\tHeight = 146\r\n\tAge = 54\r\n");
+        }
+
+        [Test]
+        public void Printing_Using_ShouldReplaceSpecialTypeOutput()
+        {
+            printer
+                .Printing<int>().Using(x => "")
+                .PrintToString(person)
+                .Should()
+                .Be("Person\r\n\tId = Guid\r\n\tName = Albert\r\n\tHeight = 146\r\n\tAge = \r\n");
         }
     }
 }
