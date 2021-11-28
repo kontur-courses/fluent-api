b4738a3a0d2513fbdccc0e965d323aacdabdc5eb
gODealOAple <maksim.math@gmail.com>
2021-11-28 11:22:36 +0500

    Refactor tests

--- ObjectPrinting.Tests/ObjectPrinterUnitTests.cs
+++ ObjectPrinting.Tests/ObjectPrinterUnitTests.cs
@@ -12,38 +12,41 @@ namespace ObjectPrinting.Tests
         public void Exclude_ShouldExcludeIntPropertiesAndFields_WhenIntGeneric()
         {
             var person = PersonFactory.Get();
-
             var config = ObjectPrinter
                 .For<Person>()
                 .Exclude<int>();
 
-            config.PrintToString(person).Should().NotMatch($"{nameof(Person.Money)}");
-            config.PrintToString(person).Should().NotMatch($"{nameof(Person.Age)}");
+            var serializedPerson = config.PrintToString(person);
+            
+            serializedPerson.Should().NotContain($"{nameof(Person.Money)}");
+            serializedPerson.Should().NotContain($"{nameof(Person.Age)}");
         }
         
         [Test]
         public void Exclude_ShouldExcludeStringProperties_WhenStringGeneric()
         {
             var person = PersonFactory.Get();
-
             var config = ObjectPrinter
                 .For<Person>()
                 .Exclude<string>();
 
-            config.PrintToString(person).Should().NotMatch($"{nameof(Person.Name)}");
+            var serializedPerson = config.PrintToString(person);
+            
+            serializedPerson.Should().NotContain($"{nameof(Person.Name)}");
         }
 
         [Test]
         public void When_Use_ShouldApplyFormattingOfType()
         {
             var person = PersonFactory.Get();
-
             var config = ObjectPrinter
                 .For<Person>()
                 .When<string>()
                 .Use(value => $"{value}{value}{Environment.NewLine}");
 
-            config.PrintToString(person).Should().Contain($"{nameof(Person.Name)} = {person.Name}{person.Name}");
+            var serializedPerson = config.PrintToString(person);
+            
+            serializedPerson.Should().Contain($"{nameof(Person.Name)} = {person.Name}{person.Name}");
         }
     }
 }
