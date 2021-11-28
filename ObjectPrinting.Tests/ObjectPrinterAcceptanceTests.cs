072b85da2b6b94e21a025c53268c1026397ac9e2
gODealOAple <maksim.math@gmail.com>
2021-11-28 10:32:00 +0500

    Init commit

---
+++ ObjectPrinting.Tests/ObjectPrinterAcceptanceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace ObjectPrinting.Tests
+{
+    [TestFixture]
+    public class ObjectPrinterAcceptanceTests
+    {
+        [Test]
+        public void Demo()
+        {
+            var person = new Person { Name = "Alex", Age = 19 };
+            var printer = ObjectPrinter.For<Person>();
+            printer.PrintToString(person);
+        }
+    }
+}
