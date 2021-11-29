ad5dc2eea17d92d1370fc2a2511c75adfede1055
makspanshin <makspanshin97@gmail.com>
2021-11-29 18:35:13 +0500

    Add test if input dictonary

--- ObjectPrintingUnitTest/ObjectPrinterTests.cs
+++ ObjectPrintingUnitTest/ObjectPrinterTests.cs
@@ -106,5 +106,25 @@ namespace ObjectPrintingUnitTest
                          .Should()
                          .Be("\tList`1\r\n\t0 Person\r\n\t\tId = Guid\r\n\t\tName = Maxim\r\n\t\tHeight = 180,2\r\n\t\tAge = 21\r\n\t\tAnotherPerson = null\r\n\t1 Person\r\n\t\tId = Guid\r\n\t\tName = Maxim\r\n\t\tHeight = 180,2\r\n\t\tAge = 21\r\n\t\tAnotherPerson = circular references\r\n");
         }
+
+        [Test]
+        public void PrintToString_ShouldBeCorrect_IfDictonary()
+        {
+            var persons = new Dictionary<int,Person>();
+
+            var anotherPerson = new Person
+            {
+                Name = "Maxim",
+                Age = 21,
+                Height = 180.2,
+                AnotherPerson = person,
+            };
+
+            persons.Add(1, person);
+
+            persons.Add(2, anotherPerson);
+
+            ObjectPrinter.For<Dictionary<int,Person>>().PrintToString(persons).Should().Be("\tDictionary`2\r\n\t0 KeyValuePair`2\r\n\t\tKey = 1\r\n\t\tValue = Person\r\n\t\t\tId = Guid\r\n\t\t\tName = Maxim\r\n\t\t\tHeight = 180,2\r\n\t\t\tAge = 21\r\n\t\t\tAnotherPerson = null\r\n\t1 KeyValuePair`2\r\n\t\tKey = 2\r\n\t\tValue = Person\r\n\t\t\tId = Guid\r\n\t\t\tName = Maxim\r\n\t\t\tHeight = 180,2\r\n\t\t\tAge = 21\r\n\t\t\tAnotherPerson = circular references\r\n");
+        }
     }
 }
