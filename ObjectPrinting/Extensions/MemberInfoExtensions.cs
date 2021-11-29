0f0f4a7cba634c139c83a9a2297c91b862ba4c93
MaxxLorge <MaxxLorge@gmail.com>
2021-11-29 17:20:45 +0500

    добавил extension методы

---
+++ ObjectPrinting/Extensions/MemberInfoExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace ObjectPrinting.Extensions
+{
+    public static class MemberInfoExtensions
+    {
+        public static object GetMemberValue(this MemberInfo member, object obj)
+        {
+            return member switch
+            {
+                PropertyInfo propertyInfo => propertyInfo.GetValue(obj),
+                FieldInfo fieldInfo => fieldInfo.GetValue(obj),
+                _ => throw new ArgumentException($"{nameof(member)} has no value"),
+            };
+        }
+
+        public static Type GetMemberType(this MemberInfo member)
+        {
+            return member switch
+            {
+                PropertyInfo propertyInfo => propertyInfo.PropertyType,
+                FieldInfo fieldInfo => fieldInfo.FieldType,
+                _ => throw new ArgumentException($"{nameof(member)} has no value"),
+            };
+        }
+    }
+}
---
+++ ObjectPrinting/Extensions/TypeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ObjectPrinting.Extensions
+{
+    public static class TypeExtensions
+    {
+        public static MemberInfo[] GetPublicPropertiesAndFields(this Type type)
+        {
+            return type.GetMembers()
+                .Where(m => m is PropertyInfo || m is FieldInfo)
+                .ToArray();
+        }
+    }
+}
