599652f94eac3255d39fe809f0cdc1eba439f115
MaxxLorge <MaxxLorge@gmail.com>
2021-11-29 17:21:14 +0500

    реализовал исключение по типу

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,22 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
+using ObjectPrinting.Extensions;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private readonly HashSet<Type> excludedTypes = new();
+
+        public PrintingConfig<TOwner> Excluding<T>()
+        {
+            excludedTypes.Add(typeof(T));
+            return this;
+        }
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -20,7 +31,7 @@ namespace ObjectPrinting
             var finalTypes = new[]
             {
                 typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
+                typeof(DateTime), typeof(TimeSpan), typeof(Guid)
             };
             if (finalTypes.Contains(obj.GetType()))
                 return obj + Environment.NewLine;
@@ -29,10 +40,10 @@ namespace ObjectPrinting
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
-            foreach (var propertyInfo in type.GetProperties())
+            foreach (var memberInfo in type.GetPublicPropertiesAndFields().Where(p => !excludedTypes.Contains(p.GetMemberType())))
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
+                sb.Append(identation + memberInfo.Name + " = " +
+                          PrintToString(memberInfo.GetMemberValue(obj),
                               nestingLevel + 1));
             }
             return sb.ToString();
