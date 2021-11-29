b8dd4c06d317779cee9d60e12bfef39af1f2b51a
Titov Stepan <titovstepan@mail.ru>
2021-11-29 11:06:47 +0500

    remove property

--- ObjectPrinting/PropertyPrintingConfig.cs
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -5,18 +5,18 @@ namespace ObjectPrinting
 {
     public class PropertyPrintingConfig<TOwner, TProperty> : IMemberPrintingConfig<TOwner, TProperty>
     {
-        public PrintingConfig<TOwner> ParentConfig { get; }
+        private readonly PrintingConfig<TOwner> parentConfig;
         private readonly PropertyInfo propertyInfo;
 
         public PropertyPrintingConfig(PrintingConfig<TOwner> parentConfig, PropertyInfo propertyInfo)
         {
-            ParentConfig = parentConfig;
+            this.parentConfig = parentConfig;
             this.propertyInfo = propertyInfo;
         }
 
         public PrintingConfig<TOwner> Using(Func<TProperty, string> serializationRule)
         {
-            return new PrintingConfig<TOwner>(ParentConfig, propertyInfo, serializationRule);
+            return new PrintingConfig<TOwner>(parentConfig, propertyInfo, serializationRule);
         }
     }
 }
