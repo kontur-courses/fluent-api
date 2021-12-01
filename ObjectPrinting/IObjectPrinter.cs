05f6502f872eddb692c9f8460b58af357283a5b2
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-01 19:52:47 +0500

    Namespace уровня файла (последние)

--- ObjectPrinting/IObjectPrinter.cs
+++ ObjectPrinting/IObjectPrinter.cs
@@ -2,17 +2,18 @@
 using System.Globalization;
 using System.Reflection;
 
-namespace ObjectPrinting;
-
-public interface IObjectPrinter
+namespace ObjectPrinting
 {
-    string PrintToString(object obj);
-    void SetTrimLength(int trimLength);
-    void SetTrimLength(MemberInfo member, int trimLength);
-    void SetCulture(Type type, CultureInfo cultureInfo);
-    void SetCulture(MemberInfo member, CultureInfo cultureInfo);
-    void SetSerializer(Type type, Func<object, string> p);
-    void SetSerializer(MemberInfo member, Func<object, string> p);
-    void Exclude(Type type);
-    void Exclude(MemberInfo member);
+    public interface IObjectPrinter
+    {
+        string PrintToString(object obj);
+        void SetTrimLength(int trimLength);
+        void SetTrimLength(MemberInfo member, int trimLength);
+        void SetCulture(Type type, CultureInfo cultureInfo);
+        void SetCulture(MemberInfo member, CultureInfo cultureInfo);
+        void SetSerializer(Type type, Func<object, string> p);
+        void SetSerializer(MemberInfo member, Func<object, string> p);
+        void Exclude(Type type);
+        void Exclude(MemberInfo member);
+    }
 }
