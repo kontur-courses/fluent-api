cbdb6fe819ec01077bef18d0a380849d7aed91ad
FreeTrally <serega2012111@gmail.com>
2021-12-01 12:36:34 +0500

    Config refractoring

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -5,167 +5,168 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 
-namespace ObjectPrinting
+namespace ObjectPrinting;
+
+public class PrintingConfig<TOwner>
 {
-    public class PrintingConfig<TOwner>
+    private HashSet<Type> defaultToStringTypes = new()
     {
-        private static HashSet<Type> defaultToStringTypes = new()
+        typeof(int),
+        typeof(double),
+        typeof(float),
+        typeof(string),
+        typeof(DateTime),
+        typeof(TimeSpan)
+    };
+    private string newline = Environment.NewLine;
+    private const int EnumerableLimitCount = 9999;
+
+    private readonly HashSet<Type> excludedTypes = new();
+    private readonly HashSet<MemberInfo> excludedMembers = new();
+    private readonly HashSet<object> serializedCache = new();
+
+    private Dictionary<Type, Delegate> serializersForTypes = new();
+    private Dictionary<MemberInfo, Delegate> serializersForMembers = new();
+
+    public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
+    {
+        return new PropertyPrintingConfig<TOwner, TPropType>(this,
+            serializersForTypes, serializersForMembers);
+    }
+
+    public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+    {
+        if (memberSelector.Body is not MemberExpression)
+            throw new ArgumentException("Incorrect member selector!");
+        return new PropertyPrintingConfig<TOwner, TPropType>(this,
+            serializersForTypes, serializersForMembers, memberSelector);
+    }
+
+    public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+    {
+        if (memberSelector.Body is not MemberExpression)
+            throw new ArgumentException("Incorrect member selector!");
+        excludedMembers.Add(((MemberExpression)memberSelector.Body).Member);
+        return this;
+    }
+
+    public PrintingConfig<TOwner> Excluding<TPropType>()
+    {
+        excludedTypes.Add(typeof(TPropType));
+        return this;
+    }
+
+    public string PrintToString(TOwner obj)
+    {
+        return PrintToString(obj, 0);
+    }
+
+    private string PrintToString(object obj, int nestingLevel, MemberInfo info = null)
+    {
+        if (obj == null)
+            return "null" + newline;
+
+        if (defaultToStringTypes.Contains(obj.GetType()))
+            return SerializeValue(obj, info);
+
+        if (obj is IEnumerable enumerable)
+            return SerializeEnumerable(enumerable, nestingLevel);
+
+        return SerializeObject(obj, nestingLevel);
+    }
+
+    private string SerializeObject(object obj, int nestingLevel)
+    {
+        serializedCache.Add(obj);
+        var identation = new string('\t', nestingLevel + 1);
+        var builder = new StringBuilder();
+        builder.AppendLine(obj.GetType().Name);
+
+        foreach (var member in GetMembers(obj))
         {
-            typeof(int),
-            typeof(double),
-            typeof(float),
-            typeof(string),
-            typeof(DateTime),
-            typeof(TimeSpan)
-        };
-        private static string newline = Environment.NewLine;
-
-        private readonly HashSet<Type> excludedTypes = new();
-        private readonly HashSet<MemberInfo> excludedMembers = new();
-        private readonly HashSet<object> serializedCache = new();
-
-        internal Dictionary<Type, Delegate> serializersForTypes = new();
-        internal Dictionary<MemberInfo, Delegate> serializersForMembers = new();
-
-        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
-        {
-            return new PropertyPrintingConfig<TOwner, TPropType>(this);
-        }
-
-        public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
-        {
-            return new PropertyPrintingConfig<TOwner, TPropType>(this, memberSelector);
-        }
-
-        public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
-        {
-            excludedMembers.Add(((MemberExpression)memberSelector.Body).Member);
-            return this;
-        }
-
-        public PrintingConfig<TOwner> Excluding<TPropType>()
-        {
-            excludedTypes.Add(typeof(TPropType));
-            return this;
-        }
-
-        public string PrintToString(TOwner obj)
-        {
-            return PrintToString(obj, 0);
-        }
-
-        private string PrintToString(object obj, int nestingLevel, MemberInfo info = null)
-        {
-            if (TryOtherSerializations(obj, nestingLevel, info, out var result))
-                return result;
-
-            serializedCache.Add(obj);
-            var identation = new string('\t', nestingLevel + 1);
-            var builder = new StringBuilder();
-            builder.AppendLine(obj.GetType().Name);
-
-            foreach (var member in GetMembers(obj))
+            if (serializedCache.Contains(member.Value))
             {
-                if (serializedCache.Contains(member.Value))
-                {
-                    builder.Append($"{identation}{member.Info.Name} = [Loop reference]{newline}");
-                    continue;
-                }
-
-                SerializeMember(nestingLevel, identation, builder, member);
+                builder.Append($"{identation}{member.Info.Name} = [Loop reference]{newline}");
+                continue;
             }
 
-            return builder.ToString();
+            SerializeMember(nestingLevel, identation, builder, member);
         }
 
-        private bool TryOtherSerializations(object obj, int nestingLevel, MemberInfo info, out string result)
+        serializedCache.Remove(obj);
+        return builder.ToString();
+    }
+
+    private string SerializeValue(object obj, MemberInfo info)
+    {
+        if (info != null && serializersForMembers.TryGetValue(info, out var memberSerializer))
+            return memberSerializer.DynamicInvoke(obj) + newline;
+
+        if (serializersForTypes.TryGetValue(obj.GetType(), out var typeSerializer))
+            return typeSerializer.DynamicInvoke(obj) + newline;
+
+        return obj + newline;
+    }
+
+    private string SerializeEnumerable(IEnumerable enumerable, int nestingLevel)
+    {
+        var builder = new StringBuilder(enumerable.GetType().Name + newline);
+        var indentation = new string('\t', nestingLevel + 1);
+
+        AppendBorders(nestingLevel, builder, true);
+
+        var startLength = builder.Length;
+
+        var enumerator = enumerable.GetEnumerator();
+        var count = 0;
+
+        while (enumerator.MoveNext() && count <= EnumerableLimitCount)
         {
-            if (obj == null)
-            {
-                result = "null" + newline;
-                return true;
-            }
-
-            if (defaultToStringTypes.Contains(obj.GetType()))
-            {
-                result = SerializeValue(obj, info);
-                return true;
-            }
-
-            if (obj is IEnumerable enumerable)
-            {
-                result = SerializeEnumerable(enumerable, nestingLevel);
-                return true;
-            }
-
-            result = "";
-            return false;
+            builder.Append($"{indentation}" +
+                $"{PrintToString(enumerator.Current, nestingLevel + 1, null)}");
+            count++;
         }
 
-        private string SerializeValue(object obj, MemberInfo info)
+        if (builder.Length == startLength)
+            builder.Append("Empty");
+
+        AppendBorders(nestingLevel, builder, false);
+
+        return builder.ToString();
+    }
+
+    private void AppendBorders(int nestingLevel, StringBuilder builder, bool isStart)
+    {
+        builder.Append(new string('\t', nestingLevel));
+        var brackets = isStart ? "{{" : "}}";
+        builder.Append(brackets);
+        builder.Append(newline);
+    }
+
+    private IEnumerable<Member> GetMembers(object obj)
+    {
+        foreach (var member in obj.GetType()
+            .GetMembers(BindingFlags.Instance | BindingFlags.Public))
         {
-            if (info != null && serializersForMembers.TryGetValue(info, out var memberSerializer))
-                return memberSerializer.DynamicInvoke(obj) + newline;
+            if (excludedMembers.Contains(member))
+                continue;
 
-            if (serializersForTypes.TryGetValue(obj.GetType(), out var typeSerializer))
-                return typeSerializer.DynamicInvoke(obj) + newline;
+            if (member is FieldInfo field)
+                if (!excludedTypes.Contains(field.FieldType))
+                    yield return new Member(field, field.GetValue(obj));
 
-            return obj + newline;
+            if (member is PropertyInfo property)
+                if (!excludedTypes.Contains(property.PropertyType))
+                    yield return new Member(property, property.GetValue(obj));
         }
+    }
 
-        private string SerializeEnumerable(IEnumerable enumerable, int nestingLevel)
-        {
-            var builder = new StringBuilder(enumerable.GetType().Name + newline);
-            var indentation = new string('\t', nestingLevel + 1);
-
-            AppendBorders(nestingLevel, builder, true);
-
-            var startLength = builder.Length;
-
-            foreach (var obj in enumerable)
-                builder.Append($"{indentation}{PrintToString(obj, nestingLevel + 1, null)}");
-
-            if (builder.Length == startLength)
-                builder.Append("Empty");
-
-            AppendBorders(nestingLevel, builder, false);
-
-            return builder.ToString();
-        }
-
-        private static void AppendBorders(int nestingLevel, StringBuilder builder, bool isStart)
-        {
-            builder.Append(new string('\t', nestingLevel));
-            var brackets = isStart ? "{{" : "}}";
-            builder.Append(brackets);
-            builder.Append(newline);
-        }
-
-        private IEnumerable<Member> GetMembers(object obj)
-        {
-            foreach (var member in obj.GetType()
-                .GetMembers(BindingFlags.Instance | BindingFlags.Public))
-            {
-                if (excludedMembers.Contains(member))
-                    continue;
-
-                if (member is FieldInfo field)
-                    if (!excludedTypes.Contains(field.FieldType))
-                        yield return new Member(field, field.GetValue(obj));
-
-                if (member is PropertyInfo property)
-                    if (!excludedTypes.Contains(property.PropertyType))
-                        yield return new Member(property, property.GetValue(obj));
-            }
-        }
-
-        private void SerializeMember(int nestingLevel, string identation,
-            StringBuilder sb, Member member)
-        {
-            serializedCache.Add(member.Value);
-            var serializedValue = $"{PrintToString(member.Value, nestingLevel + 1, member.Info)}";
-            sb.Append($"{identation}{member.Info.Name} = {serializedValue}");
-            serializedCache.Remove(member.Value);
-        }
+    private void SerializeMember(int nestingLevel, string identation,
+        StringBuilder sb, Member member)
+    {
+        serializedCache.Add(member.Value);
+        var serializedValue = $"{PrintToString(member.Value, nestingLevel + 1, member.Info)}";
+        sb.Append($"{identation}{member.Info.Name} = {serializedValue}");
+        serializedCache.Remove(member.Value);
     }
 }
--- ObjectPrinting/PropertyPrintingConfig.cs
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -1,35 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace ObjectPrinting
+namespace ObjectPrinting;
+
+public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
 {
-    public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
+    PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => printingConfig;
+    private readonly PrintingConfig<TOwner> printingConfig;
+    private readonly Dictionary<Type, Delegate> serializersForTypes;
+    private readonly Dictionary<MemberInfo, Delegate> serializersForMembers;
+    private readonly MemberInfo propertyInfo;
+
+    public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig,
+        Dictionary<Type, Delegate> serializersForTypes,
+        Dictionary<MemberInfo, Delegate> serializersForMembers,
+        Expression<Func<TOwner, TPropType>> memberSelector = null)
     {
-        PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => printingConfig;
-        private readonly PrintingConfig<TOwner> printingConfig;
-        private readonly MemberInfo propertyInfo = null;
+        this.printingConfig = printingConfig;
+        this.serializersForTypes = serializersForTypes;
+        this.serializersForMembers = serializersForMembers;
+        propertyInfo = ((MemberExpression)memberSelector?.Body)?.Member;
+    }
 
-        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig)
-        {
-            this.printingConfig = printingConfig;
-        }
+    public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
+    {
+        if (propertyInfo is null)
+            serializersForTypes[typeof(TPropType)] = print;
+        else
+            serializersForMembers[propertyInfo] = print;
 
-        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig,
-            Expression<Func<TOwner, TPropType>> memberSelector)
-        {
-            this.printingConfig = printingConfig;
-            propertyInfo = ((MemberExpression)memberSelector.Body).Member;
-        }
-
-        public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
-        {
-            if (propertyInfo is null)
-                printingConfig.serializersForTypes[typeof(TPropType)] = print;
-            else
-                printingConfig.serializersForMembers[propertyInfo] = print;
-
-            return printingConfig;
-        }
+        return printingConfig;
     }
 }
