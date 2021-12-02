0054d65b4f070d3fb98503b1caeb9d65eef19e46
Alex <udovi4@gmail.com>
2021-12-02 14:34:12 +0500

    solved stolen)

--- ObjectPrinting/ObjectPrinter.cs
+++ ObjectPrinting/ObjectPrinter.cs
@@ -1,10 +1,17 @@
+using ObjectPrinting.Solved;
+
 namespace ObjectPrinting
 {
-    public class ObjectPrinter
+    public static class ObjectPrinter
     {
         public static PrintingConfig<T> For<T>()
         {
             return new PrintingConfig<T>();
+        }
+        
+        public static string Serialize<T>(this T obj)
+        {
+            return ObjectPrinter.For<T>().PrintToString(obj);
         }
     }
 }
