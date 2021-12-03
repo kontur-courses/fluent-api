10d797b4887e9769ec33b477931b057c8ebcd806
SquirrelLeonid <rm200002@yandex.ru>
2021-12-03 20:59:08 +0500

    Added check for non negative length in TrimmedToLength method

--- ObjectPrintingTask/ObjectPrintingTask/Extensions/ChildPrintingConfigExtensions.cs
+++ ObjectPrintingTask/ObjectPrintingTask/Extensions/ChildPrintingConfigExtensions.cs
@@ -6,12 +6,6 @@ namespace ObjectPrintingTask.Extensions
 {
     public static class ChildPrintingConfigExtensions
     {
-        public static string PrintToString<T>(this T obj, Func<PrintingConfig<T>, PrintingConfig<T>> config)
-        {
-            var printingConfig = config(ObjectPrinter.For<T>());
-            return printingConfig.PrintToString(obj);
-        }
-
         public static PrintingConfig<TOwner> Using<TOwner, TMemberType>(
             this IChildPrintingConfig<TOwner, TMemberType> config,
             CultureInfo cultureInfo = null,
@@ -23,6 +17,9 @@ namespace ObjectPrintingTask.Extensions
         public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(
             this IChildPrintingConfig<TOwner, string> config, int maxLen)
         {
+            if (maxLen < 0)
+                throw new ArgumentException("Cut amount should be >= 0");
+
             return config.Using(str => str.Substring(0, Math.Min(str.Length, maxLen)));
         }
     }
