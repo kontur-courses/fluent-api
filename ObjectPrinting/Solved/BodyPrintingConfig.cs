273b0db7b17607dd28e321406b017389075e44e0
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-04 00:29:25 +0500

    выделил класс для хранения данных главного контекта

---
+++ ObjectPrinting/Solved/BodyPrintingConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ObjectPrinting.Solved
+{
+    public class BodyPrintingConfig<TOwner>
+    {
+        public BodyPrintingConfig(PrintingConfig<TOwner> printingConfig)
+        {
+            PrintingConfig = printingConfig;
+            ExcludingTypes = new List<Type>();
+            AlternativePrinting = new Dictionary<Type, Func<object, string>>();
+            AlternativePrintingProp = new Dictionary<string, Func<object, string>>();
+            ExcludingProp = new List<string>();
+        }
+        public string PropertyKey { get; set; }
+        public PrintingConfig<TOwner> PrintingConfig { get; }
+        public List<Type> ExcludingTypes { get; }
+        public Dictionary<Type, Func<object, string>> AlternativePrinting { get; }
+        public Dictionary<string, Func<object, string>> AlternativePrintingProp { get; }
+        public List<string> ExcludingProp { get; }
+    }
+}
