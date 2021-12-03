39d6ece4f5b08e78470d1ef4bb26ed7af9eb1d35
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-04 00:28:35 +0500

    выделил класс для конечных типов

---
+++ ObjectPrinting/Solved/FinalTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectPrinting.Solved
+{
+    public static class FinalTypes
+    {
+        public static readonly Type[] Types = new[]
+        {
+            typeof(int), typeof(double), typeof(float), typeof(string),
+            typeof(DateTime), typeof(TimeSpan)
+        };
+    }
+}
