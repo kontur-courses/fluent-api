8113a6952cb2859f7d45f9d5fe6b8ad93e158fc1
KirillKalutsky <kefirchik.2000@mail.ru>
2021-12-04 00:30:51 +0500

    добавил поля для тестов

--- ObjectPrinting/Solved/Tests/Person.cs
+++ ObjectPrinting/Solved/Tests/Person.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ObjectPrinting.Solved.Tests
 {
     public class Person
     {
+        //public List<Person> AnotherPersons { get; set; }
         public Guid Id { get; set; }
         public string Name { get; set; }
         public double Height { get; set; }
@@ -11,6 +13,5 @@ namespace ObjectPrinting.Solved.Tests
         public int Health { get; set; }
         public DateTime DateOfBirth { get; set; }
         public Person Parent { get; set; }
-
     }
 }
