29d0fdd5a2bf2dd0d1115abd7ea575b6bf5d34a0
SquirrelLeonid <rm200002@yandex.ru>
2021-12-05 00:20:33 +0500

    added old acceptance test

--- ObjectPrintingTask/ObjectPrintingTests/ObjectPrintingAcceptanceTests.cs
+++ ObjectPrintingTask/ObjectPrintingTests/ObjectPrintingAcceptanceTests.cs
@@ -113,6 +113,34 @@ namespace ObjectPrintingTaskTests
             Console.WriteLine(s2);
         }
 
+        [Test]
+        public void Demo()
+        {          
+            var config = ObjectPrinter.For<Person>()
+                //1. Исключить из сериализации свойства определенного типа
+                .Excluding<Guid>()
+                //2. Указать альтернативный способ сериализации для определенного типа
+                .PrintingType<int>().Using(i => i.ToString("X"))
+                //3. Для числовых типов указать культуру
+                .PrintingType<double>().Using(CultureInfo.InvariantCulture)
+                //4. Настроить сериализацию конкретного свойства
+                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .PrintingMember(p => p.Name).TrimmedToLength(10)
+                //6. Исключить из сериализации конкретного свойства
+                .Excluding(p => p.Age);
+
+            string s1 = config.BuildConfig().PrintToString(person);
+
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+            string s2 = person.PrintToString();
+
+            //8. ...с конфигурированием
+            string s3 = person.PrintToString(s => s.Excluding(p => p.Age));
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+
         private Dictionary<string, Dictionary<string, int>> GetComposedDictionary()
         {
             var JohnContacts = new Dictionary<string, int>()
