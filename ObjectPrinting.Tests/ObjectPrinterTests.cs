763abcd4f7431c60a15318bdeefc98e857dd5779
Igor Sotarev <igor@sotarev.ru>
2022-11-27 16:44:42 +0500

    Add test project

---
+++ ObjectPrinting.Tests/ObjectPrinterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ObjectPrinting.Tests
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Children)]
+    public class ObjectPrinterTests
+    {
+        [Test]
+        public void Should_SerializeCollections()
+        {
+            var list = new List<string> { "a", "b", "c", "d", "d", "f" };
+            var printer = ObjectPrinter.For<List<string>>();
+            Console.Out.WriteLine(printer.PrintToString(list));
+        }
+    }
+}
