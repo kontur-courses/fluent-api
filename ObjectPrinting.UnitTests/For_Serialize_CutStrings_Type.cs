efcd1316f2268d56576e22cbd1b0eaec5c34f6da
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2022-11-27 19:11:19 +0500

    Moved For_Serialize_CutStrings_Type.cs

---
+++ ObjectPrinting.UnitTests/For_Serialize_CutStrings_Type.cs
@@ -0,0 +1,9 @@
+﻿namespace ObjectPrinting.UnitTests;
+
+public class For_Serialize_CutStrings_Type
+{
+    public string CutUpTo25 = TestingConstants.LongTestStringValue;
+    public string CutUpTo50 { get; set; } = TestingConstants.LongTestStringValue;
+
+    public string CutUpTo75Other { get; set; } = TestingConstants.LongTestStringValue;
+}
--- ObjectPrinting.UnitTests/ObjectPrinterTests.cs
+++ ObjectPrinting.UnitTests/ObjectPrinterTests.cs
@@ -143,11 +143,3 @@ public class ObjectPrinterTests
 ");
     }
 }
-
-public class For_Serialize_CutStrings_Type
-{
-    public string CutUpTo25 = TestingConstants.LongTestStringValue;
-    public string CutUpTo50 { get; set; } = TestingConstants.LongTestStringValue;
-
-    public string CutUpTo75Other { get; set; } = TestingConstants.LongTestStringValue;
-}
