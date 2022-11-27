96d25bd3e10fe7935f166c85d38666b1f72f5083
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2022-11-27 20:23:51 +0500

    Made minimal requirement p 7
    Move For_IgnoreMembers_CycleInheritance_Type.cs

---
+++ ObjectPrinting.UnitTests/For_IgnoreMembers_CycleInheritance_Type.cs
@@ -0,0 +1,21 @@
+﻿namespace ObjectPrinting.UnitTests;
+
+public class For_IgnoreMembers_CycleInheritance_Type
+{
+    public For_IgnoreMembers_CycleInheritance_Type()
+    {
+        Children = new(this);
+    }
+
+    public NestedType Children { get; set; }
+
+    public class NestedType
+    {
+        public NestedType(For_IgnoreMembers_CycleInheritance_Type parent)
+        {
+            Parent = parent;
+        }
+
+        public For_IgnoreMembers_CycleInheritance_Type Parent { get; set; }
+    }
+}
--- ObjectPrinting.UnitTests/ObjectPrinterTests.cs
+++ ObjectPrinting.UnitTests/ObjectPrinterTests.cs
@@ -175,23 +175,3 @@ public class ObjectPrinterTests
 ");
     }
 }
-
-public class For_IgnoreMembers_CycleInheritance_Type
-{
-    public For_IgnoreMembers_CycleInheritance_Type()
-    {
-        Children = new(this);
-    }
-
-    public NestedType Children { get; set; }
-
-    public class NestedType
-    {
-        public NestedType(For_IgnoreMembers_CycleInheritance_Type parent)
-        {
-            Parent = parent;
-        }
-
-        public For_IgnoreMembers_CycleInheritance_Type Parent { get; set; }
-    }
-}
