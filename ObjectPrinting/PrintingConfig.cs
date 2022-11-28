a9eaf6f5349a6ca2bb8e07234f4433b91a701191
Anton Yaskov <yaskovanton333@gmail.com>
2022-11-28 21:33:11 +0500

    Add excluding, tests

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,53 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
+using FluentAssertions.Common;
+using NUnit.Framework;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private HashSet<string> excludedTypes;
+        private HashSet<string> excludedNames;
+
+        public PrintingConfig(HashSet<string> types, HashSet<string> names)
+        {
+            excludedTypes = types == null ? new HashSet<string>() : types;
+            excludedNames = names == null ? new HashSet<string>() : names;
+        }
+
+        public PrintingConfig<T> For<T>()
+        {
+            return new PrintingConfig<T>(excludedTypes, excludedNames);
+        }
+
+        public PrintingConfig<TOwner> SetSerialization(Action<TOwner> action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public PrintingConfig<TOwner> SetSerialization<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PrintingConfig<TOwner> Exclude<T>()
+        { 
+            excludedTypes.Add(typeof(T).Name);
+            return this;
+        }
+
+        public PrintingConfig<TOwner> Exclude<T>(Expression<Func<TOwner, T>> func)
+        {
+            var prop = (func.Body as MemberExpression).Member as PropertyInfo;
+            excludedNames.Add(prop.Name);
+            return this;
+        }
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -31,9 +73,11 @@ namespace ObjectPrinting
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
+                if (excludedTypes.Contains(propertyInfo.PropertyType.Name)
+                    && excludedNames.Contains(propertyInfo.Name)) 
+                    continue;
                 sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
+                          PrintToString(propertyInfo.GetValue(obj),nestingLevel + 1));
             }
             return sb.ToString();
         }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,27 +1,52 @@
-﻿using NUnit.Framework;
+﻿using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace ObjectPrinting.Tests
 {
     [TestFixture]
     public class ObjectPrinterAcceptanceTests
     {
-        [Test]
-        public void Demo()
+        private Person person;
+        private PrintingConfig<Person> printer;
+
+        [SetUp]
+        public void SetUp()
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            person = new Person { Name = "Alex", Age = 19 };
+            printer = ObjectPrinter.For<Person>();
+        }
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+        [Test]
+        public void ExcludeType()
+        {
+            var res = printer
+                .Exclude<string>()
+                .PrintToString(person);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
+            res.Should().Be("Person\r\n\tId = Guid\r\n\tHeight = 0\r\n\tAge = 19\r\n");
+        }
+
+        [Test]
+        public void ExcludeName()
+        {
+            var res = printer
+                .Exclude(e => e.Id)
+                .PrintToString(person);
+
+            res.Should().Be("Person\r\n\tName = Alex\r\n\tHeight = 0\r\n\tAge = 19\r\n");
+        }
+
+        [Test]
+        public void ForSavesConfiguration()
+        {
+            var res = printer
+                .Exclude(e => e.Id)
+                .For<int>()
+                .For<Person>()
+                .PrintToString(person);
+
+            res.Should().Be("Person\r\n\tName = Alex\r\n\tHeight = 0\r\n\tAge = 19\r\n");
         }
     }
 }
