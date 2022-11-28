c021d57a9322d4d9d408ce4f7b53977894bef5d8
angst-storm <serezha.kiprin@gmail.com>
2022-11-28 23:21:18 +0500

    added base tests

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -5,6 +5,12 @@ namespace ObjectPrinting;
 
 public class PrintingConfig<TOwner>
 {
+    private readonly Type[] finalTypes =
+    {
+        typeof(int), typeof(double), typeof(float), typeof(string),
+        typeof(DateTime), typeof(TimeSpan)
+    };
+
     public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
     {
         return new PropertyPrintingConfig<TOwner, TPropType>(this);
@@ -31,28 +37,26 @@ public class PrintingConfig<TOwner>
         return PrintToString(obj, 0);
     }
 
-    private string PrintToString(object obj, int nestingLevel)
+    private string PrintToString(object? obj, int nestingLevel)
     {
-        //TODO apply configurations
-        if (obj == null)
-            return "null" + Environment.NewLine;
+        if (obj is null)
+            return "null";
 
-        var finalTypes = new[]
-        {
-            typeof(int), typeof(double), typeof(float), typeof(string),
-            typeof(DateTime), typeof(TimeSpan)
-        };
         if (finalTypes.Contains(obj.GetType()))
-            return obj + Environment.NewLine;
+            return $"{obj}";
 
-        var identation = new string('\t', nestingLevel + 1);
         var sb = new StringBuilder();
+        
         var type = obj.GetType();
         sb.AppendLine(type.Name);
-        foreach (var propertyInfo in type.GetProperties())
-            sb.Append(identation + propertyInfo.Name + " = " +
-                      PrintToString(propertyInfo.GetValue(obj),
-                          nestingLevel + 1));
-        return sb.ToString();
+        
+        var identation = new string('\t', nestingLevel + 1);
+        foreach (var property in type.GetProperties())
+        {
+            var print = PrintToString(property.GetValue(obj), nestingLevel + 1);
+            sb.AppendLine($"{identation}{property.Name} = {print}");
+        }
+        
+        return sb.ToString().Trim();
     }
 }
--- ObjectPrintingTests/ObjectPrinter_Should.cs
+++ ObjectPrintingTests/ObjectPrinter_Should.cs
@@ -1,14 +1,15 @@
 ﻿using System.Globalization;
+using FluentAssertions;
 using NUnit.Framework;
 using ObjectPrinting;
 
 namespace ObjectPrintingTests;
 
 [TestFixture]
-public class ObjectPrinterAcceptanceTests
+public class ObjectPrinter_Should
 {
     [Test]
-    public void Demo()
+    public void AcceptanceTest()
     {
         var person = new Person { Name = "Alex", Age = 19 };
 
@@ -32,8 +33,54 @@ public class ObjectPrinterAcceptanceTests
 
         //8. ...с конфигурированием
         var s3 = person.PrintToString(s => s.Excluding(p => p.Age));
+
         Console.WriteLine(s1);
         Console.WriteLine(s2);
         Console.WriteLine(s3);
+    }
+
+    private static IEnumerable<TestCaseData> SimpleTypesExamples
+    {
+        get
+        {
+            yield return new TestCaseData(null, "null").SetName("On null");
+            yield return new TestCaseData(0, "0").SetName("On int");
+            yield return new TestCaseData(0.1, "0,1").SetName("On double");
+            yield return new TestCaseData(0.1f, "0,1").SetName("On float");
+            yield return new TestCaseData(new DateTime(1970, 1, 1), "01.01.1970 0:00:00").SetName("On datetime");
+            yield return new TestCaseData(new TimeSpan(0), "00:00:00").SetName("On timespan");
+        }
+    }
+
+    [TestCaseSource(nameof(SimpleTypesExamples))]
+    public void DefaultPrintToString_ReturnCorrectString_OnSimpleTypes(object value, string expected)
+    {
+        var result = value.PrintToString();
+
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void DefaultPrintToString_ReturnCorrectString()
+    {
+        var person = new Person { Id = Guid.Empty, Name = "Имя", Height = 0, Age = 0 };
+        var nl = Environment.NewLine;
+        var expected = $"Person{nl}\tId = Guid{nl}\tName = Имя{nl}\tHeight = 0{nl}\tAge = 0";
+
+        var result = person.PrintToString();
+
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void PrintToString_EqualToDefaultPrintToString_OnDefaultPrinter()
+    {
+        var person = new Person { Id = Guid.Empty, Name = "Имя", Height = 0, Age = 0 };
+        var expected = person.PrintToString();
+        var printer = ObjectPrinter.For<Person>();
+
+        var result = printer.PrintToString(person);
+
+        result.Should().Be(expected);
     }
 }
