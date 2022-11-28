9c013c3b4186b5ec9e10fccba886660455b0b13c
angst-storm <serezha.kiprin@gmail.com>
2022-11-28 16:35:01 +0500

    created tests project, removed redundant files

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,41 +1,58 @@
-using System;
-using System.Linq;
+﻿using System.Linq.Expressions;
 using System.Text;
 
-namespace ObjectPrinting
+namespace ObjectPrinting;
+
+public class PrintingConfig<TOwner>
 {
-    public class PrintingConfig<TOwner>
+    public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>()
     {
-        public string PrintToString(TOwner obj)
+        return new PropertyPrintingConfig<TOwner, TPropType>(this);
+    }
+
+    public PropertyPrintingConfig<TOwner, TPropType> Printing<TPropType>(
+        Expression<Func<TOwner, TPropType>> memberSelector)
+    {
+        return new PropertyPrintingConfig<TOwner, TPropType>(this);
+    }
+
+    public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+    {
+        return this;
+    }
+
+    public PrintingConfig<TOwner> Excluding<TPropType>()
+    {
+        return this;
+    }
+
+    public string PrintToString(TOwner obj)
+    {
+        return PrintToString(obj, 0);
+    }
+
+    private string PrintToString(object obj, int nestingLevel)
+    {
+        //TODO apply configurations
+        if (obj == null)
+            return "null" + Environment.NewLine;
+
+        var finalTypes = new[]
         {
-            return PrintToString(obj, 0);
-        }
+            typeof(int), typeof(double), typeof(float), typeof(string),
+            typeof(DateTime), typeof(TimeSpan)
+        };
+        if (finalTypes.Contains(obj.GetType()))
+            return obj + Environment.NewLine;
 
-        private string PrintToString(object obj, int nestingLevel)
-        {
-            //TODO apply configurations
-            if (obj == null)
-                return "null" + Environment.NewLine;
-
-            var finalTypes = new[]
-            {
-                typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
-            };
-            if (finalTypes.Contains(obj.GetType()))
-                return obj + Environment.NewLine;
-
-            var identation = new string('\t', nestingLevel + 1);
-            var sb = new StringBuilder();
-            var type = obj.GetType();
-            sb.AppendLine(type.Name);
-            foreach (var propertyInfo in type.GetProperties())
-            {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
-            }
-            return sb.ToString();
-        }
+        var identation = new string('\t', nestingLevel + 1);
+        var sb = new StringBuilder();
+        var type = obj.GetType();
+        sb.AppendLine(type.Name);
+        foreach (var propertyInfo in type.GetProperties())
+            sb.Append(identation + propertyInfo.Name + " = " +
+                      PrintToString(propertyInfo.GetValue(obj),
+                          nestingLevel + 1));
+        return sb.ToString();
     }
 }
---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace ObjectPrinting;
+
+public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
+{
+    private readonly PrintingConfig<TOwner> printingConfig;
+
+    public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig)
+    {
+        this.printingConfig = printingConfig;
+    }
+
+    PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => printingConfig;
+
+    public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
+    {
+        return printingConfig;
+    }
+
+    public PrintingConfig<TOwner> Using(CultureInfo culture)
+    {
+        return printingConfig;
+    }
+}
+
+public interface IPropertyPrintingConfig<TOwner, TPropType>
+{
+    PrintingConfig<TOwner> ParentConfig { get; }
+}
