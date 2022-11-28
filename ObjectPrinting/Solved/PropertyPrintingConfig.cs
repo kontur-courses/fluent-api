ccfb35f8a47c8072e99665f4216ee8d6db909527
Seekard <maks_byx@mail.ru>
2022-11-28 18:09:34 +0300

    Implemented PropertyPrintingConfig

--- ObjectPrinting/Solved/PropertyPrintingConfig.cs
+++ ObjectPrinting/Solved/PropertyPrintingConfig.cs
@@ -1,28 +1,38 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq.Expressions;
 
 namespace ObjectPrinting.Solved
 {
     public class PropertyPrintingConfig<TOwner, TPropType> : IPropertyPrintingConfig<TOwner, TPropType>
     {
-        private readonly PrintingConfig<TOwner> printingConfig;
+        private readonly PrintingConfig<TOwner> _printingConfig;
+        public Func<TPropType, string> AlternativePrint { get; set; }
 
         public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig)
         {
-            this.printingConfig = printingConfig;
+            _printingConfig = printingConfig;
+            AlternativePrint = null;
+        }
+
+        public string AlternativePrintInvoke(object prop)
+        {
+            return AlternativePrint((TPropType)prop);
         }
 
         public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
         {
-            return printingConfig;
+            AlternativePrint = print;
+            return _printingConfig;
         }
 
         public PrintingConfig<TOwner> Using(CultureInfo culture)
         {
-            return printingConfig;
+            AlternativePrint = property => string.Format(culture, "{0}", property);
+            return _printingConfig;
         }
 
-        PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => printingConfig;
+        PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => _printingConfig;
     }
 
     public interface IPropertyPrintingConfig<TOwner, TPropType>
--- ObjectPrinting/Solved/PropertyPrintingConfigExtensions.cs
+++ ObjectPrinting/Solved/PropertyPrintingConfigExtensions.cs
@@ -9,10 +9,10 @@ namespace ObjectPrinting.Solved
             return config(ObjectPrinter.For<T>()).PrintToString(obj);
         }
 
-        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this PropertyPrintingConfig<TOwner, string> propConfig, int maxLen)
+        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this PropertyPrintingConfig<TOwner, string> propertyConfig, int maxLength)
         {
-            return ((IPropertyPrintingConfig<TOwner, string>)propConfig).ParentConfig;
+            propertyConfig.AlternativePrint = str => str[..maxLength];
+            return ((IPropertyPrintingConfig<TOwner, string>)propertyConfig).ParentConfig;
         }
-
     }
 }
