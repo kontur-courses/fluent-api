4ac40a7a3be2feb2ace52dacc60c42056dd58d90
Lodgent <vovagri2003@gmail.com>
2022-11-28 05:15:02 +0500

    add documentation for api

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -13,97 +13,116 @@ namespace ObjectPrinting
         private static readonly HashSet<Type> FinalTypes = new HashSet<Type>
         {
             typeof(int), typeof(double), typeof(float), typeof(string),
-            typeof(DateTime), typeof(TimeSpan),typeof(bool), typeof(decimal),typeof(long),
-            typeof(byte),typeof(sbyte),typeof(short),typeof(char),typeof(ushort),typeof(uint),typeof(ulong),typeof(ushort)
+            typeof(DateTime), typeof(TimeSpan), typeof(bool), typeof(decimal), typeof(long),
+            typeof(byte), typeof(sbyte), typeof(short), typeof(char), typeof(ushort), typeof(uint), typeof(ulong),
+            typeof(ushort)
         };
+
         private readonly HashSet<Type> excludingTypes = new HashSet<Type>();
         private readonly HashSet<PropertyInfo> excludingProperties = new HashSet<PropertyInfo>();
         public readonly Dictionary<Type, Delegate> TypesForPrintWithSpec = new Dictionary<Type, Delegate>();
-        public readonly Dictionary<PropertyInfo, Delegate> PropertiesForPrintWithSpec = new Dictionary<PropertyInfo, Delegate>();
-        private readonly HashSet<object> printedObjects= new HashSet<object>();
-        public readonly Dictionary<PropertyInfo, int> PropertyLenForString=new Dictionary<PropertyInfo, int>();
+
+        public readonly Dictionary<PropertyInfo, Delegate> PropertiesForPrintWithSpec =
+            new Dictionary<PropertyInfo, Delegate>();
+
+        private readonly HashSet<object> printedObjects = new HashSet<object>();
+        public readonly Dictionary<PropertyInfo, int> PropertyLenForString = new Dictionary<PropertyInfo, int>();
+
         public PropertyConfig<TOwner, TPropType> Printing<TPropType>()
         {
             return new PropertyConfig<TOwner, TPropType>(this);
         }
- 
-        public PropertyConfig<TOwner,TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        /// <summary>
+        /// Include fields by property TPropType
+        /// </summary>
+        /// <typeparam name="TPropType"> Generic parameter for serialization TPropType </typeparam>
+        /// <param name="memberSelector"></param>
+        /// <returns></returns>
+        public PropertyConfig<TOwner, TPropType> Printing<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
         {
             return new PropertyConfig<TOwner, TPropType>(this, GetPropertyInfoFromExpression(memberSelector));
         }
-        private static PropertyInfo GetPropertyInfoFromExpression<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
+        private static PropertyInfo GetPropertyInfoFromExpression<TPropType>(
+            Expression<Func<TOwner, TPropType>> memberSelector)
         {
             return ((MemberExpression)memberSelector.Body).Member as PropertyInfo;
+            
         }
-
+        /// <summary>
+        /// Exclude property from <typeparam name="TPropType">Expression&lt;Func&lt;TOwner, TPropType&gt;&gt;</typeparam> 
+        /// </summary>
+        /// <typeparam name="TPropType"> Generic parameter for serialization TPropType </typeparam>
+        /// <returns> PrintingConfig&lt;TOwner&gt; </returns>
         public PrintingConfig<TOwner> Excluding<TPropType>(Expression<Func<TOwner, TPropType>> memberSelector)
         {
             excludingProperties.Add(GetPropertyInfoFromExpression(memberSelector));
             return this;
         }
+        /// <summary>
+        /// Exclude all fields of <typeparam name="TPropType">TPropType</typeparam> from PrintingConfig 
+        /// </summary>
+        /// <typeparam name="TPropType"> Generic parameter for serialization TPropType </typeparam>
+        /// <returns> PrintingConfig&lt;TOwner&gt; </returns>
 
         internal PrintingConfig<TOwner> Excluding<TPropType>()
         {
             excludingTypes.Add(typeof(TPropType));
-            return this;    
+            return this;
         }
 
-       
-        public string PrintToString(TOwner obj,int maxNesting=-1)
+
+        public string PrintToString(TOwner obj, int maxNesting = -1)
         {
             if (obj is null)
                 return "null" + Environment.NewLine;
-            return GetValueString(obj, 0,maxNesting);
+            return GetValueString(obj, 0, maxNesting);
         }
 
-        private string GetValueString(object obj, int nestingLevel,int maxNesting,PropertyInfo property=null)
+        private string GetValueString(object obj, int nestingLevel, int maxNesting, PropertyInfo property = null)
         {
-            if (ReturnDefaultString(obj,property,out var valueString)) 
+            if (ReturnDefaultString(obj, property, out var valueString))
                 return valueString;
             var stringBuilder = new StringBuilder();
             var type = obj.GetType();
             stringBuilder.AppendLine(type.Name);
             if (printedObjects.Contains(obj))
-                return "Cyclic reference"+Environment.NewLine;
-            
-            
+                return "Cyclic reference" + Environment.NewLine;
             foreach (var propertyInfo in type.GetProperties())
             {
                 printedObjects.Add(obj);
-                if (nestingLevel==maxNesting)
+                if (nestingLevel == maxNesting)
                     break;
-                if(excludingTypes.Contains(propertyInfo.PropertyType) || excludingProperties.Contains(propertyInfo))
+                if (excludingTypes.Contains(propertyInfo.PropertyType) || excludingProperties.Contains(propertyInfo))
                     continue;
 
-                var value = GetValueString(propertyInfo.GetValue(obj),nestingLevel + 1,maxNesting, propertyInfo);
+                var value = GetValueString(propertyInfo.GetValue(obj), nestingLevel + 1, maxNesting, propertyInfo);
                 if (string.IsNullOrEmpty(value)) continue;
-                stringBuilder.Append(new string('\t', nestingLevel+1));
+                stringBuilder.Append(new string('\t', nestingLevel + 1));
                 stringBuilder.Append(propertyInfo.Name);
                 stringBuilder.Append(" = ");
                 stringBuilder.Append(value);
-                
             }
-            
+
             return stringBuilder.ToString();
         }
 
         private bool ReturnDefaultString(object obj, PropertyInfo property, out string valueString)
         {
             valueString = null;
-            if (obj is null) 
+            if (obj is null)
                 return true;
             var objType = obj.GetType();
-            if (!FinalTypes.Contains(objType)) 
+            if (!FinalTypes.Contains(objType))
                 return false;
             var isTrimmed = TrimString(obj, property, ref valueString);
-            valueString= GetString(obj, valueString, objType, property, isTrimmed);
+            valueString = GetString(obj, valueString, objType, property, isTrimmed);
             valueString += Environment.NewLine;
             return true;
         }
 
         private string GetString(object obj, string valueString, Type objType, PropertyInfo property, bool isTrimmed)
         {
-            if(PropertiesForPrintWithSpec.ContainsKey(property))
+            if (PropertiesForPrintWithSpec.ContainsKey(property))
             {
                 if (isTrimmed)
                     valueString = (string)PropertiesForPrintWithSpec[property].DynamicInvoke(valueString);
@@ -117,8 +136,9 @@ namespace ObjectPrinting
                 else
                     valueString = (string)TypesForPrintWithSpec[objType].DynamicInvoke(obj);
             }
-            else if(!isTrimmed)
+            else if (!isTrimmed)
                 valueString = obj.ToString();
+
             return valueString;
         }
 
--- ObjectPrinting/PropertyConfig.cs
+++ ObjectPrinting/PropertyConfig.cs
@@ -8,51 +8,73 @@ namespace ObjectPrinting
     {
         private readonly PrintingConfig<TOwner> printingConfig;
         public PropertyInfo Properties { get; set; }
+
+
         public PropertyConfig(PrintingConfig<TOwner> printingConfig)
         {
             this.printingConfig = printingConfig;
             Properties = null;
         }
-        public PropertyConfig(PrintingConfig<TOwner> printingConfig,PropertyInfo properties)
+
+        public PropertyConfig(PrintingConfig<TOwner> printingConfig, PropertyInfo properties)
         {
             this.printingConfig = printingConfig;
-            Properties=properties;
+            Properties = properties;
         }
 
-
+        /// <summary>
+        /// Defines settings for including properties
+        /// </summary>
+        /// <param name="print"> </param>
+        /// <returns>PrintingConfig&lt;TOwner&gt;</returns>
         public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
         {
-            if(Properties == null)
+            if (Properties == null)
                 printingConfig.TypesForPrintWithSpec[typeof(TPropType)] = print;
             else
                 printingConfig.PropertiesForPrintWithSpec[Properties] = print;
             return printingConfig;
         }
- 
+
         PrintingConfig<TOwner> IPropertyPrintingConfig<TOwner, TPropType>.ParentConfig => printingConfig;
     }
-   
+
 
     public static class PropertyExtensions
     {
+        /// <summary>
+        /// Defines format settings for including properties
+        /// </summary>
+        /// <typeparam name="TOwner"></typeparam>
+        /// <typeparam name="T">IFormattable type</typeparam>
+        /// <param name="config"></param>
+        /// <param name="culture">Culture for IFormattable types </param>
+        /// <param name="format"></param>
+        /// <returns>PrintingConfig&lt;TOwner&gt;</returns>
         public static PrintingConfig<TOwner> Using<TOwner, T>(this PropertyConfig<TOwner, T> config,
             CultureInfo culture, string format = null) where T : IFormattable
         {
             return config.Using(type => type.ToString(format, culture));
         }
-
         public static string PrintToString<T>(this T obj, Func<PrintingConfig<T>, PrintingConfig<T>> config)
         {
             return config(ObjectPrinter.For<T>()).PrintToString(obj);
         }
-
-        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this PropertyConfig<TOwner, string> propertyConfig, int maxLen) 
+        /// <summary>
+        /// Trim string for object printer
+        /// </summary>
+        /// <typeparam name="TOwner"></typeparam>
+        /// <param name="propertyConfig"></param>
+        /// <param name="maxLen">Length maximum setting for string</param>
+        /// <returns></returns>
+        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this PropertyConfig<TOwner, string> propertyConfig,
+            int maxLen)
         {
             var config = (IPropertyPrintingConfig<TOwner, string>)propertyConfig;
-          
+
             config.ParentConfig.PropertyLenForString[config.Properties] = maxLen;
             return config.ParentConfig;
- 
+
         }
     }
 }
