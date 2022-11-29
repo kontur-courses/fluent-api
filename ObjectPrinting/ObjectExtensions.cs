9d6e8323828b4dbb5fc333da56e1e777bdf530de
KsamNole <atm.tsvetk@gmail.com>
2022-11-29 23:49:38 +0500

    Add extensions methods

---
+++ ObjectPrinting/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ObjectPrinting
+{
+    public static class ObjectExtensions
+    {
+        public static string PrintToString<TOwner>(this TOwner obj)
+        {
+            return new PrintingConfig<TOwner>().PrintToString(obj);
+        }
+        
+        public static string PrintToString<TOwner>(this TOwner obj, Func<PrintingConfig<TOwner>, PrintingConfig<TOwner>> func)
+        {
+            return func(ObjectPrinter.For<TOwner>()).PrintToString(obj);
+        }
+    }
+}
---
+++ ObjectPrinting/PropertyPrintingConfigExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ObjectPrinting
+{
+    public static class PropertyPrintingConfigExtensions
+    {
+        public static PrintingConfig<TOwner> TrimmedToLength<TOwner>(this PropertyPrintingConfig<TOwner, string> propertyPrintingConfig, int length)
+        {
+            if (propertyPrintingConfig.PropertyInfo == null) 
+                return propertyPrintingConfig.ParentConfig;
+            
+            var propertyName = propertyPrintingConfig.PropertyInfo.Name;
+            
+            if (propertyPrintingConfig.Serializers.ContainsKey(propertyName))
+            {
+                var trim = new Func<string, string>(x => x[..length]);
+                propertyPrintingConfig.Serializers[propertyName] = Delegate.Combine((Delegate)propertyPrintingConfig.Serializers[propertyName], trim);
+            }
+            else
+                propertyPrintingConfig.Serializers.Add(propertyName, new Func<string, string>(x => x[..length]));
+        
+            return propertyPrintingConfig.ParentConfig;
+        }
+    }
+}
