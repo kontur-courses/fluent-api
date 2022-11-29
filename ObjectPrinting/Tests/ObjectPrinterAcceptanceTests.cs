2228646b7db25bfc7dca49d3047951a31730b040
KsamNole <atm.tsvetk@gmail.com>
2022-11-29 23:49:56 +0500

    Add tests

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -7,11 +7,18 @@ namespace ObjectPrinting.Tests
     [TestFixture]
     public class ObjectPrinterAcceptanceTests
     {
+        private PrintingConfig<Person> printingConfig;
+        private readonly Person person = new Person { Name = "Alex", Surname = "Tsvetkov", Age = 19, Height = 5.6, Width = 7.2f };
+
+        [SetUp]
+        public void SetUp()
+        {
+            printingConfig = ObjectPrinter.For<Person>();
+        }
+        
         [Test]
         public void Demo()
         {
-            var person = new Person { Name = "Alex", Age = 19, Height = 5.6, Width = 7.2f };
-
             var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
                 .Excluding<Guid>()
@@ -22,12 +29,45 @@ namespace ObjectPrinting.Tests
                 //3. Для числовых типов указать культуру
                 .Printing<float>().Using(CultureInfo.InvariantCulture)
                 //2. Указать альтернативный способ сериализации для определенного типа
-                .Printing<double>().Using(x => (x + 100).ToString());
+                .Printing<double>().Using(x => (x + 100).ToString())
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Printing(x => x.Name).TrimmedToLength(2);
 
                 var s1 = printer.PrintToString(person);
-                //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+                //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+                var s2 = person.PrintToString();
                 //8. ...с конфигурированием
+                var s3 = person.PrintToString(x => x.Excluding(x => x.Age));
+        }
+
+        [Test]
+        public void PrintingConfig_WithExcludeType_ShouldReturnString()
+        {
+            var personConfig = printingConfig.Excluding<Guid>();
+
+            var result = personConfig.PrintToString(person);
+            
+            Assert.That(result, Has.No.Contains("Id"));
+        }
+        
+        [Test]
+        public void PrintingConfig_WithExcludeProperty_ShouldReturnString()
+        {
+            var personConfig = printingConfig.Excluding(x => x.Age);
+
+            var result = personConfig.PrintToString(person);
+            
+            Assert.That(result, Has.No.Contains("Age"));
+        }
+        
+        [Test]
+        public void PrintingConfig_WithCustomSerializeType_ShouldReturnString()
+        {
+            var personConfig = printingConfig.Printing<Guid>().Using(x => "id111");
+
+            var result = personConfig.PrintToString(person);
+            
+            Assert.That(result, Has.No.Contains("Guid"));
         }
     }
 }
