61dc3d6d1c24cb5c1d48815f1d65778eb149d79b
Art <bartalamu@list.ru>
2022-11-30 16:10:40 +0500

    objects for printing

---
+++ ObjectPrinting/Tests/DTOs/RecursivePerson.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ObjectPrinting.Tests.DTOs
+{
+    public class RecursivePerson
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public int Age { get; set; }
+        public RecursivePerson Father { get; set; }
+    }
+}
