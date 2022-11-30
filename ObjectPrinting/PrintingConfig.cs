49a60058eac6b0399151ef6364f78cb927996693
nina_prokopova <nina30600@gmail.com>
2022-11-30 20:55:00 +0500

    Add Exclude

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,40 +1,100 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace ObjectPrinting
 {
+    
     public class PrintingConfig<TOwner>
     {
+        private List<Type> removedTypes = new List<Type>();
+        private Dictionary<Type, CultureInfo> cultures = new Dictionary<Type, CultureInfo>();
+
+        private Dictionary<Type, Func<object, string>> alternativeSerialization = 
+            new Dictionary<Type, Func<object, string>>();
+        
+        //тут бы название свойства
+        public Dictionary<String, Func<object, string>> propertySerialization = 
+            new Dictionary<String, Func<object, string>>();
+        
+        //и тут бы тоже название свойства
+        // public Dictionary<MemberInfo, Func<object, string>> crop = 
+        //     new Dictionary<String, Func<object, string>>();
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
         }
 
+        public PrintingConfig<TOwner> ExcludeType<T>()
+        {
+            var tType = typeof(T);
+            removedTypes.Add(tType);
+            return this;
+        }
+
+        public PrintingConfig<TOwner> SerializeType<T>(Func<T, string> func)
+        {
+            alternativeSerialization.Add(typeof(T), x => func((T)x));
+            return this;
+        }
+
+        public PrintingConfig<TOwner> SetCulture<T>(CultureInfo culture)
+        {
+            cultures.Add(typeof(T), culture);
+            return this;
+        }
+        
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
                 return "null" + Environment.NewLine;
-
+            var type = obj.GetType();
+            if (alternativeSerialization.ContainsKey(type))
+                return alternativeSerialization[type](obj);
             var finalTypes = new[]
             {
                 typeof(int), typeof(double), typeof(float), typeof(string),
                 typeof(DateTime), typeof(TimeSpan)
             };
-            if (finalTypes.Contains(obj.GetType()))
+            if (finalTypes.Contains(type))
+            {
+                if (cultures.ContainsKey(type))
+                    return ((IFormattable)obj).ToString(null, cultures[type]);
+
                 return obj + Environment.NewLine;
+            }
+
 
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
-            var type = obj.GetType();
+            
+
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
+                if (removedTypes.Contains(propertyInfo.PropertyType))
+                    continue;
+                if (alternativeSerialization.ContainsKey(propertyInfo.PropertyType))
+                {
+                    sb.Append(alternativeSerialization[propertyInfo.PropertyType](propertyInfo.GetValue(obj)));
+                    continue;
+                }
+
+                if (cultures.ContainsKey(type))
+                {
+                    sb.Append(((IFormattable)propertyInfo.GetValue(obj)!).ToString(null, cultures[type]));
+                    continue;
+                }
+
                 sb.Append(identation + propertyInfo.Name + " = " +
                           PrintToString(propertyInfo.GetValue(obj),
                               nestingLevel + 1));
             }
+
+
             return sb.ToString();
         }
     }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+using FluentAssertions;
+
 
 namespace ObjectPrinting.Tests
 {
@@ -10,18 +15,78 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .ExcludeType<int>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .SerializeType<string>(x => x.ToUpper())
                 //3. Для числовых типов указать культуру
+                .SetCulture<int>(CultureInfo.CurrentCulture);
                 //4. Настроить сериализацию конкретного свойства
+                //.SerializeProperty(p.).SetSerialization(x => { })
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                //.SelectString(p => p.name).Crop(10)
                 //6. Исключить из сериализации конкретного свойства
+                //.ExcludeProperty(p => p.name);
             
             string s1 = printer.PrintToString(person);
+            
+            Console.WriteLine(s1);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
+        }
+
+        [Test]
+        public void ExcludeType_Int()
+        {
+            var person = new Person { Name = "Alex", Age = 19, Height = 70, Id = new Guid()};
+
+            var printer = ObjectPrinter.For<Person>()
+                .ExcludeType<int>();
+            
+            string s1 = printer.PrintToString(person);
+            string result = "Person" + Environment.NewLine +
+                            "\tId = Guid" + Environment.NewLine +
+                            "\tName = Alex" + Environment.NewLine +
+                            "\tHeight = 70" + Environment.NewLine;
+            s1.Should().Be(result);
+        }
+        
+        [Test]
+        public void ExcludeType_String()
+        {
+            var person = new Person { Name = "Alex", Age = 19, Height = 70, Id = new Guid()};
+
+            var printer = ObjectPrinter.For<Person>()
+                .ExcludeType<string>();
+            
+            string s1 = printer.PrintToString(person);
+            string result = "Person" + Environment.NewLine +
+                            "\tId = Guid" + Environment.NewLine +
+                            "\tHeight = 70" + Environment.NewLine +
+                            "\tAge = 19" + Environment.NewLine;
+            s1.Should().Be(result);
+            
+            //Console.WriteLine(s1);
+        }
+        
+        [Test]
+        public void ExcludeType_Double()
+        {
+            var person = new Person { Name = "Alex", Age = 19, Height = 70, Id = new Guid()};
+
+            var printer = ObjectPrinter.For<Person>()
+                .ExcludeType<double>();
+            
+            string s1 = printer.PrintToString(person);
+            string result = "Person" + Environment.NewLine +
+                            "\tId = Guid" + Environment.NewLine +
+                            "\tName = Alex" + Environment.NewLine +
+                            "\tAge = 19" + Environment.NewLine;
+            s1.Should().Be(result);
+            
+            //Console.WriteLine(s1);
         }
     }
 }
