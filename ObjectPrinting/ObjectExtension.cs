64ca8fc9791b060dfb20f2b03adda5bf2598ae3e
Art <bartalamu@list.ru>
2022-11-30 16:16:05 +0500

    ObjectExtension - расширение для сериализации по уполчанию и с конфигурацией

---
+++ ObjectPrinting/ObjectExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectPrinting
+{
+    public static class ObjectExtensions
+    {
+        public static string PrintToString<T>(this T obj) => ObjectPrinter.For<T>().PrintToString(obj);
+
+        public static string PrintListToString<T>(this List<T> obj) => ObjectPrinter.For<T>().PrintListToString(obj);
+
+        public static string PrintingWithConfigure<T>(this T obj,
+            Func<PrintingConfig<T>, PrintingConfig<T>> config)
+        {
+            return config(ObjectPrinter.For<T>()).PrintToString(obj);
+        }
+
+        public static string PrintingListWithConfigure<T>(this List<T> obj,
+            Func<PrintingConfig<T>, PrintingConfig<T>> config)
+        {
+            return config(ObjectPrinter.For<T>()).PrintListToString(obj);
+        }
+    }
+}
