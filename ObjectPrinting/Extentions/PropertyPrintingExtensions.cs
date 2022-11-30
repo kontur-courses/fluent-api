b5d03ca81754e60c3fd2439a762a52a8e431b20a
Zuguki <guskov.zahar20@gmail.com>
2022-11-30 13:39:39 +0300

    Solved dir was removed

--- ObjectPrinting/Extentions/PropertyPrintingExtensions.cs
+++ ObjectPrinting/Extentions/PropertyPrintingExtensions.cs
@@ -1,13 +1,12 @@
 using System;
 using ObjectPrinting.BasicConfigurator.Implementation;
-using ObjectPrinting.Solved;
 
 namespace ObjectPrinting.Extentions;
 
 public static class PropertyPrintingExtensions
 {
     public static string PrintToString<T>(this T item) =>
-        ObjectPrinter.For<T>().PrintToString(item);
+        ObjectConfig.For<T>().ConfigurePrinter().PrintToString(item);
     
     public static string PrintToString<T>(this T obj, Func<ObjectConfigurator<T>, PrintingConfig<T>> config) =>
         config(new ObjectConfigurator<T>()).PrintToString(obj);
