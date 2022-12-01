ac3c5d579bb55e23ed3b775c949f83b43ccd18d9
Ruden <karimov.ruden@mail.ru>
2022-12-02 02:16:49 +0300

    add Tests

---
+++ ObjectPrinter.Tests/Entity/NestedClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPrinting.Tests.Entity
+{
+    public class NestedClass
+    {
+        public int Number { get; set; }
+        public NestedClass Parent { get; set; }
+    }
+}
