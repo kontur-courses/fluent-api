c81a09dcbeee285ef2a74b994bb1f1ef4a2bd3b2
FoxDictator <mega.y1979@gmail.com>
2022-12-01 14:17:15 +0500

    Added SetCulture for property extencion

--- ObjectPrinting/PropertyExtensions.cs
+++ ObjectPrinting/PropertyExtensions.cs
@@ -1,10 +1,21 @@
+using System;
+using System.Globalization;
+
 namespace ObjectPrinting
 {
     public static class PropertyExtensions
     {
-        public static PrintingConfig<TOwner> CutString<TOwner>(this IPropertyConfig<TOwner, string> config, int maxLength)
+        public static PrintingConfig<TOwner> CutString<TOwner>(this IPropertyConfig<TOwner, string> config,
+            int maxLength)
         {
             config.Printer.SetStringCut(config.PropertyExpression, maxLength);
+            return config.Printer;
+        }
+
+        public static PrintingConfig<TOwner> SetCulture<TOwner, T>(this IPropertyConfig<TOwner, T> config,
+            CultureInfo info) where T : IFormattable
+        {
+            config.Printer.SetSerializeMethodForProperty(config.PropertyExpression, (f) => f.ToString("", info));
             return config.Printer;
         }
     }
