b37b30adf676786f638a69fe9957642bf979da7f
Ovchinnikov Nikita <ovchinnikov.poisk@gmail.com>
2022-12-01 05:24:30 +0500

    Added PropertyPrintingConfig and its extensions

---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ObjectPrinting
+{
+    public class PropertyPrintingConfig<TOwner, TField> : PrintingConfig<TOwner>
+    {
+        public readonly PrintingConfig<TOwner> parentConfig;
+        private readonly string propertyName;
+
+        public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig, string propertyName = null)
+        {
+            parentConfig = printingConfig;
+            this.propertyName = propertyName;
+        }
+
+        public PrintingConfig<TOwner> With(Func<TField, string> config)
+        {
+            string PrintingMethod(object obj) => config((TField)obj);
+            var propertyType = typeof(TField);
+
+            if (propertyName == null)
+            {
+                parentConfig.MethodsForPrintingTypes[propertyType] = PrintingMethod;
+            }
+            else
+            {
+                parentConfig.MethodsForPrintingProperties[propertyName] = PrintingMethod;
+            }
+
+            return parentConfig;
+        }
+    }
+}
---
+++ ObjectPrinting/PropertyPrintingConfigExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ObjectPrinting
+{
+    public static class PropertyPrintingConfigExtensions
+    {
+        public static PrintingConfig<TOwner> With<TOwner, TField>(
+            this PropertyPrintingConfig<TOwner, TField> propertyPrintingConfig,
+            CultureInfo culture)
+            where TField : IFormattable
+        {
+            propertyPrintingConfig.With(t => t.ToString(format: null, culture));
+            return propertyPrintingConfig.parentConfig;
+        }
+
+        public static PrintingConfig<TOwner> Trim<TOwner>(
+            this PropertyPrintingConfig<TOwner, string> propPrintingConfig, int maxLength)
+        {
+            propPrintingConfig.With(s => s.Substring(0, maxLength));
+
+            if (propPrintingConfig.parentConfig != null)
+                return propPrintingConfig.parentConfig;
+
+            throw new ArgumentException("parentConfig should not be null");
+        }
+
+        public static string PrintToString<T>(this T obj)
+        {
+            return ObjectPrinter.For<T>().PrintToString(obj);
+        }
+
+        public static string PrintToString<T>(this T obj, Func<PrintingConfig<T>, PrintingConfig<T>> config)
+        {
+            return config(ObjectPrinter.For<T>()).PrintToString(obj);
+        }
+    }
+}
