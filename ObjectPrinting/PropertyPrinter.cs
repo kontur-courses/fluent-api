e39d10226b436c001a9b8f26701b4e29f19b70f2
Luvr681 <secondgoldvip@gmail.com>
2022-12-02 07:27:31 +0500

    Добавил PropertyPrinter

---
+++ ObjectPrinting/PropertyPrinter.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class PropertyPrinter<TOwner, TOwnerProperty>
+    {
+        private readonly PrintingConfig<TOwner> _printingConfig;
+        private readonly PropertyInfo _propertyInfo;
+        
+        public PropertyPrinter(PrintingConfig<TOwner> printingConfig, PropertyInfo propertyInfo)
+        {
+            _printingConfig = printingConfig;
+            _propertyInfo = propertyInfo;
+        }
+
+        public PrintingConfig<TOwner> Using(Func<PropertyInfo, string> printOptions)
+        {
+            if (_propertyInfo != null)
+                _printingConfig.AddSerilizationOptions<TOwnerProperty>(_propertyInfo, printOptions);
+            else
+                _printingConfig.AddSerilizationOptions<TOwnerProperty>(printOptions);
+            
+            return _printingConfig;
+        }
+    }
+}
