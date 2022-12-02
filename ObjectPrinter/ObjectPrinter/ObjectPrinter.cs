ecdabd3f8a4f73f320c8d8bf2fb6611d313869e5
Everal <89090080110@mail.ru>
2022-12-03 04:11:38 +0500

    add collection printing feature

--- ObjectPrinter/ObjectPrinter/ObjectPrinter.cs
+++ ObjectPrinter/ObjectPrinter/ObjectPrinter.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Collections;
+using System.Reflection;
 using System.Text;
 using ObjectPrinter.PrintingConfigs;
 
@@ -14,6 +15,8 @@ public class ObjectPrinter<TFor>
 		this.config = config;
 	}
 
+	private static string NewLine => Environment.NewLine;
+
 	public static PrintingConfig<TFor> Configuration()
 	{
 		return new PrintingConfig<TFor>();
@@ -28,24 +31,27 @@ public class ObjectPrinter<TFor>
 	private string PrintObject(object? obj, int nestingLevel)
 	{
 		if (obj is null)
-			return "null" + Environment.NewLine;
+			return $"null{NewLine}";
 
 		var type = obj.GetType();
 
+		if (obj is ICollection collection)
+			return PrintCollection(collection, nestingLevel);
+
 		if (type.IsClass)
 			if (!printedMembers.Add(obj))
-				return $"Cycling reference{Environment.NewLine}";
+				return $"Cycling reference{NewLine}";
 
 		if (type.IsSerializable)
-			return obj + Environment.NewLine;
+			return $"{obj}{NewLine}";
 
 		var instances = InstanceInfo.GetInstances(obj, BindingFlags.Public | BindingFlags.Instance);
-		var printedProperties = PrintInstances(instances, obj, nestingLevel + 1);
+		var printedProperties = PrintInstances(instances, nestingLevel + 1);
 
-		return $"{type.Name}{Environment.NewLine}{printedProperties}";
+		return $"{type.Name}{NewLine}{printedProperties}";
 	}
 
-	private string PrintInstances(IEnumerable<InstanceInfo> instances, object obj, int nestingLevel)
+	private string PrintInstances(IEnumerable<InstanceInfo> instances, int nestingLevel)
 	{
 		var tabulation = new string('\t', nestingLevel);
 		var printedInstances = new StringBuilder();
@@ -84,10 +90,42 @@ public class ObjectPrinter<TFor>
 		bool TypeContainSelfPrintingRule(Type type) => config.TypesPrintingRules.ContainsKey(type);
 		bool InstanceContainSelfPrintingRule(MemberInfo member) => config.MembersPrintingRules.ContainsKey(member);
 
-		void Append(string typeName, object? value, bool newLine = true)
+		void Append(string typeName, object? value, bool withNewLine = true)
 		{
-			var ending = newLine ? Environment.NewLine : string.Empty;
+			var ending = withNewLine ? NewLine : string.Empty;
 			printedInstances.Append($"{tabulation}{typeName} = {value}{ending}");
 		}
+	}
+
+	private string PrintCollection(ICollection collection, int nestingLevel)
+	{
+		if (collection is IDictionary dict)
+			return PrintDictionary(dict, nestingLevel);
+
+		const string separator = ", ";
+		var start = collection is Array ? "[" : "(";
+		var end = collection is Array ? "]" : ")";
+
+		var sb = new StringBuilder();
+		foreach (var element in collection) sb.Append($"{element}{separator}");
+
+		if (sb.Length > start.Length + separator.Length + end.Length)
+			sb.Remove(sb.Length - separator.Length, separator.Length);
+
+		return $"{start}{sb}{end}{NewLine}";
+	}
+
+	private string PrintDictionary(IDictionary dict, int nestingLevel)
+	{
+		const string separator = " : ";
+		var start = $"{{{NewLine}";
+		var end = $"}}{NewLine}";
+		var tabulation = new string('\t', nestingLevel + 1);
+
+		var sb = new StringBuilder();
+		foreach (DictionaryEntry dictionaryEntry in dict)
+			sb.Append($"{tabulation}{dictionaryEntry.Key}{separator}{dictionaryEntry.Value}{NewLine}");
+
+		return $"{start}{sb}{end}";
 	}
 }
