eb3570c33876f21d929bdc6070c6736693c6dfae
Hrenaki <shikhovwtr@mail.ru>
2022-12-03 21:42:12 +0300

    Added 'PrintToString' extension method without IPrintingConfig parameter

--- ObjectPrinting/Common/PrintingConfigExtensions.cs
+++ ObjectPrinting/Common/PrintingConfigExtensions.cs
@@ -7,6 +7,11 @@ namespace ObjectPrinting.Common
 {
     public static class PrintingConfigExtensions
     {
+        public static string PrintToString<T>(this T obj)
+        {
+            return ObjectPrinter.For<T>().PrintToString(obj);
+        }
+
         public static string PrintToString<T>(this T obj, Func<IPrintingConfig<T>, IPrintingConfig<T>> config)
         {
             return config(ObjectPrinter.For<T>()).PrintToString(obj);
