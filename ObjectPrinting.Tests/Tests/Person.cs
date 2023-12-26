b2169a291df3fea3dea984307ec62ce92e3e5f53
batyadmx <andronovaleksandrptg@yandex.ru>
2023-12-27 00:49:34 +0500

    Add tests

---
+++ ObjectPrinting.Tests/Tests/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObjectPrinting.Tests.Tests
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Height { get; set; }
+        public int Age { get; set; }
+        public Parent Father { get; set; }
+        public Parent Mother { get; set; }
+    }
+
+    public class Parent : Person
+    {
+
+    }
+}
