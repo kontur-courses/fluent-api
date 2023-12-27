a15f1d8be864f9c1c96bf6b66db40712b9deebc0
PavelUd <paveludin4@gmail.com>
2023-12-27 22:05:15 +0500

    remove altconfig

--- ObjectPrinterTests/Collections.cs
+++ ObjectPrinterTests/Collections.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using ObjectPrinting.Tests;
 
 namespace ObjectPrinting
--- ObjectPrinterTests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinterTests/ObjectPrinterAcceptanceTests.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+﻿using System.Globalization;
 using FluentAssertions;
-using NUnit.Framework;
 
 namespace ObjectPrinting.Tests;
 
