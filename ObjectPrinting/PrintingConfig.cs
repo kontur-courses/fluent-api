947fa2db4d007727158fa991877c72442d529d8f
devilboi <turnickii.n@gmail.com>
2024-12-03 15:12:39 +0500

    from offline practice

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,11 +1,19 @@
 using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
+        private readonly HashSet<Type> excludeType= new ();
+        private readonly Dictionary<Type, Func<string, string, string>> serializer = [];
+        private CultureInfo culture = CultureInfo.CurrentCulture;
+       
+
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -20,22 +28,51 @@ namespace ObjectPrinting
             var finalTypes = new[]
             {
                 typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
+                typeof(DateTime), typeof(TimeSpan), typeof(Guid)
             };
             if (finalTypes.Contains(obj.GetType()))
-                return obj + Environment.NewLine;
-
+            {
+                
+                return obj.GetType() + Environment.NewLine;
+            }
+            
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
+                if (excludeType.Contains(propertyInfo.PropertyType)) continue;
+                
+                if (!serializer.ContainsKey(propertyInfo.PropertyType))
+                {
+                    sb.Append(identation + propertyInfo.Name + " = " +
+                              PrintToString(propertyInfo.GetValue(obj),
+                                  nestingLevel + 1));
+                    continue;
+                }
+                sb.Append(identation + serializer[propertyInfo.PropertyType](propertyInfo.Name,
+                    propertyInfo.GetValue(obj) + Environment.NewLine));
             }
             return sb.ToString();
+        }
+        
+        public PrintingConfig<TOwner> Exclude<T>()
+        {
+            excludeType.Add(typeof(T));
+            
+            return this;
+        }
+
+        public PrintingConfig<TOwner> SerializeFor<TProperty>(Func<string, string, string> func)
+        {
+            serializer.Add(typeof(TProperty), func);
+            return this;
+        }
+
+        public PrintingConfig<TOwner> UseCulture(CultureInfo cultureInfo)
+        {
+            
         }
     }
 }
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,7 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace ObjectPrinting.Tests
 {
@@ -9,19 +12,27 @@ namespace ObjectPrinting.Tests
         public void Demo()
         {
             var person = new Person { Name = "Alex", Age = 19 };
-
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
             
-            string s1 = printer.PrintToString(person);
+            var printer = ObjectPrinter.For<Person>();
+            printer.Exclude<Guid>().SerializeFor<string>((name, value) => $"{name} have {value}")
+                .   UseCulture();
+            
+            //1. Исключить из сериализации свойства определенного типа
+            //2. Указать альтернативный способ сериализации для определенного типа
+            //3. Для числовых типов указать культуру
+            //4. Настроить сериализацию конкретного свойства
+            //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+            //6. Исключить из сериализации конкретного свойства
 
+            var s1 = printer.PrintToString(person);
+                
+                
+                
+            Console.WriteLine(s1);
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
         }
+
+       
     }
 }
