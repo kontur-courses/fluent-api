611a707c00c09aec54cf3a37d94f884d5cbebb8c
Geratoptus <shura.bocharov.2004@mail.ru>
2024-12-07 18:17:58 +0500

    add(ObjectPrintingTests.csproj): инитнул тесты

---
+++ ObjectPrintingTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ObjectPrintingTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
