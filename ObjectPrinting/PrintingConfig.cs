360534d1fb8e6e29106ae78272b75061a9888c95
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-09 00:10:26 +0500

    implemented basic functionality

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -10,17 +10,24 @@ namespace ObjectPrinting
 {
     public class PrintingConfig<TOwner>
     {
-        private List<Type> _excludedTypes = new();
-        private List<MemberInfo> _excludedProperties = new();
-        private Dictionary<Type, Delegate> _typeConverters = new();
-        private Dictionary<MemberInfo, Delegate> _propertyConverters = new();
+        private readonly List<Type> _excludedTypes = new();
+        private readonly List<MemberInfo> _excludedProperties = new();
+        private readonly Dictionary<Type, Delegate> _typeConverters = new();
+        private readonly Dictionary<MemberInfo, Delegate> _propertyConverters = new();
         internal CultureInfo DoubleCultureInfo { get; set; } = CultureInfo.CurrentCulture;
         internal CultureInfo FloatCultureInfo { get; set; } = CultureInfo.CurrentCulture;
         internal int MaxStringLength { get; set; }
+        internal int MaxRecursionDepth { get; set; } = 16;
 
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
+        }
+
+        public PrintingConfig<TOwner> WithMaxRecursionDepth(int maxRecursionDepth)
+        {
+            MaxRecursionDepth = maxRecursionDepth;
+            return this;
         }
 
         internal void AddTypeConverter<TParam>(Type type, Func<TParam, string?> converter)
@@ -76,8 +83,7 @@ namespace ObjectPrinting
 
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
-            if (obj == null)
+            if (obj == null || nestingLevel > MaxRecursionDepth)
                 return "null" + Environment.NewLine;
 
             var finalTypes = new[]
@@ -86,19 +92,40 @@ namespace ObjectPrinting
                 typeof(DateTime), typeof(TimeSpan)
             };
             if (finalTypes.Contains(obj.GetType()))
+            {
+                if (obj is string stringValue)
+                    return string.Concat(
+                        stringValue.AsSpan(0, Math.Min(MaxStringLength, stringValue.Length)),
+                        Environment.NewLine);
                 return obj + Environment.NewLine;
+            }
 
-            var identation = new string('\t', nestingLevel + 1);
+            var indentation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
+                if (!_excludedProperties.Contains(propertyInfo) && !_excludedTypes.Contains(propertyInfo.PropertyType))
+                {
+                    var propertyValue = propertyInfo.GetValue(obj);
+                    if (propertyValue == null || !TryConvert(propertyInfo, propertyValue, out var valueString))
+                        valueString = PrintToString(propertyValue, nestingLevel + 1);
+                    
+                    sb.Append($"{indentation}{propertyInfo.Name} = {valueString}");
+                }
             }
             return sb.ToString();
+        }
+
+        private bool TryConvert(PropertyInfo propertyInfo, object? propertyValue, out string value)
+        {
+            value = String.Empty;
+            if (_propertyConverters.TryGetValue(propertyInfo, out var converter))
+                value = $"{converter.DynamicInvoke(propertyValue) as string ?? "null"}{Environment.NewLine}";
+            else if (_typeConverters.TryGetValue(propertyInfo.PropertyType, out var typeConverter))
+                value = $"{typeConverter.DynamicInvoke(propertyValue) as string ?? "null"}{Environment.NewLine}";
+            return value != String.Empty;
         }
     }
 }
