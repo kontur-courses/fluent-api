6be01d34c1a1080151c2c54495c0a5d81011dabc
kokodio <kirill.zaripow@gmail.com>
2024-12-08 20:28:49 +0500

    Add tests

---
+++ ObjectPrinting/Tests/PersonV2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ObjectPrinting.Tests;
+
+public class PersonV2
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public double Height { get; set; }
+    public int Age { get; set; }
+    public PersonV2 Mother { get; set; }
+    public List<PersonV2> Kids { get; set; }
+}
