bfa1ca5018f875c2c8decf8e97c5d9a5432de5bf
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-10 01:15:02 +0500

    Добавил тесты

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,27 +1,242 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using FluentAssertions;
+using NUnit.Framework;
 
-namespace ObjectPrinting.Tests
+namespace ObjectPrinting.Tests;
+
+[TestFixture]
+public class ObjectPrinterAcceptanceTests
 {
-    [TestFixture]
-    public class ObjectPrinterAcceptanceTests
+    [Test]
+    public void PrintToString_ExcludeType()
     {
-        [Test]
-        public void Demo()
+        var person = new Person { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) };
+        const string expected = """
+                                Person
+                                	Id = 00000000-0000-0000-0000-000000000000
+                                	Name = Bob
+                                	Height = 200
+                                	Birthday = 10/10/2000 00:00:00
+                                	Father = null
+        
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(person, config => config.ExcludeType<int>());
+
+        serializedString.Should().Be(expected);
+    }
+    
+    [Test]
+    public void PrintToString_AlternativeTypeSerialization()
+    {
+        var person = new Person { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) };
+        const string expected = """
+                                Person
+                                	Id = 00000000-0000-0000-0000-000000000000
+                                	Name = Bob
+                                	Height = 200
+                                	Age = 20
+                                	Birthday = date
+                                	Father = null
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(
+            person, 
+            config => config.AddSerializationMethod<DateTime>(_ => "date"));
+        
+        serializedString.Should().Be(expected);
+    }
+
+    [Test]
+    public void PrintToString_SetCulture()
+    {
+        var person = new Person { Name = "Bob", Age = 20, Height = 200.2, Birthday = new DateTime(2000, 10, 10) };
+        const string expected = """
+                                Person
+                                	Id = 00000000-0000-0000-0000-000000000000
+                                	Name = Bob
+                                	Height = 200.2
+                                	Age = 20
+                                	Birthday = 10/10/2000 00:00:00
+                                	Father = null
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(
+            person, 
+            config => config.SpecifyTheCulture<double>(new CultureInfo("en-US")));
+        
+        serializedString.Should().Be(expected);
+    }
+    
+    [Test]
+    public void PrintToString_AlternativePropertySerialization()
+    {
+        var person = new Person { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) };
+        const string expected = """
+                                Person
+                                	Id = 00000000-0000-0000-0000-000000000000
+                                	Name = NoName
+                                	Height = 200
+                                	Age = 20
+                                	Birthday = 10/10/2000 00:00:00
+                                	Father = null
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(
+            person, 
+            config => config.AddSerializationMethod(_ => "NoName", p => p.Name));
+        
+        serializedString.Should().Be(expected);
+    }
+
+    [Test]
+    public void PrintToString_Trim()
+    {
+        var person = new Person { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) };
+        const string expected = """
+                                Person
+                                	Id = 00000000-0000-0000-0000-000000000000
+                                	Name = Bo
+                                	Height = 200
+                                	Age = 20
+                                	Birthday = 10/10/2000 00:00:00
+                                	Father = null
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(
+            person, 
+            config => config.Trim(2));
+        
+        serializedString.Should().Be(expected);
+    }
+
+    [Test]
+    public void PrintToString_CircularReference()
+    {
+        var person = new Person { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) };
+        person.Father = person;
+        const string expected = """
+                                Person
+                                	Id = 00000000-0000-0000-0000-000000000000
+                                	Name = Bob
+                                	Height = 200
+                                	Age = 20
+                                	Birthday = 10/10/2000 00:00:00
+                                	Father = Circular Reference
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(person);
+        
+        serializedString.Should().Be(expected);
+    }
+
+    [Test]
+    public void PrintToString_SerializeArray()
+    {
+        var persons = new Person[]
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            new() { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) },
+            new() { Name = "Bread", Age = 22, Height = 222, Birthday = new DateTime(2001, 11, 11) }
+        };
+        
+        const string expected = """
+                                Person[]:
+                                	Person
+                                		Id = 00000000-0000-0000-0000-000000000000
+                                		Name = Bob
+                                		Height = 200
+                                		Age = 20
+                                		Birthday = 10/10/2000 00:00:00
+                                		Father = null
+                                	Person
+                                		Id = 00000000-0000-0000-0000-000000000000
+                                		Name = Bread
+                                		Height = 222
+                                		Age = 22
+                                		Birthday = 11/11/2001 00:00:00
+                                		Father = null
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                //2. Указать альтернативный способ сериализации для определенного типа
-                //3. Для числовых типов указать культуру
-                //4. Настроить сериализацию конкретного свойства
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(persons);
+        
+        serializedString.Should().Be(expected);
+    }
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
-        }
+    [Test]
+    public void PrintToString_SerializeList()
+    {
+        var persons = new List<Person>
+        {
+            new() { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) },
+            new() { Name = "Bread", Age = 22, Height = 222, Birthday = new DateTime(2001, 11, 11) }
+        };
+        
+        const string expected = """
+                                List`1:
+                                	Person
+                                		Id = 00000000-0000-0000-0000-000000000000
+                                		Name = Bob
+                                		Height = 200
+                                		Age = 20
+                                		Birthday = 10/10/2000 00:00:00
+                                		Father = null
+                                	Person
+                                		Id = 00000000-0000-0000-0000-000000000000
+                                		Name = Bread
+                                		Height = 222
+                                		Age = 22
+                                		Birthday = 11/11/2001 00:00:00
+                                		Father = null
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(persons);
+        
+        serializedString.Should().Be(expected);
+    }
+
+    [Test]
+    public void PrintToString_SerializeDictionary()
+    {
+        var persons = new Dictionary<Person, string>
+        {
+            { new() { Name = "Bob", Age = 20, Height = 200, Birthday = new DateTime(2000, 10, 10) }, "first" },
+            { new() { Name = "Bread", Age = 22, Height = 222, Birthday = new DateTime(2001, 11, 11) }, "second" }
+        };
+        
+        const string expected = """
+                                Dictionary`2:
+                                	KeyValuePair`2
+                                		Key = Person
+                                			Id = 00000000-0000-0000-0000-000000000000
+                                			Name = Bob
+                                			Height = 200
+                                			Age = 20
+                                			Birthday = 10/10/2000 00:00:00
+                                			Father = null
+                                		Value = first
+                                	KeyValuePair`2
+                                		Key = Person
+                                			Id = 00000000-0000-0000-0000-000000000000
+                                			Name = Bread
+                                			Height = 222
+                                			Age = 22
+                                			Birthday = 11/11/2001 00:00:00
+                                			Father = null
+                                		Value = second
+
+                                """;
+        
+        var serializedString = PrintingConfig<Person>.Serialize(persons);
+        
+        serializedString.Should().Be(expected);
     }
 }
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 
-namespace ObjectPrinting.Tests
+namespace ObjectPrinting.Tests;
+
+public class Person
 {
-    public class Person
-    {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public double Height { get; set; }
-        public int Age { get; set; }
-    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+        
+    public double Height { get; set; }
+    public int Age { get; set; }
+        
+    public DateTime Birthday { get; set; }
+    
+    public Person? Father { get; set; }
 }
