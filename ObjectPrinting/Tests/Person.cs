3106b0d9e6d77c1cddf33f95e76118b3638dc356
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-10 01:15:41 +0500

    Небольшое изменение в Person

--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -5,9 +5,11 @@ namespace ObjectPrinting.Tests;
 public class Person
 {
     public Guid Id { get; set; }
+    
     public string Name { get; set; }
         
     public double Height { get; set; }
+    
     public int Age { get; set; }
         
     public DateTime Birthday { get; set; }
