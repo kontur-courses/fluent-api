d8a829486d4aca9ce005cc79f0819fae873091db
S4MPAI <flekshum@gmail.com>
2024-12-10 01:46:59 +0500

    add tests for ObjectPrinter

---
+++ ObjectPrinting/Tests/PersonWithWeight.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ObjectPrinting.Tests;
+
+public class PersonWithWeight
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public double Height { get; set; }
+    public double Weight { get; set; }
+    public int Age { get; set; }
+}
