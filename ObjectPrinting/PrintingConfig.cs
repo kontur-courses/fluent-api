88a3a90c22834a2c573b6fa323bde53dca8d6d5c
Ilya4rh <chernykn12ilya12@gmail.com>
2024-12-09 20:32:14 +0500

    Добавил метод исключения типа

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,41 +1,50 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Text;
 
-namespace ObjectPrinting
+namespace ObjectPrinting;
+
+public class PrintingConfig<TOwner>
 {
-    public class PrintingConfig<TOwner>
+    private readonly HashSet<Type> finalTypes =
+    [
+        typeof(int), typeof(double), typeof(float), typeof(string),
+        typeof(DateTime), typeof(TimeSpan)
+    ];
+    private readonly HashSet<Type> excludedTypes = [];
+    
+    public string PrintToString(TOwner obj)
     {
-        public string PrintToString(TOwner obj)
+        return PrintToString(obj, 0);
+    }
+
+    private string PrintToString(object? obj, int nestingLevel)
+    {
+        if (obj == null)
         {
-            return PrintToString(obj, 0);
+            return "null" + Environment.NewLine;
         }
 
-        private string PrintToString(object obj, int nestingLevel)
+        if (finalTypes.Contains(obj.GetType()))
+            return obj + Environment.NewLine;
+
+        var identation = new string('\t', nestingLevel + 1);
+        var sb = new StringBuilder();
+        var type = obj.GetType();
+        sb.AppendLine(type.Name);
+        foreach (var propertyInfo in type.GetProperties())
         {
-            //TODO apply configurations
-            if (obj == null)
-                return "null" + Environment.NewLine;
-
-            var finalTypes = new[]
-            {
-                typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
-            };
-            if (finalTypes.Contains(obj.GetType()))
-                return obj + Environment.NewLine;
-
-            var identation = new string('\t', nestingLevel + 1);
-            var sb = new StringBuilder();
-            var type = obj.GetType();
-            sb.AppendLine(type.Name);
-            foreach (var propertyInfo in type.GetProperties())
-            {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
-            }
-            return sb.ToString();
+            sb.Append(identation + propertyInfo.Name + " = " +
+                      PrintToString(propertyInfo.GetValue(obj),
+                          nestingLevel + 1));
         }
+
+        return sb.ToString();
+    }
+
+    public PrintingConfig<TOwner> ExcludeType<TType>()
+    {
+        excludedTypes.Add(typeof(TType));
+        return this;
     }
 }
