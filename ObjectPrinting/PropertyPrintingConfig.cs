864c34ec7126fb40aa3c8ef946e4b31612ba80a4
Савелий <vildanov.s@bk.ru>
2024-12-11 03:38:38 +0500

    Add implementation of ObjectPrinter

---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectPrinting;
+
+public class PropertyPrintingConfig<TOwner, TPropType>
+{
+    public readonly PrintingConfig<TOwner> PrintingConfig;
+    public readonly PropertyInfo PropertyInfo;
+
+
+    public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig, PropertyInfo propertyInfo)
+    {
+        PrintingConfig = printingConfig;
+        PropertyInfo = propertyInfo;
+    }
+
+    public PrintingConfig<TOwner> Using(Func<TPropType, string> newSerialize)
+    {
+        PrintingConfig.AddSerializedProperty(PropertyInfo, newSerialize);
+        return PrintingConfig;
+    }
+}
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -6,6 +6,8 @@ namespace ObjectPrinting.Tests
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string NameOfPet { get; set; }
         public double Height { get; set; }
         public int Age { get; set; }
     }
