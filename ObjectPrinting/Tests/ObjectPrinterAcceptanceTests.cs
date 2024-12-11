db042a5201eecf85edd6edf4c120301a97fb566a
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2024-12-11 18:54:40 +0500

    Базовая настройка проекта, исключил предложенный тестовый файл

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -8,9 +8,10 @@ namespace ObjectPrinting.Tests
         [Test]
         public void Demo()
         {
+            
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
+            //var printer = ObjectPrinter.For<Person>();
                 //1. Исключить из сериализации свойства определенного типа
                 //2. Указать альтернативный способ сериализации для определенного типа
                 //3. Для числовых типов указать культуру
@@ -18,7 +19,7 @@ namespace ObjectPrinting.Tests
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
                 //6. Исключить из сериализации конкретного свойства
             
-            string s1 = printer.PrintToString(person);
+            //string s1 = printer.PrintToString(person);
 
             //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
             //8. ...с конфигурированием
