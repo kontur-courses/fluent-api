92dbb2a1e03977e5e70549092ae71a48903bdcfc
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-11 21:50:10 +0500

    added tests

--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,40 +1,180 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Threading.Tasks;
 using NUnit.Framework;
+using VerifyNUnit;
 
-namespace ObjectPrinting.Tests
+namespace ObjectPrinting.Tests;
+
+[TestFixture]
+public class ObjectPrinterAcceptanceTests
 {
-    [TestFixture]
-    public class ObjectPrinterAcceptanceTests
+    private Person _person;
+    private PrintingConfig<Person> _config;
+
+    [SetUp]
+    public void SetUp()
     {
-        [Test]
-        public void Demo()
+        _person = new Person
         {
-            var person = new Person { Name = "Alex", Age = 19 };
+            Id = new Guid(), Name = "Alex", Surname = "Smith",
+            Age = 19, Height = 177.4
+        };
+        _config = ObjectPrinter.For<Person>();
+    }
 
-            var printer = ObjectPrinter.For<Person>();
-                //1. Исключить из сериализации свойства определенного типа
-                printer.ExceptType<string>();
-                //2. Указать альтернативный способ сериализации для определенного типа
-                printer.ForType<Person>()
-                    .Use(o => o.ToString());
-                //3. Для числовых типов указать культуру
-                printer.ForType<double>()
-                    .UseCulture(CultureInfo.InvariantCulture);
-                //4. Настроить сериализацию конкретного свойства
-                printer.ForProperty(x => x.Age) // TODO use Expression<Func<...>>
-                    .Use(o => o.ToString());
-                //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
-                printer.ForType<string>()
-                    .UseMaxLength(15);
-                //6. Исключить из сериализации конкретного свойства
-                printer.ExceptProperty(nameof(Person.Name));
-            
-            string s1 = printer.PrintToString(person);
-            // TODO use Verify.NUnit
+    [Test]
+    [Description("Возможность исключения из сериализации свойств определенного типа")]
+    public Task TypeExclusion()
+    {
+        var str = _config
+            .ExceptType<string>()
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
-            //8. ...с конфигурированием
-        }
+    [Test]
+    [Description("Возможность указать альтернативный способ сериализации для определенного типа")]
+    public Task TypeSerializationSpecification()
+    {
+        var str = _config
+            .ForType<Guid>()
+            .Use(o => o.ToString())
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
+
+    [Test]
+    [Description("Возможность для числовых типов указать культуру")]
+    public Task CultureSpecification()
+    {
+        var str = _config
+            .ForType<double>()
+            .UseCulture(CultureInfo.InvariantCulture)
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Возможность настройки сериализации конкретного свойства")]
+    public Task PropertySerializationSpecification()
+    {
+        _person.OtherPerson = new Person { Id = new Guid(), Name = "John", Surname = "Doe" };
+        
+        var str = _config
+            .ForProperty(x => x.OtherPerson!)
+            .Use(o => o.Name)
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Возможность обрезания строк")]
+    public Task StringTrimming()
+    {
+        var str = _config
+            .ForType<string>()
+            .UseMaxLength(2)
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Возможность исключения из сериализации конкретного свойства/поля")]
+    public Task PropertyExclusion()
+    {
+        var str = _config
+            .ExceptProperty(x => x.Id)
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Обработка циклических ссылок между объектами")]
+    public Task CyclicReferenceSupport()
+    {
+        _person.OtherPerson = _person;
+        
+        var str = _config
+            .WithMaxRecursionDepth(3)
+            .PrintToString(_person);
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Метод-расширение для сериализации по умолчанию")]
+    public Task ExtensionMethod_ForDefaultSerialization()
+    {
+        var str = _person.PrintToString();
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Метод-расширение, позволяющий проводить настройку сериализации")]
+    public Task ExtensionMethod_WithConfigurationSupport()
+    {
+        var str = _person.PrintToString(
+            config => config.ExceptProperty(x => x.Id));
+        
+        return Verifier.Verify(str);
+    }
+
+    [Test]
+    [Description("Сериализация массивов")]
+    public Task CollectionSupport_Arrays()
+    {
+        var arr = new double[] {1, 2, 3.14, 4, 5};
+        
+        var str = arr.PrintToString(
+            config => config
+                .ForType<double>()
+                .UseCulture(CultureInfo.InvariantCulture));
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Сериализация списков")]
+    public Task CollectionSupport_Lists()
+    {
+        var list = new List<Person>
+        {
+            _person,
+            new Person { Id = new Guid(), Name = "John", Surname = "Doe" }
+        };
+
+        var str = list.PrintToString(
+            config => config
+                .ForType<Person>()
+                .Use(o => o.Name));
+        
+        return Verifier.Verify(str);
+    }
+    
+    [Test]
+    [Description("Сериализация словарей")]
+    public Task CollectionSupport_Dictionaries()
+    {
+        var dictionary = new Dictionary<string, double>
+        {
+            { "a", 1.1 },
+            { "b", 2.2 }
+        };
+
+        var str = dictionary.PrintToString(
+            config => config
+                .ForType<double>()
+                .UseCulture(CultureInfo.InvariantCulture));
+        
+        return Verifier.Verify(str);
     }
 }
--- ObjectPrinting/Tests/Person.cs
+++ ObjectPrinting/Tests/Person.cs
@@ -6,7 +6,10 @@ namespace ObjectPrinting.Tests
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
+        public string Surname { get; set; }
         public double Height { get; set; }
         public int Age { get; set; }
+        
+        public Person? OtherPerson { get; set; }
     }
 }
