0c5878ba8cf4a7687d797275b0123f13b211b527
vafle228 <dimasmyslaev228@gmail.com>
2024-12-11 20:47:16 +0500

    Перенес сериализацию в сереализатор

--- ObjectPrinting/Serializer/Configs/PrintingConfig.cs
+++ ObjectPrinting/Serializer/Configs/PrintingConfig.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using ObjectPrinting.Serializer.Configs.Children;
 using ObjectPrinting.Tools;
 
@@ -42,33 +40,7 @@ public class PrintingConfig<TOwner>
 
     public string PrintToString(TOwner obj)
     {
-        return PrintToString(obj, 0);
-    }
-
-    private string PrintToString(object obj, int nestingLevel)
-    {
-        //TODO apply configurations
-        if (obj == null)
-            return "null" + Environment.NewLine;
-
-        var finalTypes = new[]
-        {
-            typeof(int), typeof(double), typeof(float), typeof(string),
-            typeof(DateTime), typeof(TimeSpan)
-        };
-        if (finalTypes.Contains(obj.GetType()))
-            return obj + Environment.NewLine;
-
-        var identation = new string('\t', nestingLevel + 1);
-        var sb = new StringBuilder();
-        var type = obj.GetType();
-        sb.AppendLine(type.Name);
-        foreach (var propertyInfo in type.GetProperties())
-        {
-            sb.Append(identation + propertyInfo.Name + " = " +
-                      PrintToString(propertyInfo.GetValue(obj),
-                          nestingLevel + 1));
-        }
-        return sb.ToString();
+        var serializer = new ConfigSerializer();
+        return serializer.PrintToString(obj, 0);
     }
 }
