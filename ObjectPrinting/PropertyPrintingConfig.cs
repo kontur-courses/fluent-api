4fb1cbddc195611bb071f9ad118a97c9b25c53c9
Asrom11 <asrom.11@mail.ru>
2024-12-11 22:01:23 +0500

    complete ObjectPrinter Task

---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace ObjectPrinting
+{
+    public class PropertyPrintingConfig<TOwner, TProperty>
+    {
+        private readonly PrintingConfig<TOwner> _config;
+        private readonly PropertyInfo _propertyInfo;
+
+        public PropertyPrintingConfig(PrintingConfig<TOwner> config, PropertyInfo propertyInfo)
+        {
+            _config = config;
+            _propertyInfo = propertyInfo;
+        }
+
+        public PrintingConfig<TOwner> UseSerializer(Func<TProperty, string> serializer)
+        {
+            _config.PropertySerializers[_propertyInfo] = value => serializer((TProperty)value);
+            return _config;
+        }
+
+        public PrintingConfig<TOwner> SetMaxLength(int maxLength)
+        {
+            _config.PropertiesMaxLength[_propertyInfo] = maxLength;
+            return _config;
+        }
+    }
+}
