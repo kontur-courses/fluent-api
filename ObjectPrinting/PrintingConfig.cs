8280b75fc1c2b236a9070076d0b77cabf4e11ce4
Leonid <rtdddawda2534@yandex.ru>
2024-12-11 23:04:12 +0500

    complete full logic

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -1,5 +1,5 @@
-using ObjectPrinting.Solved;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -13,15 +13,20 @@ namespace ObjectPrinting
     {
         private readonly HashSet<Type> excludedTypes = new();
         private readonly HashSet<MemberInfo> excludedProperties = new();
-        private readonly Dictionary<Type, Delegate> typeSerializers = new();
-        private readonly Dictionary<string, Delegate> propertySerializers = new();
+        private readonly Dictionary<Type, Delegate> typeSerializers = new(); // конкретный делегат
+        private readonly Dictionary<string, Delegate> propertySerializers = new(); // конкретный делегат
         private readonly Dictionary<Type, CultureInfo> typeCultures = new();
         private readonly Dictionary<string, int> propertyTrim = new();
 
-        // методы пошли кучно:
+        private readonly HashSet<Type> finalTypes =
+        [
+            typeof(bool), typeof(sbyte),  typeof(byte),  typeof(short),  typeof(ushort),
+            typeof(int),  typeof(uint),  typeof(long), typeof(ulong), typeof(float),
+            typeof(double),  typeof(decimal),  typeof(string), typeof(DateTime), typeof(TimeSpan), typeof(Guid)
+        ];
 
         // 1. Исключить из сериализации свойства определенного типа
-        public PrintingConfig<TOwner> ExcludePropertiesWithType<TPropType>()
+        public PrintingConfig<TOwner> Exclude<TPropType>()
         {
             excludedTypes.Add(typeof(TPropType));
             return this;
@@ -48,8 +53,6 @@ namespace ObjectPrinting
             return this;
         }
 
-        //
-
         public string PrintToString(TOwner obj)
         {
             return PrintToString(obj, 0);
@@ -58,28 +61,91 @@ namespace ObjectPrinting
 
         private string PrintToString(object obj, int nestingLevel)
         {
-            //TODO apply configurations
             if (obj == null)
-                return "null" + Environment.NewLine;
+                return "null";
 
-            var finalTypes = new[]
-            {
-                typeof(int), typeof(double), typeof(float), typeof(string),
-                typeof(DateTime), typeof(TimeSpan)
-            };
+            var type = obj.GetType();
+
+            if (excludedTypes.Contains(type))
+                return string.Empty;
+
             if (finalTypes.Contains(obj.GetType()))
-                return obj + Environment.NewLine;
+                return obj.ToString();
+
+            if (typeCultures.TryGetValue(type, out var culture) && obj is IFormattable formattable)
+                return formattable.ToString(null, culture);
+
+            if (typeSerializers.TryGetValue(type, out var serializer))
+                return serializer.DynamicInvoke(obj).ToString();
+
+            if (obj is ICollection collection)
+                return SerializeCollection(collection, nestingLevel);
 
             var identation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
-            var type = obj.GetType();
             sb.AppendLine(type.Name);
             foreach (var propertyInfo in type.GetProperties())
             {
-                sb.Append(identation + propertyInfo.Name + " = " +
-                          PrintToString(propertyInfo.GetValue(obj),
-                              nestingLevel + 1));
+                var propertyValue = propertyInfo.GetValue(obj);
+                var propertyType = propertyInfo.PropertyType;
+                var propertyName = propertyInfo.Name;
+
+                if (excludedProperties.Contains(propertyInfo))
+                    continue;
+                if (excludedTypes.Contains(propertyType))
+                    continue;
+
+                if (propertySerializers.TryGetValue(propertyName, out var propertySerializer))
+                {
+                    sb.AppendLine($"{identation}{propertySerializer.DynamicInvoke(propertyName)} = {propertyValue}");
+                    continue;
+                }
+
+                if (typeCultures.TryGetValue(propertyType, out var cult))
+                {
+                    if (propertyValue is IFormattable format)
+                    {
+                        sb.AppendLine($"{identation}{propertyName} = {format.ToString(null, cult)}");
+                        continue;
+                    }
+                }
+
+                if (propertyTrim.TryGetValue(propertyName, out var toTrimLength) && propertyValue is string stringValue)
+                {
+                    sb.AppendLine($"{identation}{propertyName} = {stringValue.Substring(0, Math.Min(stringValue.Length - toTrimLength, stringValue.Length))}");
+                    continue;
+                }
+
+                sb.AppendLine($"{identation}{propertyName} = {PrintToString(propertyValue, nestingLevel + 1)}");
             }
+            return sb.ToString();
+        }
+
+        private string SerializeCollection(ICollection collection, int nestingLevel)
+        {
+            var identation = new string('\t', nestingLevel);
+            var sb = new StringBuilder();
+
+            sb.AppendLine("[");
+
+            if (collection is IDictionary dictionary)
+            {
+                foreach (var key in dictionary.Keys)
+                {
+                    sb.Append("{" + PrintToString(key, nestingLevel + 1) + " = ");
+                    sb.Append(PrintToString(dictionary[key], nestingLevel + 1) + "}; ");
+                }
+            }
+            else
+            {
+                foreach (var value in collection)
+                {
+                    sb.Append('\t', nestingLevel + 1);
+                    sb.Append(PrintToString(value, nestingLevel + 1));
+                }
+            }
+
+            sb.AppendLine($"{identation}]");
             return sb.ToString();
         }
 
--- ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
+++ ObjectPrinting/Tests/ObjectPrinterAcceptanceTests.cs
@@ -1,4 +1,7 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Globalization;
+using ObjectPrinting.Extensions;
 
 namespace ObjectPrinting.Tests
 {
@@ -10,18 +13,29 @@ namespace ObjectPrinting.Tests
         {
             var person = new Person { Name = "Alex", Age = 19 };
 
-            var printer = ObjectPrinter.For<Person>();
+            var printer = ObjectPrinter.For<Person>()
                 //1. Исключить из сериализации свойства определенного типа
+                .Exclude<Guid>()
                 //2. Указать альтернативный способ сериализации для определенного типа
+                .Printing<int>().Using(i => i.ToString("X"))
                 //3. Для числовых типов указать культуру
+                .Printing<double>().UseCulture(CultureInfo.InvariantCulture)
                 //4. Настроить сериализацию конкретного свойства
+                .Printing(x => x.Name).Using(p => $"---{p}---")
                 //5. Настроить обрезание строковых свойств (метод должен быть виден только для строковых свойств)
+                .Printing(p => p.Name).TrimmedToLength(10)
                 //6. Исключить из сериализации конкретного свойства
-            
-            string s1 = printer.PrintToString(person);
+                .Exclude(p => p.Age);
 
-            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию        
+            string s1 = printer.PrintToString(person);
+            Console.WriteLine(s1);
+
+            //7. Синтаксический сахар в виде метода расширения, сериализующего по-умолчанию
+            var s2 = person.PrintToString();
+            Console.WriteLine(s2);
             //8. ...с конфигурированием
+            var s3 = person.PrintToString(p => p.Exclude(x => x.Age));
+            Console.WriteLine(s3);
         }
     }
 }
