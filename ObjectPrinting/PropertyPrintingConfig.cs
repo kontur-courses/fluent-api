7b477f044230af2eacdff1b1cb9a8c44db58e11e
Emi1337-ops <zaicev2400@mail.ru>
2024-12-12 00:12:05 +0500

    Add Homework Task Solution

---
+++ ObjectPrinting/PropertyPrintingConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace ObjectPrinting;
+
+public class PropertyPrintingConfig<TOwner, TPropType>
+{
+    private readonly PrintingConfig<TOwner> printingConfig;
+    protected internal readonly string? PropertyName;
+
+    public PropertyPrintingConfig(PrintingConfig<TOwner> printingConfig, string? propertyName = null)
+    {
+        this.printingConfig = printingConfig;
+        PropertyName = propertyName;
+    }
+
+    public PrintingConfig<TOwner> Using(Func<TPropType, string> print)
+    {
+        if (string.IsNullOrEmpty(PropertyName))
+        {
+            printingConfig.AddSerializationMethod(print);
+        }
+        else
+        {
+            printingConfig.AddSerializationMethod(print, PropertyName);
+        }
+        
+        return ParentConfig;
+    }
+
+    public PrintingConfig<TOwner> Using(CultureInfo culture)
+    {
+        if (string.IsNullOrEmpty(PropertyName))
+        {
+            printingConfig.SpecifyTheCulture<TPropType>(culture);
+        }
+        else
+        {
+            printingConfig.SpecifyTheCulture(culture, PropertyName);
+        }
+
+        return ParentConfig;
+    }
+
+    public PrintingConfig<TOwner> ParentConfig => printingConfig;
+}
---
+++ ObjectPrinting/Tests/ObjectPrinterTests.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ObjectPrinting.Extensions;
+using VerifyNUnit;
+using VerifyTests;
+
+namespace ObjectPrinting.Tests;
+
+[TestFixture]
+public class ObjectPrinterTests
+{
+    private static readonly VerifySettings Settings = new();
+    private Person person;
+    
+    [SetUp]
+    public void Setup()
+    {
+        Settings.UseDirectory("snapshots");
+        person = new Person
+        {
+            Name = "Bob", 
+            Age = 20, 
+            Height = 200, 
+            Weight = 100,
+            Birthday = new DateTime(2000, 10, 10),
+            IsStudent = true
+        };
+    }
+    
+    [Test]
+    public Task PrintToString_ExcludeType()
+    {
+        var actual = ObjectPrinter.For<Person>()
+            .Excluding<int>()
+            .PrintToString(person);
+
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_Correct_WhenCallAllMethods()
+    {
+        person.Height = 200.2;
+        person.Weight = 100.11;
+        var russianCulture = new CultureInfo("ru-RU");
+        var actual = ObjectPrinter.For<Person>()
+            .Excluding<bool>()
+            .Excluding(p => p.Id)
+            .Printing<DateTime>()
+            .Using(_ => "date")
+            .Printing<double>()
+            .Using(russianCulture)
+            .Printing(p => p.Name)
+            .TrimmedToLength(2)
+            .PrintToString(person);
+
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_AlternativeTypeSerialization()
+    {
+        var actual = ObjectPrinter.For<Person>()
+            .Printing<DateTime>()
+            .Using(_ => "date")
+            .PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_SetCultureForType()
+    {
+        person.Height = 200.2;
+        person.Weight = 100.11;
+        var russianCulture = new CultureInfo("ru-RU");
+
+        var actual = ObjectPrinter.For<Person>()
+            .Printing<double>()
+            .Using(russianCulture)
+            .PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_SetCultureForProperty()
+    {
+        person.Height = 200.2;
+        person.Weight = 100.11;
+        var russianCulture = new CultureInfo("ru-RU");
+
+        var actual = ObjectPrinter.For<Person>()
+            .Printing(p => p.Height)
+            .Using(russianCulture)
+            .PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+    
+    [Test]
+    public Task PrintToString_AlternativePropertySerialization()
+    {
+        var actual = ObjectPrinter.For<Person>()
+            .Printing(p => p.Name)
+            .Using(_ => "NoName")
+            .PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_Trim()
+    {
+        var actual = ObjectPrinter.For<Person>()
+            .Printing(p => p.Name)
+            .TrimmedToLength(2)
+            .PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_ExcludeProperty()
+    {
+        var actual = ObjectPrinter.For<Person>()
+            .Excluding(p => p.Id)
+            .PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+    
+    [Test]
+    public Task PrintToString_CircularReference()
+    {
+        person.Father = person;
+
+        var actual = ObjectPrinter.For<Person>().PrintToString(person);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_SerializeArray()
+    {
+        var persons = new[]
+        {
+            person,
+            new()
+            {
+                Name = "Bread", 
+                Age = 22, 
+                Height = 222, 
+                Weight = 111, 
+                Birthday = new DateTime(2001, 11, 11),
+                IsStudent = false
+            }
+        };
+
+        var actual = ObjectPrinter.For<Person[]>().PrintToString(persons);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_SerializeList()
+    {
+        var persons = new List<Person>
+        {
+            person,
+            new()
+            {
+                Name = "Bread", 
+                Age = 22, 
+                Height = 222, 
+                Weight = 111, 
+                Birthday = new DateTime(2001, 11, 11),
+                IsStudent = false
+            }
+        };
+
+        var actual = ObjectPrinter.For<List<Person>>().PrintToString(persons);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+
+    [Test]
+    public Task PrintToString_SerializeDictionary()
+    {
+        var persons = new Dictionary<Person, string>
+        {
+            { person, "first" },
+            { 
+                new()
+                {
+                    Name = "Bread", 
+                    Age = 22, 
+                    Height = 222, 
+                    Weight = 111, 
+                    Birthday = new DateTime(2001, 11, 11),
+                    IsStudent = false
+                }, 
+                "second" 
+            }
+        };
+
+        var actual = ObjectPrinter.For<Dictionary<Person, string>>().PrintToString(persons);
+        
+        return Verifier.Verify(actual, Settings);
+    }
+}
