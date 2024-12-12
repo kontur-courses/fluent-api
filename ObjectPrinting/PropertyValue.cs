0720f9073fd370f550f3184fd4ab2644f40dfe35
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-12 23:43:01 +0500

    Add PropertyPath and PropertyValue

---
+++ ObjectPrinting/PropertyValue.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace ObjectPrinting;
+
+internal record PropertyValue(string? Name, Type? DeclaringType, object? Value)
+{
+    public PropertyValue(PropertyInfo? propertyInfo, object? value)
+        : this(propertyInfo?.Name, propertyInfo?.DeclaringType, value)
+    {
+    }
+}
