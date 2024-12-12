d79b36a1cf462061c10e1709d04596f2d634395b
Nikita Shevyrin <nikita.shevyrin@gmail.com>
2024-12-12 21:52:39 +0500

    improved collections support and overall serialization style

--- ObjectPrinting/PrintingConfig.cs
+++ ObjectPrinting/PrintingConfig.cs
@@ -22,7 +22,7 @@ namespace ObjectPrinting
 
         public string PrintToString(TOwner obj)
         {
-            return PrintToString(obj, 0);
+            return PrintToString(obj, 0, 0);
         }
 
         internal void AddCultureSpec(Type type, CultureInfo cultureInfo)
@@ -94,69 +94,112 @@ namespace ObjectPrinting
             throw new ArgumentException("Expression does not refer to a property or field.");
         }
 
-        private string PrintToString(object obj, int nestingLevel)
+        private string PrintToString(object obj, int nestingLevel, int recursionDepth)
         {
             if (obj == null)
-                return $"null{Environment.NewLine}";
-
+                return "null";
+            
             if (obj is IFormattable formattable
                 && _cultureSpecs.TryGetValue(formattable.GetType(), out var cultureSpec))
-                return $"{formattable.ToString(null, cultureSpec)}{Environment.NewLine}";
+                return $"{formattable.ToString(null, cultureSpec)}";
             
             if (obj is string str)
-                return string.Concat(
-                    str.AsSpan(0, Math.Min(MaxStringLength, str.Length)),
-                    Environment.NewLine);
+                return str.Substring(0, Math.Min(MaxStringLength, str.Length));
             
             if (obj.GetType().IsValueType)
-                return $"{obj}{Environment.NewLine}";
+                return $"{obj}";
             
-            if (nestingLevel > MaxRecursionDepth)
-                return $"null{Environment.NewLine}";
+            if (recursionDepth > MaxRecursionDepth)
+                return "null";
 
             var indentation = new string('\t', nestingLevel + 1);
             var sb = new StringBuilder();
             var type = obj.GetType();
-            sb.AppendLine($"{type.Name}:");
+            
+            if (obj is IDictionary dictionary)
+                return SerializeDictionary(sb, dictionary, nestingLevel, recursionDepth);
 
             if (obj is IEnumerable enumerable)
-                return SerializeEnumerable(sb, enumerable, nestingLevel);
+                return SerializeEnumerable(sb, enumerable, nestingLevel, recursionDepth);
             
-            foreach (var propertyInfo in type.GetProperties())
+            var bracketIndentation = new string('\t', nestingLevel);
+            sb.AppendLine($"{type.Name}:");
+            sb.AppendLine($"{bracketIndentation}{{");
+            var properties = type.GetProperties();
+            foreach (var propertyInfo in properties)
             {
                 if (!_excludedProperties.Contains(propertyInfo) && !_excludedTypes.Contains(propertyInfo.PropertyType))
                 {
-                    var valueString = GetValueString(propertyInfo, obj, nestingLevel);
-                    sb.Append($"{indentation}{propertyInfo.Name} = {valueString}");
+                    var valueString = GetValueString(propertyInfo, obj, nestingLevel, recursionDepth);
+                    sb.AppendLine($"{indentation}{propertyInfo.Name} = {valueString}");
                 }
             }
+            sb.Append($"{bracketIndentation}}}");
             return sb.ToString();
         }
 
-        private string SerializeEnumerable(StringBuilder sb, IEnumerable enumerable, int nestingLevel)
+        private string SerializeEnumerable(StringBuilder sb, IEnumerable enumerable, int nestingLevel, int recursionDepth)
         {
             var bracketIndentation = new string('\t', nestingLevel);
             sb.AppendLine($"{bracketIndentation}[");
+            if (!enumerable.GetEnumerator().MoveNext())
+            {
+                return "[]";
+            }
+            enumerable.GetEnumerator().Reset();
             foreach (var element in enumerable)
             {
-                sb.Append($"{bracketIndentation}-\t");
+                sb.Append($"{bracketIndentation}\t");
                 var valueString = String.Empty;
                 if (_typeConverters.TryGetValue(element.GetType(), out var typeConverter))
                     valueString =
-                        $"{typeConverter.DynamicInvoke(element) as string ?? "null"}{Environment.NewLine}";
+                        $"{typeConverter.DynamicInvoke(element) as string ?? "null"}";
                 else
-                    valueString = PrintToString(element, nestingLevel + 1);
-                sb.Append($"{valueString}");
+                    valueString = PrintToString(element, nestingLevel + 1, recursionDepth + 1);
+                sb.AppendLine($"{valueString},");
             }
             sb.AppendLine($"{bracketIndentation}]");
             return sb.ToString();
         }
 
-        private string GetValueString(PropertyInfo propertyInfo, object obj, int nestingLevel)
+        private string SerializeDictionary(StringBuilder sb, IDictionary dictionary, int nestingLevel, int recursionDepth)
+        {
+            var bracketIndentation = new string('\t', nestingLevel);
+            sb.AppendLine($"{bracketIndentation}[");
+            foreach (DictionaryEntry element in dictionary)
+            {
+                var key = element.Key;
+                var value = element.Value;
+                
+                var keyValueIndentation = new string('\t', nestingLevel + 1);
+                sb.AppendLine($"{bracketIndentation}{{");
+                sb.Append($"{keyValueIndentation}key: ");
+                var keyString = String.Empty;
+                if (_typeConverters.TryGetValue(key.GetType(), out var typeConverter))
+                    keyString =
+                        $"{typeConverter.DynamicInvoke(key) as string ?? "null"}";
+                else
+                    keyString = PrintToString(key, nestingLevel + 2, recursionDepth + 1);
+                sb.AppendLine($"{keyString}");
+                sb.Append($"{keyValueIndentation}value: ");
+                var valueString = String.Empty;
+                if (_typeConverters.TryGetValue(value.GetType(), out typeConverter))
+                    valueString =
+                        $"{typeConverter.DynamicInvoke(value) as string ?? "null"}";
+                else
+                    valueString = PrintToString(value, nestingLevel + 2, recursionDepth + 1);
+                sb.AppendLine($"{valueString}");
+                sb.AppendLine($"{bracketIndentation}}},");
+            }
+            sb.AppendLine($"{bracketIndentation}]");
+            return sb.ToString();
+        }
+
+        private string GetValueString(PropertyInfo propertyInfo, object obj, int nestingLevel, int recursionDepth)
         {
             var propertyValue = propertyInfo.GetValue(obj);
             if (propertyValue == null || !TryConvert(propertyInfo, propertyValue, out var valueString))
-                valueString = PrintToString(propertyValue, nestingLevel + 1);
+                valueString = PrintToString(propertyValue, nestingLevel + 1, recursionDepth + 1);
             return valueString;
         }
 
@@ -164,11 +207,11 @@ namespace ObjectPrinting
         {
             value = String.Empty;
             if (_propertyConverters.TryGetValue(propertyInfo, out var converter))
-                value = $"{converter.DynamicInvoke(propertyValue) as string ?? "null"}{Environment.NewLine}";
+                value = $"{converter.DynamicInvoke(propertyValue) as string ?? "null"}";
             else if (propertyValue is string str && _stringPropertyLengths.TryGetValue(propertyInfo, out var length))
-                value = $"{str.Substring(0, Math.Min(length, str.Length))}{Environment.NewLine}{length}";
+                value = $"{str.Substring(0, Math.Min(length, str.Length))}";
             else if (_typeConverters.TryGetValue(propertyInfo.PropertyType, out var typeConverter))
-                value = $"{typeConverter.DynamicInvoke(propertyValue) as string ?? "null"}{Environment.NewLine}";
+                value = $"{typeConverter.DynamicInvoke(propertyValue) as string ?? "null"}";
             return value != String.Empty;
         }
     }
