43dc28ac12d5114e00a71629ab6bbebcbc66215a
stupidnessplusplus <58785209+stupidnessplusplus@users.noreply.github.com>
2024-12-12 23:43:43 +0500

    Add internal PrintingConfig interfaces

---
+++ ObjectPrinting/IPrintingConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace ObjectPrinting;
+
+internal interface IPrintingConfig<TOwner>
+{
+    Dictionary<Type, IPropertyPrintingConfig<TOwner>> PropertyConfigsByType { get; }
+
+    Dictionary<PropertyPath, IPropertyPrintingConfig<TOwner>> PropertyConfigsByPath { get; }
+
+    CultureInfo CultureInfo { get; }
+
+    bool IsToLimitNestingLevel { get; }
+
+    int MaxNestingLevel { get; }
+}
---
+++ ObjectPrinting/IPropertyPrintingConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+namespace ObjectPrinting;
+
+internal interface IPropertyPrintingConfig<TOwner>
+{
+    PrintingConfig<TOwner> ParentConfig { get; }
+
+    bool IsExcluded { get; }
+
+    CultureInfo? CultureInfo { get; }
+
+    Func<object, string>? PrintOverride { get; }
+}
